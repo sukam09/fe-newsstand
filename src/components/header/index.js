54df347a3a2a44d73bac6852db7beccee93d69ae
bae-sh <b5460881@naver.com>
2023-07-06 14:38:24 +0900

    feat : 뉴스스탠드 버튼 구현
    Co-authored-by: lee-yun-pyo <yun2193@hanmail.net>

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,3 +1,5 @@
+import Button from '../common/Button.js';
+
 export default class Header {
   constructor() {
     this.$header = document.createElement('header');
@@ -9,10 +11,13 @@ export default class Header {
   init() {
     const $headerName = document.createElement('h1');
     const $date = document.createElement('span');
+    const $icon = new Button({ icon: 'newspaper' });
 
-    $headerName.innerText = '뉴스스탠드';
+    $headerName.appendChild($icon);
+
     $date.innerText = '2023.02.10 금요일';
     $date.className = 'date';
+    $icon.src = '../../assets/icons/news-stand-simbol.svg';
 
     this.$header.appendChild($headerName);
     this.$header.appendChild($date);
