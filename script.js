3f868949d244b9d3b8208a0d59bdd763835e12c0
오지은 <0909oje@naver.com>
2023-07-06 16:51:00 +0900

    feat: 뉴스스탠드 reload() 추가

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const newsWrapper = document.querySelector(".news-wrapper");
 const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
+const systemDate = document.querySelector(".system-date");
 
 const makeGrid = (isLightMode, pageNum) => {
   for (let i = pageNum * 24; i < pageNum * 24 + 24; i++) {
@@ -23,8 +24,6 @@ const makeGrid = (isLightMode, pageNum) => {
 
 const movePrevPage = (page) => {};
 
-const systemDate = document.querySelector(".system-date");
-
 /* 시스템 날짜로 표시하기 */
 const getSystemDate = () => {
   const WEEKDAY = [
@@ -38,17 +37,13 @@ const getSystemDate = () => {
   ];
   let today = new Date();
   let year = today.getFullYear();
-  let month = today.getMonth() + 1; // 월은 0부터 시작
+  let month = today.getMonth() + 1;
   let date = today.getDate();
   let day = today.getDay();
 
   // 한자리 숫자 앞에 0을 추가
-  if (month < 10) {
-    month = "0" + month;
-  }
-  if (date < 10) {
-    date = "0" + date;
-  }
+  if (month < 10) month = "0" + month;
+  if (date < 10) date = "0" + date;
 
   const saveDate =
     year + ". " + month + ". " + date + ". " + WEEKDAY[Number(day)];
@@ -56,12 +51,17 @@ const getSystemDate = () => {
   return saveDate;
 };
 
-window.onload = function () {
+window.onload = () => {
   const dateItem = document.createElement("p");
   const dateText = document.createTextNode(getSystemDate());
   dateItem.appendChild(dateText);
   systemDate.append(dateItem);
 };
+
+/* 로고를 클릭하면 새로고침 */
+function reloadPage() {
+  location.reload();
+}
 
 function init() {
   makeGrid(true, 3);
