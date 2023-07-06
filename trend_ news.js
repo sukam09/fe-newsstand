706005ff38c9d6ae08880aff25706de909ea8eae
ori5r2 <ori5ri512@gmail.com>
2023-07-06 17:11:22 +0900

    feat: update banner

---
+++ trend_ news.js
@@ -0,0 +1,15 @@
+export const news_trend_left = [
+    {
+        press: "연합뉴스",
+        title: "[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출",
+        url: "#",
+    },
+];
+
+export const news_trend_right = [
+    {
+        press: "연합뉴스",
+        title: "[속보1] 與최고위원 본경선, 김병민·김용태·김재원·민영삼",
+        url: "#",
+    },
+];
