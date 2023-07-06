a5b97c8b99c4b6c562d22aa144571ce9db7d9cbe
bae-sh <b5460881@naver.com>
2023-07-06 17:47:12 +0900

    feat : AllNewHeader 컴포넌트 구현
    Co-authored-by: lee-yun-pyo <yun2193@hanmail.net>

--- src/components/allNews/index.js
+++ src/components/allNews/index.js
@@ -1,7 +1,5 @@
-import Icon from '../common/Icon.js';
+import AllNewHeader from './AllNewHeader.js';
 import AllNewsGrid from './AllNewsGrid.js';
-import AllNewsList from './AllnewsList.js';
-import ArrowButton from './ArrowButton.js';
 
 export default class AllNews {
   constructor() {
@@ -12,36 +10,7 @@ export default class AllNews {
   }
 
   init() {
-    this.$wrapper.appendChild(this.createAllNewHeader());
+    this.$wrapper.appendChild(new AllNewHeader());
     this.$wrapper.appendChild(new AllNewsGrid());
-  }
-
-  createAllNewHeader() {
-    const $header = document.createElement('div');
-    const $titleNavigation = document.createElement('nav');
-    const $iconNavigation = document.createElement('div');
-    $header.className = 'all-news-header';
-    $titleNavigation.className = 'view-type-wrapper';
-    $iconNavigation.className = 'view-type-icon';
-
-    const $allPress = document.createElement('span');
-    const $subscibedPress = document.createElement('span');
-
-    $allPress.innerText = '전체 언론사';
-    $subscibedPress.innerText = '내가 구독한 언론사';
-
-    $titleNavigation.appendChild($allPress);
-    $titleNavigation.appendChild($subscibedPress);
-
-    const $listViewIcon = new Icon({ name: 'list-view' });
-    const $gridViewIcon = new Icon({ name: 'grid-view' });
-
-    $iconNavigation.appendChild($listViewIcon);
-    $iconNavigation.appendChild($gridViewIcon);
-
-    $header.appendChild($titleNavigation);
-    $header.appendChild($iconNavigation);
-
-    return $header;
   }
 }
