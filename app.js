4eb1865277a3afbf528c75f450cb64b6ccb05c7b
asdf99245 <asdf99245@naver.com>
2023-07-06 18:10:22 +0900

    feat: news grid paging 구현
    Co-authored-by: Yunkyu Jung <asdf99245@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const NEWS_COUNT = 24;
+
 let pages = 0;
 
 const $prevPageButton = document.querySelector(".container-grid-view_left-btn");
@@ -18,11 +20,19 @@ const fetchNewsData = async () => {
   }
 };
 
+const isFirstPage = () => {
+  return pages === 0;
+};
+
+const isLastPage = () => {};
+
 /* grid view control */
 const fillNewsContents = (newsData) => {
   const $gridView = document.querySelector(".grid-view");
+  $gridView.innerHTML = "";
+  const startIndex = pages * NEWS_COUNT;
 
-  for (let i = 0; i < 24; i++) {
+  for (let i = startIndex; i < startIndex + NEWS_COUNT; i++) {
     const $li = document.createElement("li");
     $li.className = "grid-cell";
 
@@ -30,6 +40,7 @@ const fillNewsContents = (newsData) => {
 
     const $img = document.createElement("img");
     $img.className = "grid-cell_news-img";
+
     $img.src = newsData[i].src;
     $img.alt = newsData[i].name;
 
@@ -38,15 +49,6 @@ const fillNewsContents = (newsData) => {
 
     $gridView.appendChild($li);
   }
-};
-
-/* event handler */
-const handlePrevPageButtonClick = () => {
-  pages && pages--;
-};
-
-const handleNextPageButtonClick = () => {
-  pages++;
 };
 
 (async function init() {
@@ -58,6 +60,32 @@ const handleNextPageButtonClick = () => {
 
   fillNewsContents(newsData);
 
-  $prevPageButton.addEventListener("click", handlePrevPageButtonClick);
-  $nextPageButton.addEventListener("click", handleNextPageButtonClick);
+  /* event handler */
+
+  const handlePrevButtonClick = () => {
+    $nextPageButton.classList.remove("hidden");
+    pages--;
+
+    if (pages === 0) {
+      $prevPageButton.classList.add("hidden");
+    }
+    fillNewsContents(newsData);
+  };
+
+  const handleNextButtonClick = () => {
+    const maxPage = Math.floor(newsData.length / NEWS_COUNT) - 1;
+
+    if (pages === maxPage) {
+      return;
+    }
+
+    $prevPageButton.classList.remove("hidden");
+    pages++;
+
+    if (pages === maxPage) $nextPageButton.classList.add("hidden");
+    fillNewsContents(newsData);
+  };
+
+  $prevPageButton.addEventListener("click", handlePrevButtonClick);
+  $nextPageButton.addEventListener("click", handleNextButtonClick);
 })();
