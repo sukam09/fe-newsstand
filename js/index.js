ac0e860dbbd6e1c25a031f4c88f8a244b4dde1a0
WongilK <wwwkim99@naver.com>
2023-07-07 10:13:00 +0900

    [FEAT] : 날짜 정보 표시
    Co-authored-by: kimdaye77 <kimdaye77@naver.com>

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
 document.documentElement.setAttribute("color-theme", "light");
 
 //변수
-const mainLogo = document.querySelector(".title");
+const week = ["일", "월", "화", "수", "목", "금", "토"];
 
+const mainLogo = document.querySelector(".title");
+const day = document.querySelector(".date");
 //함수
 const reload = () => {
   window.location.reload();
 };
 
+const makeDate = () => {
+  const date = new Date();
+  day.innerHTML = `${date.getFullYear()}. ${String(
+    date.getMonth() + 1
+  ).padStart(2, 0)}. ${String(date.getDate()).padStart(2, 0)}. ${
+    week[date.getDay()]
+  }요일`;
+};
+//todo padstart
+const getDateInterval = () => {
+  setInterval(makeDate, 60000);
+};
 //이벤트 리스너
 mainLogo.addEventListener("click", reload);
+
+//default
+makeDate();
+getDateInterval();
