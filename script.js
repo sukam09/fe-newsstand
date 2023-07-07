b46b11715a60462f0b77f76ba016383613e9ca61
jhyep <pjh1115_@naver.com>
2023-07-07 11:50:09 +0900

    feat: 언론사 그리드 페이지네이션 및 새로고침 시 랜덤으로 순서 변경

--- script.js
+++ script.js
@@ -1,4 +1,92 @@
+let presses = [];
+let grid_page_count = 0;
+
+for (let i = 1; i < 97; i++) {
+  presses.push({
+    id: i,
+    src: `${i}.png`,
+  });
+}
+
+const shuffle = () => Math.random() - 0.5;
+let shuffled_presses = [...presses].sort(shuffle);
+
+document.getElementById("grid-next").addEventListener("click", () => {
+  if (grid_page_count + 1 === presses.length / 24 - 1) {
+    document.getElementById("grid-next").style.display = "none";
+  }
+  if (grid_page_count + 1 < parseInt(presses.length / 24)) {
+    document.getElementById("grid-prev").style.display = "block";
+    document.getElementById("press-list").innerHTML = "";
+    grid_page_count += 1;
+    const slice_shuffled_presses = shuffled_presses.slice(
+      grid_page_count * 24,
+      (grid_page_count + 1) * 24
+    );
+    slice_shuffled_presses.forEach((press) => {
+      let image = document.createElement("img");
+      image.src = `./icons/press_logo/${press.src}`;
+      let sub = document.createElement("img");
+      image.classList.add("original");
+      sub.src = `./icons/Button.svg`;
+      sub.classList.add("sub");
+
+      let list = document.createElement("li");
+      list.classList.add("press-item");
+      list.appendChild(image);
+      list.appendChild(sub);
+      document.getElementById("press-list").appendChild(list);
+    });
+  }
+});
+
+document.getElementById("grid-prev").addEventListener("click", () => {
+  if (grid_page_count - 1 === 0) {
+    document.getElementById("grid-prev").style.display = "none";
+  }
+  if (grid_page_count - 1 >= 0) {
+    document.getElementById("grid-next").style.display = "block";
+    document.getElementById("press-list").innerHTML = "";
+    grid_page_count -= 1;
+    const slice_shuffled_presses = shuffled_presses.slice(
+      grid_page_count * 24,
+      grid_page_count * 24 + 24
+    );
+    slice_shuffled_presses.forEach((press) => {
+      let image = document.createElement("img");
+      image.src = `./icons/press_logo/${press.src}`;
+      let list = document.createElement("li");
+      let sub = document.createElement("img");
+      image.classList.add("original");
+      sub.src = `./icons/Button.svg`;
+      sub.classList.add("sub");
+      list.classList.add("press-item");
+      list.appendChild(image);
+      list.appendChild(sub);
+
+      document.getElementById("press-list").appendChild(list);
+    });
+  }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("grid-prev").style.display = "none";
+  const slice_shuffled_presses = shuffled_presses.slice(0, 24);
+  slice_shuffled_presses.forEach((press) => {
+    let image = document.createElement("img");
+    image.src = `./icons/press_logo/${press.src}`;
+    let sub = document.createElement("img");
+    image.classList.add("original");
+    sub.src = `./icons/Button.svg`;
+    sub.classList.add("sub");
+
+    let list = document.createElement("li");
+    list.classList.add("press-item");
+    list.appendChild(image);
+    list.appendChild(sub);
+    document.getElementById("press-list").appendChild(list);
+  });
+
   var interval = window.setInterval(firstRollingCallback, 5000);
   setTimeout(function () {
     var interval2 = window.setInterval(secondRollingCallback, 5000);
