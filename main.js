1da250a902f4e10e1b950054ca9dde8f617d2a61
seokhun <tommya98@seokhunui-MacBookPro.local>
2023-07-07 13:13:35 +0900

    style: date style changed

--- main.js
+++ main.js
@@ -82,6 +82,7 @@ function moveGrid(dir) {
 function initDate() {
   const date = new Date();
   const week = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
-  const todaystr = `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}. ${week[date.getDay()]}`;
+  // const todaystr = `${date.getFullYear()}. ${(date.getMonth() + 1).padStat(2, "0")}. ${date.getDate().padStat(2, "0")}. ${week[date.getDay()]}`;
+  const todaystr = `${date.getFullYear()}. ${String(date.getMonth() + 1).padStart(2, "0")}. ${String(date.getDate()).padStart(2, "0")}. ${week[date.getDay()]}`;
   document.querySelector(".today").innerHTML = todaystr;
 }
