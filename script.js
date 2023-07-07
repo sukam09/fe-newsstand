2b2b4497e7a016670db5f69cf703b3a44d04c6fb
jhyep <pjh1115_@naver.com>
2023-07-07 10:47:41 +0900

    feat: 뉴스 무한 롤링 시간 차이 구현

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-  var interval = window.setInterval(rollingCallback, 5000);
+  var interval = window.setInterval(firstRollingCallback, 5000);
+  setTimeout(function () {
+    var interval2 = window.setInterval(secondRollingCallback, 5000);
+  }, 2000);
 });
-function rollingCallback() {
-  for (item of document.querySelectorAll(".rollingbanner .prev")) {
-    item.classList.remove("prev");
+function firstRollingCallback() {
+  //.prev 클래스 삭제
+  document.querySelector(".rollingbanner .prev").classList.remove("prev");
+
+  //.current -> .prev
+  let current = document.querySelector(".rollingbanner .current");
+  current.classList.remove("current");
+  current.classList.add("prev");
+
+  //.next -> .current
+  let next = document.querySelector(".rollingbanner .next");
+  //다음 목록 요소가 널인지 체크
+  if (next.nextElementSibling == null) {
+    document
+      .querySelector(".rollingbanner ul li:first-child")
+      .classList.add("next");
+  } else {
+    //목록 처음 요소를 다음 요소로 선택
+    next.nextElementSibling.classList.add("next");
   }
-  for (item of document.querySelectorAll(".rollingbanner .current")) {
-    item.classList.remove("current");
-    item.classList.add("prev");
+  next.classList.remove("next");
+  next.classList.add("current");
+}
+
+function secondRollingCallback() {
+  //.prev 클래스 삭제
+  const prev = document.querySelectorAll(".rollingbanner .prev");
+  prev[1].classList.remove("prev");
+
+  //.current -> .prev
+  const current = document.querySelectorAll(".rollingbanner .current");
+  current[1].classList.remove("current");
+  current[1].classList.add("prev");
+
+  //.next -> .current
+  const next = document.querySelectorAll(".rollingbanner .next");
+
+  //다음 목록 요소가 널인지 체크
+  if (next[1].nextElementSibling == null) {
+    document
+      .querySelectorAll(".rollingbanner ul li:first-child")[1]
+      .classList.add("next");
+  } else {
+    //목록 처음 요소를 다음 요소로 선택
+    next[1].nextElementSibling.classList.add("next");
   }
-  for (let i = 0; i < 2; i++) {
-    let item = document.querySelectorAll(".rollingbanner .next");
-    let first_lists = document.querySelectorAll(
-      ".rollingbanner ul li:first-child"
-    );
-    if (item[i].nextElementSibling === null) {
-      first_lists[i].classList.add("next");
-    } else {
-      item[i].nextElementSibling.classList.add("next");
-    }
-    item[i].classList.remove("next");
-    item[i].classList.add("current");
-  }
+  next[1].classList.remove("next");
+  next[1].classList.add("current");
 }
