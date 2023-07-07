cd1c8ef057e88b89cfad258236bceb98389fdf1a
오지은 <0909oje@naver.com>
2023-07-07 11:04:49 +0900

    feat: 그리드 페이지 전환

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const newsWrapper = document.querySelector(".news-wrapper");
-const leftArrow = document.querySelector(".left-arrow");
-const rightArrow = document.querySelector(".right-arrow");
 const systemDate = document.querySelector(".system-date");
 
 let idList = Array.from({ length: 96 }, (_, idx) => idx);
+let isLightMode = true;
+let pageNum = 0;
 
 const shuffleIdList = (idList) => {
   idList.sort(() => Math.random() - 0.5);
 };
 
-const makeGrid = (isLightMode, pageNum) => {
-  for (let i = pageNum * 24; i < pageNum * 24 + 24; i++) {
+const makeGrid = () => {
+  for (let i = 0; i < 24; i++) {
     const gridItem = document.createElement("li");
     const imgSrc = isLightMode
       ? `./img/light-media/${idList[i]}.png`
@@ -20,6 +20,7 @@ const makeGrid = (isLightMode, pageNum) => {
     checkImg.src = imgSrc;
     checkImg.onload = function () {
       const img = document.createElement("img");
+      img.classList.add(`img${i}`);
       img.src = imgSrc;
       img.style.height = "20px";
       gridItem.appendChild(img);
@@ -29,7 +30,50 @@ const makeGrid = (isLightMode, pageNum) => {
   }
 };
 
-const movePrevPage = (page) => {};
+const changeImgSrc = () => {
+  let newImg = idList.slice(pageNum * 24, pageNum * 24 + 24);
+
+  for (let i = 0; i < 24; i++) {
+    const $img = document.querySelector(`.img${i}`);
+    const imgSrc = isLightMode
+      ? `./img/light-media/${newImg[i]}.png`
+      : `./img/dark-media/${newImg[i]}.png`;
+
+    let checkImg = new Image();
+    checkImg.src = imgSrc;
+    checkImg.onload = function () {
+      $img.src = imgSrc;
+    };
+    checkImg.onerror = function () {
+      $img.remove();
+    };
+  }
+};
+
+const setArrowVisible = () => {
+  const leftArrow = document.querySelector(".left-arrow");
+  const rightArrow = document.querySelector(".right-arrow");
+
+  if (pageNum === 0) {
+    leftArrow.classList.add("hidden");
+  } else if (pageNum > 0 && pageNum < 3) {
+    leftArrow.classList.remove("hidden");
+    rightArrow.classList.remove("hidden");
+  } else if (pageNum === 3) {
+    rightArrow.classList.add("hidden");
+  }
+};
+
+const movePage = (className) => {
+  if (className === "left-arrow") {
+    pageNum--;
+  }
+  if (className === "right-arrow") {
+    pageNum++;
+  }
+  changeImgSrc();
+  setArrowVisible();
+};
 
 /* 시스템 날짜로 표시하기 */
 const getSystemDate = () => {
@@ -71,7 +115,8 @@ function reloadPage() {
 
 function init() {
   shuffleIdList(idList);
-  makeGrid(true, 0);
+  setArrowVisible();
+  makeGrid();
 }
 
 init();
