174f0c830b285c69ea1c922b0de5e573ded69ad7
choiseona <tjsdk617@naver.com>
2023-07-07 10:25:43 +0900

    Feat: 객체 리터럴 셔플하기

--- newslist.js
+++ newslist.js
@@ -1,15 +1,16 @@
 
 const newsList = [];
 const array = [];
+
 for(let i=0;i<96;i++){
     array.push(i);
 }
 array.forEach(arr => {
     newsList.push({"id":arr});
 })
-console.log(newsList);
 
-const shuffledArray = array.sort(() => Math.random() - 0.5);
+const shuffledArray = [...newsList].sort(() => Math.random() - 0.5);
+console.log(newsList);
 console.log(shuffledArray);
 
 
