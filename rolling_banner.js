71b1a2ef78e057191049b32c9ade13590e03af66
ori5r2 <ori5ri512@gmail.com>
2023-07-07 10:06:46 +0900

    feat: rolling banner

--- rolling_banner.js
+++ rolling_banner.js
@@ -2,7 +2,35 @@ import { news_trend_left, news_trend_right } from "./trend_ news.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     getBanner();
+    window.setInterval(rollingCallback, 3000);
 });
+
+function rollingCallback() {
+    let bannerArr = document.querySelectorAll(".rolling_banner");
+    bannerArr.forEach((banner) => rollingBanner(banner));
+}
+
+function rollingBanner(banner) {
+    //.prev 클래스 삭제
+    let prev = banner.querySelector(".news_list .prev");
+    prev.classList.remove("prev");
+
+    //.current -> .prev
+    let current = banner.querySelector(".news_list .current");
+    current.classList.remove("current");
+    current.classList.add("prev");
+    //.next -> .current
+    let next = banner.querySelector(".news_list .next");
+    //다음 목록 요소가 널인지 체크
+    if (next.nextElementSibling == null) {
+        banner.querySelector(".news_list li:first-child").classList.add("next");
+    } else {
+        //목록 처음 요소를 다음 요소로 선택
+        next.nextElementSibling.classList.add("next");
+    }
+    next.classList.remove("next");
+    next.classList.add("current");
+}
 
 // 리스트 하나 만들기
 function getNewsLi(press, title, url, idx) {
@@ -26,6 +54,7 @@ function getNewsLi(press, title, url, idx) {
 // 전체 리스트 만들기
 function getNewsUl(data) {
     const news_list = document.createElement("ul");
+    news_list.setAttribute("class", "news_list");
 
     data.forEach((news, idx) => {
         news_list.appendChild(getNewsLi(news.press, news.title, news.url, idx));
@@ -35,6 +64,6 @@ function getNewsUl(data) {
 }
 
 function getBanner() {
-    document.getElementById("sectionLeft").appendChild(getNewsUl(news_trend_left));
-    document.getElementById("sectionRight").appendChild(getNewsUl(news_trend_right));
+    document.getElementById("rollingBannerLeft").appendChild(getNewsUl(news_trend_left));
+    document.getElementById("rollingBannerRight").appendChild(getNewsUl(news_trend_right));
 }
