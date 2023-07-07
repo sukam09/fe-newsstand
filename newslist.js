2f79861b66f695a829700dd92ae6bdfbf1868dcc
meanz1 <zndnfjqbd1206@gmail.com>
2023-07-07 11:10:14 +0900

    Feat: 생성된 셔플 배열을 이용하여 언론사 목록의 첫페이지 띄우기

--- newslist.js
+++ newslist.js
@@ -1,4 +1,3 @@
-
 const newsList = [];
 const array = [];
 
@@ -10,7 +9,37 @@ array.forEach(arr => {
 })
 
 const shuffledArray = [...newsList].sort(() => Math.random() - 0.5);
-console.log(newsList);
-console.log(shuffledArray);
+const sectionNewsList =  document.getElementById('section-news-list');
+const page0 = [];
+const page1 = [];
+const page2 = [];
+const page3 = [];
 
+shuffledArray.forEach((arr, idx) => {
+    if(0<=idx && idx <=23) {
+        page0.push(arr);
+    }
+})
+
+shuffledArray.forEach((arr, idx) => {
+    if(24<=idx && idx <=47) {
+        page1.push(arr);
+    }
+})
+
+shuffledArray.forEach((arr, idx) => {
+    if(48<=idx && idx <=71) {
+        page2.push(arr);
+    }
+})
+
+shuffledArray.forEach((arr, idx) => {
+    if(72<=idx && idx <=95) {
+        page3.push(arr);
+    }
+})
+
+sectionNewsList.innerHTML = `
+    ${page1.map(arr => `<li><img src="./icons/asset ${arr["id"]} 1.png"</li>`).join('')};
+`
 
