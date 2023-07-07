19ad060526107c536efc476658d09d84da291588
Jo <tockiib@gmail.com>
2023-07-07 12:07:33 +0900

    fix: 구독 여부에 따라 구독, 해지 정상 작동하도록 수정

--- script/grid.js
+++ script/grid.js
@@ -1,4 +1,5 @@
 import { MEDIA } from './constants.js';
+import media_data from '../assets/data/media_data.js';
 const mediaLogo = document.querySelectorAll('.media_logo');
 const arrow_left = document.querySelector('#arrow_wrapper_left');
 const arrow_right = document.querySelector('#arrow_wrapper_right');
@@ -8,20 +9,39 @@ const subscribed = [
 ];
 let page = 0;
 
-const logoIndex = Array.from({ length: MEDIA.TOTAL }, (_, index) => index + 1);
+const logoIndex = Array.from({ length: MEDIA.TOTAL }, (_, index) => index);
 
 const shuffle = array => {
   array.sort(() => Math.random() - 0.5);
 };
 
 const setLogoList = () => {
+  const addSubButton = isSub => {
+    const subElement = document.createElement('div');
+    subElement.className = 'media-hover surface-alt';
+    subElement.innerHTML = isSub ? '구독하기' : '해지하기';
+    return subElement;
+  };
   const gridIndex = Array.from(
     { length: MEDIA.PAGE_SIZE },
     (_, index) => page * MEDIA.PAGE_SIZE + index
   );
   gridIndex.forEach((media, index) => {
-    mediaLogo[index].src = `assets/images/logo/light/${logoIndex[media]}.png`;
-    mediaLogo[index].className = `media_logo media_${logoIndex[media]}`;
+    const imageElement = mediaLogo[index];
+    imageElement.className = `media_logo media_${logoIndex[media]}`;
+    imageElement.src = `assets/images/logo/light/${
+      media_data[logoIndex[media]]?.src
+    }`;
+
+    // 이미지랑 같은 노드인 구독하기 or 해지하기 제거
+    imageElement.nextElementSibling?.remove();
+    imageElement.parentElement.className = '';
+
+    // 구독 여부에 따라 구독하기 or 해지하기 추가
+    imageElement.insertAdjacentElement(
+      'afterend',
+      addSubButton(!subscribed.includes(logoIndex[media]))
+    );
   });
 };
 
@@ -47,20 +67,10 @@ const setArrowVisible = () => {
   arrow_left.className = `page_${page}`;
   arrow_right.className = `page_${page}`;
 };
-const setSubscribed = () => {
-  subscribed.forEach(index => {
-    const media = document.querySelector(`.media_${index}`);
-    if (media) {
-      media.parentElement.classList.add('subscribed');
-      media.nextElementSibling.innerHTML = '해지하기';
-    }
-  });
-};
 
 const init = () => {
   shuffle(logoIndex);
   setLogoList();
-  setSubscribed();
   setArrow();
 };
 
