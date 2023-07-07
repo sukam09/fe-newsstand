a71d97e10d9a8d6c3d50c4244bc25bfb732d02e7
KimJungHun <j7papa@naver.com>
2023-07-07 13:36:32 +0900

    [Feat] 페이지 전환 이벤트 구현

--- js/main.js
+++ js/main.js
@@ -2,28 +2,9 @@ const indexArr = Array.from({length: 96}, (_,i)=>i);
 indexArr.sort(() => Math.random() - 0.5);
 let page = 0;
 
-const arr = {
-    list: [
-        {
-            lightUrl: "light0",
-            darkUrl: "dark0",
-            name: "name0",
-            id: 0,
-        },
-        {
-            lightUrl: "light1",
-            darkUrl: "dark1",
-            name: "name1",
-            id: 1,
-        },
-        {
-            lightUrl: "light2",
-            darkUrl: "dark2",
-            name: "name2",
-            id: 2,
-        },
-    ]
-}
+const newspaperList = document.querySelector(".newspaper__list");
+const leftButton = document.querySelector(".left-button_content");
+const rightButton = document.querySelector(".right-button_content");
 
 const createNewspaperItem = function(index, mode) {
     return `
@@ -33,12 +14,37 @@ const createNewspaperItem = function(index, mode) {
     `
 }
 
-const createNewspaperList = function(page, mode) {
+const createNewspaperList = function(mode) {
     const nowPageIndexArr = indexArr.slice(page * 24, (page + 1) * 24);
     const liArr = nowPageIndexArr.map(item => createNewspaperItem(item, mode))
     let newspaperList = liArr.reduce((news, currentIndex) => news + currentIndex);
     return newspaperList;
 }
 
-console.log(createNewspaperList(page, "light"));
-document.querySelector(".newspaper__list").innerHTML = createNewspaperList(page, "light");
+const renderNewspaper = function(mode) {
+    newspaperList.innerHTML = createNewspaperList(mode);
+}
+
+const setDisplayButton = function() {
+    leftButton.style.display = page === 0 ? "none" : "block";
+    rightButton.style.display = page === 3 ? "none" : "block";
+}
+
+const renderContent = function() {
+    renderNewspaper("light");
+    setDisplayButton();
+}
+
+const movePageLeft = function() {
+    page--;
+    renderContent();
+}
+const movePageRight = function() {
+    page++;
+    renderContent();
+}
+
+leftButton.addEventListener("click", movePageLeft);
+rightButton.addEventListener("click", movePageRight);
+
+renderNewspaper("light");
