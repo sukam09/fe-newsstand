9ed4d501f76d494f6104d25906a74ecc8ffc1287
bae-sh <b5460881@naver.com>
2023-07-09 00:12:22 +0900

    chore : getTodayDate 로직 변경

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,13 @@
 function getTodayDate() {
-  const date = new Date();
-  const days = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+  const today = new Date();
+  const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    weekday: 'long',
+  };
 
-  return `${date.getFullYear()}. ${`${date.getMonth() + 1}`.padStart(
-    2,
-    '0',
-  )}. ${`${date.getDate()}`.padStart(2, '0')}. ${days[date.getDay()]}`;
+  return today.toLocaleDateString('ko-KR', options);
 }
 
 function shufflePressOrder() {
