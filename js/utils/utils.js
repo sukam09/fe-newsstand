87884110609affae639e3117174d147da1ee2a10
박태규 Park Tae Kyu <ptq124@naver.com>
2023-07-10 14:32:44 +0900

    feat: 셔플, 데이터 함수

---
+++ js/utils/utils.js
@@ -0,0 +1,19 @@
+function shuffle(arr) {
+  return arr.sort(() => Math.random() - 0.5);
+}
+
+function date() {
+  const $headerDate = document.querySelector('.header—date');
+  const today = new Date();
+
+  const options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    weekday: 'long',
+  };
+
+  $headerDate.innerText = today.toLocaleDateString('ko-KR', options);
+}
+
+export { shuffle, date };
