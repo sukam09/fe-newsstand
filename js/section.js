9744403aac039313f89aeecb93cdf28989bd963a
DaeHee99 <mokday99@gmail.com>
2023-07-10 11:05:09 +0900

    [Fix] rolling section & time

--- js/section.js
+++ js/section.js
@@ -1,14 +1,16 @@
-const setRollingEvent = function(rollingElement){
-	window.setInterval( function () {
-		rollingElement.style.transitionDuration = "400ms";
-		rollingElement.style.marginTop = "-16px";
-	
-		window.setTimeout( function () {
-			rollingElement.removeAttribute("style");         
-			rollingElement.appendChild(rollingElement.firstElementChild);
-		}, 400);
-	}, 2000);
-}
+const setRollingEvent = function (rollingElement, startTime) {
+  setTimeout(() => {
+    window.setInterval(function () {
+      rollingElement.style.transitionDuration = "400ms";
+      rollingElement.style.marginTop = "-16px";
+
+      window.setTimeout(function () {
+        rollingElement.removeAttribute("style");
+        rollingElement.appendChild(rollingElement.firstElementChild);
+      }, 400);
+    }, 5000);
+  }, startTime * 1000);
+};
 
 let rolling = document.querySelectorAll(".rolling > ul");
-rolling.forEach(elem => setRollingEvent(elem));
+rolling.forEach((elem, index) => setRollingEvent(elem, index));
