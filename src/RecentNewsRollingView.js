baf272d27d5dbe33327f354491e6db1d401f18d3
sukam09 <sukam09@naver.com>
2023-07-10 13:46:12 +0900

    feat: 최신 뉴스 롤링 영역에 initial state 추가

--- src/RecentNewsRollingView.js
+++ src/RecentNewsRollingView.js
@@ -1,20 +1,25 @@
-export default function RecentNewsRollingViewView({ $target }) {
+export default function RecentNewsRollingViewView({ $target, initialState }) {
   const $section = document.createElement('section');
   $section.classList.add('recent-news-container');
 
+  this.state = initialState;
+
+  this.setState = nextState => {
+    this.state = nextState;
+    this.render();
+  };
+
   this.render = () => {
+    const { headlineIndex, leftHeadlines, rightHeadlines } = this.state;
+
     $section.innerHTML = `
       <div class="recent-news-item">
-      <span class="recent-news-press">연합뉴스</span>
-      <span class="recent-news-headline"
-        >[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출</span
-      >
+        <span class="recent-news-press">연합뉴스</span>
+        <span class="recent-news-headline">${leftHeadlines[headlineIndex]}</span>
       </div>
       <div class="recent-news-item">
         <span class="recent-news-press">연합뉴스</span>
-        <span class="recent-news-headline"
-          >[속보] 與최고위원 본경선, 김병민·김용태·김재원·민영삼</span
-        >
+        <span class="recent-news-headline">${rightHeadlines[headlineIndex]}</span>
       </div>
     `;
 
--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import NewsPressGridview from './NewsPressGridview.js';
 export default function App({ $app }) {
   new Header({ $target: $app });
 
-  new RecentNewsRollingView({ $target: $app });
+  new RecentNewsRollingView({
+    $target: $app,
+    initialState: {
+      headlineIndex: 0,
+      leftHeadlines: [
+        `정부, '처리수' 표현에 "문제없어", '핵폐수'엔 "불안감 내용 부적절"`,
+        `IAEA 최종보고서 반발…전국 곳곳서 후쿠시마 오염수 방류 규탄`,
+        `일본 원전 오염수 방류도 안했는데…찬바람 부는 횟집·양식어가`,
+        `'오염수 저지' 野의원단 방일 출국…"국제공조 통해 방류 저지"`,
+        `與 "민주 'IAEA 총장면담·日방문', 부끄럽고 한심…국격 추락"`,
+      ],
+      rightHeadlines: [
+        `尹대통령, 리투아니아로 출국…나토회의서 북핵 공조 모색`,
+        `검찰, 국회 2차 압수수색…송영길 보좌진 동선 추적`,
+        `검찰, 박영수 측근 불러 보강수사…구속영장 재청구 '무게'`,
+        `조국 아들 "연세대 대학원에 석사 학위 반납"`,
+        `민주, 양평고속道 의혹에 "게이트·국정농단"…국조·특검도 거론`,
+      ],
+    },
+  });
 
   new NewsPressGridview({
     $target: $app,
