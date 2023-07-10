f40654e056edc0023da69e510adb90d181e1b05e
WongilK <wwwkim99@naver.com>
2023-07-10 15:12:49 +0900

    [FEAT] : 자동 롤링 뉴스 배너 구현

--- js/index.js
+++ js/index.js
@@ -1,80 +1,144 @@
-document.documentElement.setAttribute("color-theme", "light");
-
-//변수
-const week = ["일", "월", "화", "수", "목", "금", "토"];
-const imgIndex = Array(96)
-  .fill()
-  .map((arr, i) => i);
-let main_list_page = 1;
-
-const mainLogo = document.querySelector(".title");
-const day = document.querySelector(".date");
-const main_list_ul = document.querySelector(".main-list-ul");
-const left_btn = document.getElementById("left-btn");
-const right_btn = document.getElementById("right-btn");
-
-//함수
-function reload() {
-  window.location.reload();
-}
-
-function makeDate() {
-  const date = new Date();
-  day.innerHTML = `${date.getFullYear()}. ${String(
-    date.getMonth() + 1
-  ).padStart(2, 0)}. ${String(date.getDate()).padStart(2, 0)}. ${
-    week[date.getDay()]
-  }요일`;
-}
-
-function shuffleImgIndex() {
-  return [...imgIndex].sort(() => Math.random() - 0.5);
-}
-const shuffledPress = shuffleImgIndex();
-
-function getDateInterval() {
-  setInterval(makeDate, 60000);
-}
-
-function showMainList() {
-  main_list_ul.innerHTML = "";
-  for (let i = 24 * (main_list_page - 1); i < 24 * main_list_page; i++) {
-    const li = document.createElement("li");
-    const img = document.createElement("img");
-    img.setAttribute(
-      "src",
-      `../images/lightmode-media/asset ${shuffledPress[i]} 1.png`
-    );
-    main_list_ul.appendChild(li);
-    li.appendChild(img);
+//새로 고침
+function ReloadWrapper() {
+  const mainLogo = document.querySelector(".title");
+  function reload() {
+    window.location.reload();
   }
+  mainLogo.addEventListener("click", reload);
 }
-function changePage(e) {
-  if (e.target.id === "left") {
-    main_list_page--;
-  } else {
-    main_list_page++;
+
+//날짜 생성
+function DateWrapper() {
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
+  const day = document.querySelector(".date");
+
+  function makeDate() {
+    const date = new Date();
+    day.innerHTML = `${date.getFullYear()}. ${String(
+      date.getMonth() + 1
+    ).padStart(2, 0)}. ${String(date.getDate()).padStart(2, 0)}. ${
+      week[date.getDay()]
+    }요일`;
   }
+
+  function getDateInterval() {
+    setInterval(makeDate, 60000);
+  }
+  makeDate();
+  getDateInterval();
+}
+
+// 랜덤 그리드 && 버튼
+function GridWrapper() {
+  const PRESS_NUM = 96;
+  const GRID_NUM = 24;
+  const MIN_PAGE = 1;
+  const MAX_PAGE = 4;
+
+  let main_list_page = MIN_PAGE;
+  const main_list_ul = document.querySelector(".main-list-ul");
+  const left_btn = document.getElementById("left-btn");
+  const right_btn = document.getElementById("right-btn");
+
+  const imgIndex = Array(PRESS_NUM)
+    .fill()
+    .map((arr, i) => i);
+
+  function shuffleImgIndex() {
+    return [...imgIndex].sort(() => Math.random() - 0.5);
+  }
+  const shuffledPress = shuffleImgIndex();
+
+  function showMainList() {
+    main_list_ul.innerHTML = "";
+    for (
+      let i = GRID_NUM * (main_list_page - 1);
+      i < GRID_NUM * main_list_page;
+      i++
+    ) {
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+      img.setAttribute(
+        "src",
+        `../images/lightmode-media/asset ${shuffledPress[i]} 1.png`
+      );
+      main_list_ul.appendChild(li);
+      li.appendChild(img);
+    }
+  }
+  function changePage(e) {
+    if (e.target.id === "left") {
+      main_list_page--;
+    } else {
+      main_list_page++;
+    }
+    showMainList();
+    checkPage();
+  }
+
+  function checkPage() {
+    if (main_list_page === MIN_PAGE) left_btn.style.visibility = "hidden";
+    else if (main_list_page === MAX_PAGE) right_btn.style.visibility = "hidden";
+    else {
+      left_btn.style.visibility = "visible";
+      right_btn.style.visibility = "visible";
+    }
+  }
+
+  right_btn.addEventListener("click", (e) => changePage(e));
+  left_btn.addEventListener("click", (e) => changePage(e));
+
   showMainList();
   checkPage();
 }
 
-function checkPage() {
-  if (main_list_page === 1) left_btn.style.visibility = "hidden";
-  else if (main_list_page === 4) right_btn.style.visibility = "hidden";
-  else {
-    left_btn.style.visibility = "visible";
-    right_btn.style.visibility = "visible";
+function rollingNewsBarLeft() {
+  document.querySelector(".prev").classList.remove("prev");
+
+  const current = document.querySelector(".current");
+  current.classList.remove("current");
+  current.classList.add("prev");
+
+  const next = document.querySelector(".next");
+  //마지막 요소 check
+  if (next.nextElementSibling === null) {
+    document
+      .querySelector(".auto-rolling-news ul li:first-child")
+      .classList.add("next");
+  } else {
+    next.nextElementSibling.classList.add("next");
   }
+  next.classList.remove("next");
+  next.classList.add("current");
 }
 
-//이벤트 리스너
-mainLogo.addEventListener("click", reload);
-right_btn.addEventListener("click", (e) => changePage(e));
-left_btn.addEventListener("click", (e) => changePage(e));
+function rollingNewsBarRight() {
+  document.querySelectorAll(".prev")[1].classList.remove("prev");
 
-//default
-makeDate();
-getDateInterval();
-showMainList();
-checkPage();
+  const current = document.querySelectorAll(".current")[1];
+  current.classList.remove("current");
+  current.classList.add("prev");
+
+  const next = document.querySelectorAll(".next")[1];
+  //마지막 요소 check
+  if (next.nextElementSibling === null) {
+    document
+      .querySelectorAll(".auto-rolling-news ul li:first-child")[1]
+      .classList.add("next");
+  } else {
+    next.nextElementSibling.classList.add("next");
+  }
+  next.classList.remove("next");
+  next.classList.add("current");
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.documentElement.setAttribute("color-theme", "light");
+  ReloadWrapper();
+  DateWrapper();
+  GridWrapper();
+  setInterval(rollingNewsBarLeft, 5000);
+  setTimeout(() => {
+    setInterval(rollingNewsBarRight, 5000);
+  }, 1000);
+});
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # fe-newsstand
 
-<img src="./%EA%B8%B0%EB%B3%B8%20html%20%EC%84%A4%EA%B3%84.jpeg">
+html, css, js로 구현한 뉴스스탠드 웹
+
+# 기능 요구사항
