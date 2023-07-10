a6bb7bec029c92cd162cd43ba1e47c9882f248a1
asdf99245 <asdf99245@naver.com>
2023-07-10 17:31:51 +0900

    fix: grid view 기능이 동작하지 않던 현상 수정
    pages와 newsdata 정보를 공유하고 있어야해 grid view에서 button에 event를
    등록하는 로직을 main으로 가져옴

--- app.js
+++ app.js
@@ -5,17 +5,62 @@ import {
   shuffleData,
   getKRLocaleDateString,
 } from "./utils/index.js";
+import { NEWS_COUNT } from "./constants/index.js";
 
-let scheme = "light";
+// TODO: global state로 관리해야할듯
+let theme = "light";
+let pages = 0;
 
+const $prevPageButton = document.querySelector(".container-grid-view_left-btn");
+const $nextPageButton = document.querySelector(
+  ".container-grid-view_right-btn"
+);
 const $headerDate = document.querySelector(".container-header_date");
 
-(async function init() {
+const getSlicedDataFromPage = (data, page, count) => {
+  return data.slice(page * count, (page + 1) * count);
+};
+
+// main
+(async function () {
   $headerDate.innerText = getKRLocaleDateString(new Date());
 
   const newsData = await customFetch("./mocks/news.json", shuffleData);
-  fillNewsContents(newsData);
+  fillNewsContents(getSlicedDataFromPage(newsData, pages, NEWS_COUNT));
 
   const headlineData = await customFetch("./mocks/headline.json");
   startRollingBanner(headlineData);
+
+  const handlePrevButtonClick = () => {
+    const maxPage = Math.floor(newsData.length / NEWS_COUNT) - 1;
+
+    if (pages === maxPage) {
+      $nextPageButton.classList.remove("hidden");
+    }
+
+    pages -= 1;
+    if (pages === 0) {
+      $prevPageButton.classList.add("hidden");
+    }
+
+    fillNewsContents(getSlicedDataFromPage(newsData, pages, NEWS_COUNT));
+  };
+
+  const handleNextButtonClick = () => {
+    const maxPage = Math.floor(newsData.length / NEWS_COUNT) - 1;
+
+    if (pages === 0) {
+      $prevPageButton.classList.remove("hidden");
+    }
+
+    pages += 1;
+    if (pages === maxPage) {
+      $nextPageButton.classList.add("hidden");
+    }
+
+    fillNewsContents(getSlicedDataFromPage(newsData, pages, NEWS_COUNT));
+  };
+
+  $prevPageButton.addEventListener("click", handlePrevButtonClick);
+  $nextPageButton.addEventListener("click", handleNextButtonClick);
 })();
--- scripts/grid-view.js
+++ scripts/grid-view.js
@@ -1,18 +1,8 @@
-import { NEWS_COUNT } from "../constants/index.js";
-
-const $prevPageButton = document.querySelector(".container-grid-view_left-btn");
-const $nextPageButton = document.querySelector(
-  ".container-grid-view_right-btn"
-);
-
-let pages = 0;
-
 export const fillNewsContents = (newsData) => {
   const $gridView = document.querySelector(".grid-view");
   $gridView.innerHTML = "";
-  const startIndex = pages * NEWS_COUNT;
 
-  for (let i = startIndex; i < startIndex + NEWS_COUNT; i++) {
+  for (let i = 0; i < newsData.length; i++) {
     const $li = document.createElement("li");
     $li.className = "grid-cell";
 
@@ -30,32 +20,3 @@ export const fillNewsContents = (newsData) => {
     $gridView.appendChild($li);
   }
 };
-
-const handlePrevButtonClick = () => {
-  const maxPage = Math.floor(newsData.length / NEWS_COUNT) - 1;
-
-  if (pages === maxPage) {
-    $nextPageButton.classList.remove("hidden");
-  }
-
-  if (--pages === 0) {
-    $prevPageButton.classList.add("hidden");
-  }
-  fillNewsContents(newsData);
-};
-
-const handleNextButtonClick = () => {
-  const maxPage = Math.floor(newsData.length / NEWS_COUNT) - 1;
-
-  if (pages === 0) {
-    $prevPageButton.classList.remove("hidden");
-  }
-
-  if (++pages === maxPage) {
-    $nextPageButton.classList.add("hidden");
-  }
-  fillNewsContents(newsData);
-};
-
-$prevPageButton.addEventListener("click", handlePrevButtonClick);
-$nextPageButton.addEventListener("click", handleNextButtonClick);
