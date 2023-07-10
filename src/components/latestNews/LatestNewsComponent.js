ace35774dfdd7a4e5cc8e4c61c8353e6c2c43cf4
bae-sh <b5460881@naver.com>
2023-07-10 23:37:28 +0900

    feat : timer 로직 수정

--- src/components/latestNews/LatestNewsComponent.js
+++ src/components/latestNews/LatestNewsComponent.js
@@ -4,51 +4,43 @@ import Component from '../Component.js';
 export default class LatestNewsComponent extends Component {
   setup() {
     this.state = { ...this.props };
-    this.isRolling = false;
 
-    this.timerOn();
+    setTimeout(() => {
+      this.timerOn();
+    }, this.state.delay * SECOND);
   }
 
   template() {
     const currentConent = this.state.content[this.state.currentIndex];
+    const nextContent = this.state.content[(this.state.currentIndex + 1) % 5];
+
     return `<span class='display-bold14'>${this.state.name}</span>
-            <p class='auto-rolling-animation available-medium14'>${currentConent}</p>
-            `;
+    <div>
+      <div class='auto-rolling-div'>
+        <p class='auto-rolling-animation available-medium14'>${currentConent}</p>
+        <p class='auto-rolling-animation available-medium14'>${nextContent}</p>
+      </div>
+    </div>
+    `;
   }
 
   setEvent() {
-    this.$target.addEventListener('mouseover', () => {
-      const $newsList = this.$target.querySelector('.auto-rolling-animation');
-      const currentOpacity = getComputedStyle($newsList).opacity;
-
-      if (currentOpacity === '1') {
-        $newsList.style.animationPlayState = 'paused';
-        this.timerOff();
-      }
-    });
+    this.$target.addEventListener('mouseover', this.timerOff.bind(this));
 
     this.$target.addEventListener('mouseout', this.timerOn.bind(this));
   }
 
   timerOn() {
-    if (this.isRolling) return;
-    this.isRolling = true;
+    this.timer = setInterval(() => {
+      const $content = this.$target.querySelector('.auto-rolling-div');
+      $content.style.top = '-22px';
 
-    if (this.props.delay === 1) {
-      this.props.delay = 0;
-
-      setTimeout(() => {
-        this.$target.querySelector('.auto-rolling-animation').style.animationPlayState = 'paused';
-        this.timer = setInterval(this.nextNews.bind(this), ROLLING_SECOND);
-      }, SECOND);
-    } else {
-      this.timer = setInterval(this.nextNews.bind(this), ROLLING_SECOND);
-    }
+      setTimeout(this.nextNews.bind(this), SECOND);
+    }, ROLLING_SECOND);
   }
 
   timerOff() {
     clearInterval(this.timer);
-    this.isRolling = false;
   }
 
   nextNews() {
