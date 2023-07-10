f0c6d47a8b355188a1675ba9424ee888ace32e72
bae-sh <b5460881@naver.com>
2023-07-10 16:42:24 +0900

    chore : 한쪽 애니메이션 구현

--- src/components/latestNews/LatestNewsComponent.js
+++ src/components/latestNews/LatestNewsComponent.js
@@ -3,33 +3,45 @@ import Component from '../Component.js';
 export default class LatestNewsComponent extends Component {
   setup() {
     this.state = { ...this.props };
+    this.isRolling = false;
+    this.timerOn();
+    // setTimeout(this.timerOn.bind(this), this.state.delay * 1000);
   }
 
   template() {
+    const currentConent = this.state.content[this.state.currentIndex];
     return `<span>${this.state.name}</span>
-            <p class='auto-rolling-animation'>${this.state.content}</p>`;
+            <p class='auto-rolling-animation'>${currentConent}</p>
+            `;
   }
 
   setEvent() {
-    const $newsList = this.$target.querySelector('.auto-rolling-animation');
-
-    setTimeout(() => {
-      $newsList.style.animationPlayState = 'paused';
-    }, this.state.delay * 1000);
-    setTimeout(() => {
-      $newsList.style.animationPlayState = 'running';
-    }, this.state.delay * 2000);
-
     this.$target.addEventListener('mouseover', () => {
+      const $newsList = this.$target.querySelector('.auto-rolling-animation');
       const currentOpacity = getComputedStyle($newsList).opacity;
 
       if (currentOpacity === '1') {
         $newsList.style.animationPlayState = 'paused';
+        this.timerOff();
       }
     });
 
-    this.$target.addEventListener('mouseout', () => {
-      $newsList.style.animationPlayState = 'running';
-    });
+    this.$target.addEventListener('mouseout', this.timerOn.bind(this));
+  }
+
+  timerOn() {
+    if (this.isRolling) return;
+    this.isRolling = true;
+
+    this.timer = setInterval(this.nextNews.bind(this), 5000);
+  }
+
+  timerOff() {
+    clearInterval(this.timer);
+    this.isRolling = false;
+  }
+
+  nextNews() {
+    this.state.currentIndex = this.setState({ currentIndex: (this.state.currentIndex + 1) % 5 });
   }
 }
