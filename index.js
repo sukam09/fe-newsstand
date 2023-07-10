8867dd152f707f4e62ac5044c7000d75f8cc9d2d
wonho1401 <wonho1401@naver.com>
2023-07-10 18:09:59 +0900

    feat: 자동롤링 기사 추출 및 기사 삽입 추가

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fetchData from "./modules/fetchData.js";
 import { render, makeGridPages } from "./modules/gridView.js";
+import { appendRollingList } from "./modules/rollingNews.js";
 
 // time 설정
 const setTime = () => {
@@ -18,6 +19,25 @@ setTime();
 // json에서 데이터 가져오기
 fetchData().then((data) => {
   const agencies = data.agencies.sort(() => 0.5 - Math.random());
+
+  // 자동 롤링에 들어갈 좌우 기사 5개씩 추출
+  const leftNewsList = agencies.slice(0, 5);
+  const rightNewsList = agencies.slice(5, 10);
+
+  const leftNews = document.querySelector(".recent-news-left");
+  const rightNews = document.querySelector(".recent-news-right");
+  // 5개의 리스트를 ul 내 li로 삽입
+  leftNewsList.map((data) => appendRollingList(leftNews, data));
+  rightNewsList.map((data) => appendRollingList(rightNews, data));
+
+  // 롤링을 위한 prev, current, next class 삽입
+  leftNews.childNodes[0].className = "prev";
+  leftNews.childNodes[1].className = "current";
+  leftNews.childNodes[2].className = "next";
+
+  rightNews.childNodes[0].className = "prev";
+  rightNews.childNodes[1].className = "current";
+  rightNews.childNodes[2].className = "next";
 
   // if grid view
   if (true) {
