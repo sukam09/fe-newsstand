5ad7f9817b483ef1de8b8cfc55cffe1c24910d66
wonho1401 <wonho1401@naver.com>
2023-07-11 00:38:46 +0900

    refactor: refactoring index.js

--- index.js
+++ index.js
@@ -1,47 +1,26 @@
-import fetchData from "./modules/fetchData.js";
-import { render, makeGridPages } from "./modules/gridView.js";
-import { appendRollingList } from "./modules/rollingNews.js";
+import { render, createGridPages } from "./components/gridView.js";
+import { rollNews } from "./components/roller.js";
+import { fetchData } from "./utils/fetch.js";
+import { shuffleData } from "./utils/shuffle.js";
+import { sliceData } from "./utils/slice.js";
+import { setTime } from "./utils/time.js";
 
 // time 설정
-const setTime = () => {
-  const $time = document.querySelector(".time time");
-  const today = new Date();
-  const DAYS = ["일", "월", "화", "수", "목", "금", "토"];
-  $time.setAttribute("datetime", String(today));
-  const format = (num) => String(num).padStart(2, "0");
-  $time.innerText = `${today.getFullYear()}. ${format(
-    today.getMonth() + 1
-  )}. ${format(today.getDate())}. ${DAYS[today.getDay()]}요일`;
-};
-
 setTime();
 
-// json에서 데이터 가져오기
 fetchData().then((data) => {
-  const agencies = data.agencies.sort(() => 0.5 - Math.random());
+  // response로 받은 data shuffle
+  const agencies = shuffleData(data);
 
-  // 자동 롤링에 들어갈 좌우 기사 5개씩 추출
-  const leftNewsList = agencies.slice(0, 5);
-  const rightNewsList = agencies.slice(5, 10);
+  // rolling할 뉴스 slice
+  const slicedData = sliceData(data);
 
-  const leftNews = document.querySelector(".recent-news-left");
-  const rightNews = document.querySelector(".recent-news-right");
-  // 5개의 리스트를 ul 내 li로 삽입
-  leftNewsList.map((data) => appendRollingList(leftNews, data));
-  rightNewsList.map((data) => appendRollingList(rightNews, data));
-
-  // 롤링을 위한 prev, current, next class 삽입
-  leftNews.childNodes[0].className = "prev";
-  leftNews.childNodes[1].className = "current";
-  leftNews.childNodes[2].className = "next";
-
-  rightNews.childNodes[0].className = "prev";
-  rightNews.childNodes[1].className = "current";
-  rightNews.childNodes[2].className = "next";
+  // 뉴스 rolling
+  rollNews(slicedData);
 
   // if grid view
   if (true) {
-    const pages = makeGridPages(agencies);
+    const pages = createGridPages(agencies);
     render(0, pages);
 
     let currentPage = 0;
