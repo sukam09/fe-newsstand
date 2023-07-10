2065e65eda37e5c355bd6b377c3cc28b74d89f2d
yen001004 <yenn001004@gmail.com>
2023-07-10 13:53:53 +0900

    refactor: grid li 태그 createElement로 수정

--- src/utils.js
+++ src/utils.js
@@ -1,81 +1,83 @@
 let page = 1;
 let newsPressData = [];
 
-const shuffleArray = array => array.sort(() => Math.random() - 0.5);
+const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
 
 const initNewsPressData = () => {
-  fetch('../data/press-info.json')
-    .then(response => response.json())
-    .then(jsonData => {
-      newsPressData = shuffleArray(jsonData);
-      showNewsPressItems();
-    });
+    fetch("../data/press-info.json")
+        .then((response) => response.json())
+        .then((jsonData) => {
+            newsPressData = shuffleArray(jsonData);
+            showNewsPressItems();
+        });
 };
 
 const showNewsPressItems = () => {
-  checkShowPageButton(page);
-  const startIndex = 24 * (page - 1);
-  const endIndex = startIndex + 23;
-  const currentNewsPressData = newsPressData.slice(startIndex, endIndex + 1);
+    checkShowPageButton(page);
+    const startIndex = 24 * (page - 1);
+    const endIndex = startIndex + 23;
+    const currentNewsPressData = newsPressData.slice(startIndex, endIndex + 1);
 
-  newsPressItems.forEach((item, index) => {
-    item.innerHTML = '';
+    const newsPressGrid = document.querySelector(".news-press-grid");
+    newsPressGrid.innerHTML = "";
+    currentNewsPressData.forEach((_, index) => {
+        const $li = document.createElement("li");
+        $li.className = "news-press-item";
 
-    const $img = document.createElement('img');
-    const { logo } = currentNewsPressData[index];
-    $img.src = logo;
-    $img.classList.add('press-logo');
+        const $img = document.createElement("img");
+        const { name, logo } = currentNewsPressData[index];
+        $img.src = logo;
+        $img.alt = name;
+        $img.classList.add("news-press-item-logo");
 
-    item.appendChild($img);
-  });
+        $li.appendChild($img);
+        newsPressGrid.appendChild($li);
+    });
 };
-
-const newsPressItems = document.querySelectorAll('.news-press-item');
-
-const titleIcon = document.querySelector('.title-icon');
 
 const handleClickTitleIcon = () => {
-  titleIcon.addEventListener('click', () => {
-    location.reload();
-  });
+    const titleIcon = document.querySelector(".title-icon");
+    titleIcon.addEventListener("click", () => {
+        location.reload();
+    });
 };
 
-const validatePage = page => page >= 1 && page <= 4;
+const validatePage = (page) => page >= 1 && page <= 4;
 
-const prevPageButton = document.querySelector('.left-arrow-button');
+const prevPageButton = document.querySelector(".left-arrow-button");
 
 const handleClickPrevPageButton = () => {
-  prevPageButton.addEventListener('click', () => {
-    if (!validatePage(page - 1)) return;
-    page--;
-    showNewsPressItems();
-  });
+    prevPageButton.addEventListener("click", () => {
+        if (!validatePage(page - 1)) return;
+        page--;
+        showNewsPressItems();
+    });
 };
 
-const nextPageButton = document.querySelector('.right-arrow-button');
+const nextPageButton = document.querySelector(".right-arrow-button");
 
 const handleClickNextPageButton = () => {
-  nextPageButton.addEventListener('click', () => {
-    if (!validatePage(page + 1)) return;
-    page++;
-    showNewsPressItems();
-  });
+    nextPageButton.addEventListener("click", () => {
+        if (!validatePage(page + 1)) return;
+        page++;
+        showNewsPressItems();
+    });
 };
 
-const checkShowPageButton = page => {
-  prevPageButton.classList.remove('disabled');
-  nextPageButton.classList.remove('disabled');
+const checkShowPageButton = (page) => {
+    prevPageButton.classList.remove("disabled");
+    nextPageButton.classList.remove("disabled");
 
-  if (page === 1) prevPageButton.classList.add('disabled');
-  else if (page === 4) nextPageButton.classList.add('disabled');
+    if (page === 1) prevPageButton.classList.add("disabled");
+    else if (page === 4) nextPageButton.classList.add("disabled");
 };
 
 checkShowPageButton(page);
 
 export {
-  initNewsPressData,
-  showNewsPressItems,
-  handleClickTitleIcon,
-  handleClickPrevPageButton,
-  handleClickNextPageButton,
+    initNewsPressData,
+    showNewsPressItems,
+    handleClickTitleIcon,
+    handleClickPrevPageButton,
+    handleClickNextPageButton,
 };
