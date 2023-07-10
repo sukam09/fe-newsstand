eceb90a8d9c4b65459272d65ef9a149a9e47a7f6
박태규 Park Tae Kyu <ptq124@naver.com>
2023-07-10 11:06:49 +0900

    1주차 코드 복사

---
+++ js/core/app.js
@@ -0,0 +1,15 @@
+import { date } from "../utils/date.js";
+import { newsstandSystem } from "../newsstand/newsstandSystem.js";
+
+// 기능
+headerRender();
+mainRender();
+
+function headerRender() {
+  date();
+}
+
+function mainRender() {
+  const newsstanSystem = new newsstandSystem();
+  newsstanSystem.createRandomNewsstand();
+}
