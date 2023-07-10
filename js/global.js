169dea9ee537a0961c3af4d690cf11299cd8a223
jjun990908 <jjun990908@naver.com>
2023-07-10 10:34:08 +0900

    [init] branch 생성

---
+++ js/global.js
@@ -0,0 +1,77 @@
+// 그리드 뷰로 전환
+function changeToGrid() {
+  const list_button = document.getElementsByClassName("list_button")[0];
+  const grid_button = document.getElementsByClassName("grid_button")[0];
+  const list_container = document.getElementsByClassName("list_container")[0];
+  const grid_container =
+    document.getElementsByClassName("grid_container")[now_grid_page];
+  const left_list_button =
+    document.getElementsByClassName("left_list_button")[0];
+  const right_list_button =
+    document.getElementsByClassName("right_list_button")[0];
+  const left_grid_button =
+    document.getElementsByClassName("left_grid_button")[0];
+  const right_grid_button =
+    document.getElementsByClassName("right_grid_button")[0];
+
+  // 버튼 색상 바꾸기
+  list_button.src = "./assets/icons/list_off.png";
+  grid_button.src = "./assets/icons/grid_on.png";
+
+  // 그리드, 리스트 전환
+  list_container.style.display = "none";
+  grid_container.style.display = "grid";
+
+  // 버튼 바꾸기
+  left_grid_button.style.display = "block";
+  right_grid_button.style.display = "block";
+  left_list_button.style.display = "none";
+  right_list_button.style.display = "none";
+  showGridPageButton();
+}
+
+// 리스트 뷰로 변환
+function changeToList() {
+  const list_button = document.getElementsByClassName("list_button")[0];
+  const grid_button = document.getElementsByClassName("grid_button")[0];
+  const list_container = document.getElementsByClassName("list_container")[0];
+  const grid_container =
+    document.getElementsByClassName("grid_container")[now_grid_page];
+  const left_list_button =
+    document.getElementsByClassName("left_list_button")[0];
+  const right_list_button =
+    document.getElementsByClassName("right_list_button")[0];
+  const left_grid_button =
+    document.getElementsByClassName("left_grid_button")[0];
+  const right_grid_button =
+    document.getElementsByClassName("right_grid_button")[0];
+
+  // 버튼 색상 바꾸기
+  list_button.src = "./assets/icons/list_on.png";
+  grid_button.src = "./assets/icons/grid_off.png";
+
+  // 그리드, 리스트 전환
+  list_container.style.display = "block";
+  grid_container.style.display = "none";
+
+  // 버튼 바꾸기
+  left_grid_button.style.display = "none";
+  right_grid_button.style.display = "none";
+  left_list_button.style.display = "block";
+  right_list_button.style.display = "block";
+}
+
+// 오늘 날짜 update
+function updateDate() {
+  let today = new Date();
+  const dateHtml = document.getElementsByClassName("date")[0];
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    weekday: "long",
+  };
+
+  today = today.toLocaleDateString("ko-KR", options);
+  dateHtml.innerHTML = today;
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,98 @@
+# FE-newsstand
 
+## 설계
+
+![](./images/%EC%84%A4%EA%B3%84.png)
+
+## 구현 내용
+
+### **html**
+
+```html
+<body>
+  <section>
+    <!-- 제목, 로고, 날짜 영역 -->
+    <div>
+      <!-- 제목, 로고 -->
+    </div>
+  </section>
+  <section>
+    <!-- 부제목 영역 -->
+    <div>
+      <!-- 부제목 -->
+    </div>
+    <div>
+      <!-- 부제목 -->
+    </div>
+  </section>
+  <section>
+    <!-- 그리드 영역 -->
+    <div>
+      <!-- 제목 -->
+      <!-- 버튼 -->
+    </div>
+    <div>
+      <!-- 버튼 -->
+      <!-- 그리드 -->
+      <!-- 버튼 -->
+    </div>
+  </section>
+</body>
+```
+
+### **style**
+
+- flex
+
+  공통적으로 사용하는 css를 정의하여 재사용성을 높였습니다.
+
+  ```css
+  .flex_row {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .flex_column {
+    display: flex;
+    flex-direction: column;
+  }
+  ```
+
+- grid
+
+  `display: grid` 속성을 활용하여 grid를 생성하였습니다.
+  `.grid_container` 의 **좌측과 상단** border를 설정하고,  
+   `.grid_container > li` 의 **우측과 하단** border를 설정하여  
+   border가 겹치는 현상을 해결하였습니다.
+
+  ```css
+  .grid_container {
+    display: grid;
+    border-top: solid 1px #d2dae0;
+    border-left: solid 1px #d2dae0;
+    grid-template-rows: repeat(4, 1fr);
+    grid-template-columns: repeat(6, 1fr);
+    ...;
+  }
+
+  .grid_item {
+    border-right: solid 1px #d2dae0;
+    border-bottom: solid 1px #d2dae0;
+    ...;
+  }
+  ```
+
+- 애니메이션
+
+### **기능**
+
+-[] 오늘 날짜 가져오기
+
+-[] 그리드 뷰, 리스트 뷰 전환
+
+-[] 그리드 아이템 랜덤 배치
+
+-[] 그리드 페이지 전환
+
+###
