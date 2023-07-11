11be9b1fc83e91af0b540fb6100758d5b2b83972
DaeHee99 <mokday99@gmail.com>
2023-07-11 13:11:43 +0900

    [Feat] 리스트 보기 기본 UI

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@ import { setHeaderDate } from "./headerDate.js";
 import { setRolling } from "./rolling.js";
 import { renderNewspaper } from "./newspaper.js";
 import { setPageButton } from "./pageButton.js";
+import { setViewerEvent } from "./newsViewer.js";
 
 const main = () => {
   setHeaderDate();
   setRolling();
   renderNewspaper(constants.MIN_PAGE, constants.LIGHT_MODE);
   setPageButton();
+  setViewerEvent();
 };
 
 main();
