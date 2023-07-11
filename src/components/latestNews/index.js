0e345961f660a23742f3535277a5213dd4bc2d31
lee-yun-pyo <yun2193@hanmail.net>
2023-07-11 13:59:12 +0900

    feat: 롤링 애니메이션 구현

--- src/components/latestNews/index.js
+++ src/components/latestNews/index.js
@@ -1,30 +1,70 @@
 export default class LatestNews {
   constructor() {
-    this.$wrapper = document.createElement('div');
-    this.$wrapper.className = 'latest-main-news';
+    this.$wrapper = document.createElement("div");
+    this.$wrapper.className = "latest-main-news";
+
+    this.LATEST_NEWS_COUNT = 6;
+    this.ROLLING_SPEED = 2000;
 
     this.render();
     return this.$wrapper;
   }
 
   render() {
-    this.$wrapper.appendChild(this.createLatestNewsComponent('연합뉴스', '[1보] !'));
-    this.$wrapper.appendChild(this.createLatestNewsComponent('연합뉴스', '[1보] !'));
+    this.$wrapper.appendChild(this.createLatesNewsContainer("left"));
+    this.$wrapper.appendChild(this.createLatesNewsContainer("right"));
+  }
+
+  createLatesNewsContainer(location) {
+    const $container = document.createElement("div");
+    const $hiddenCompoent = document.createElement("div");
+    const $wrapper = document.createElement("div");
+    $container.className = "latest--container";
+    $hiddenCompoent.className = "text--wrapper";
+    $wrapper.className = "auto-rolling-news";
+
+    $wrapper.appendChild(this.createLatestNewsComponent("연합뉴스", `[1보] 1`));
+    $wrapper.appendChild(this.createLatestNewsComponent("연합뉴스", `[1보] 2`));
+    $wrapper.appendChild(this.createLatestNewsComponent("연합뉴스", `[1보] 3`));
+    $wrapper.appendChild(this.createLatestNewsComponent("연합뉴스", `[1보] 4`));
+    $wrapper.appendChild(this.createLatestNewsComponent("연합뉴스", `[1보] 5`));
+    $wrapper.appendChild(this.createLatestNewsComponent("연합뉴스", `[1보] 1`));
+
+    this.handleRolling($wrapper);
+
+    $hiddenCompoent.appendChild($wrapper);
+    $container.appendChild($hiddenCompoent);
+
+    return $container;
   }
 
   createLatestNewsComponent(name, content) {
-    const $component = document.createElement('div');
-    $component.className = 'auto-rolling-news';
+    const $component = document.createElement("div");
+    $component.className = "latest--content";
 
-    const $newsName = document.createElement('span');
-    const $newsContent = document.createElement('p');
+    const $newsName = document.createElement("span");
+    const $newsContent = document.createElement("p");
 
     $newsName.innerText = name;
-    $newsContent.innerText = '[1보] 김기현·안철수·천하람·황교안, 與전대 본경선 진출';
+    $newsContent.innerText = content;
 
     $component.appendChild($newsName);
     $component.appendChild($newsContent);
 
     return $component;
+  }
+
+  handleRolling(wrapper) {
+    let order = 1;
+
+    setInterval(() => {
+      if (order > this.LATEST_NEWS_COUNT) {
+        wrapper.classList.remove("play" + (order - 1));
+        order = 1;
+      }
+      wrapper.classList.add("play" + order);
+      wrapper.classList.remove("play" + (order - 1));
+      order += 1;
+    }, this.ROLLING_SPEED);
   }
 }
