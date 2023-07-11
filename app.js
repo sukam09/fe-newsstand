9e64349b502d6bddd7c10d1b6f66115a8a10bc80
asdf99245 <asdf99245@naver.com>
2023-07-11 11:59:57 +0900

    feat: grid, list view toggle 기능 구현

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ import {
   shuffleData,
   getKRLocaleDateString,
 } from "./utils/index.js";
-import { NEWS_COUNT } from "./constants/index.js";
+import { NEWS_COUNT, VIEW_TYPE } from "./constants/index.js";
 
 // TODO: global state로 관리해야할듯
 let theme = "light";
 let pages = 0;
 
-const $prevPageButton = document.querySelector(".container-main_left-btn");
-const $nextPageButton = document.querySelector(".container-main_right-btn");
+const $containerMain = document.querySelector(".container-main");
+const $gridView = $containerMain.querySelector(".grid-view");
+const $listView = $containerMain.querySelector(".list-view");
+const $prevPageButton = $containerMain.querySelector(
+  ".container-main_left-btn"
+);
+const $nextPageButton = $containerMain.querySelector(
+  ".container-main_right-btn"
+);
 const $headerDate = document.querySelector(".container-header_date");
+const $mainNav = document.querySelector(".main-nav");
+const $mainNavViewerButtons = $mainNav.querySelectorAll(
+  ".main-nav_viewer > button"
+);
 
 const getSlicedDataFromPage = (data, page, count) => {
   return data.slice(page * count, (page + 1) * count);
@@ -59,6 +70,30 @@ const getSlicedDataFromPage = (data, page, count) => {
     fillNewsContents(getSlicedDataFromPage(newsData, pages, NEWS_COUNT));
   };
 
+  const handleViewerButtonClick = (e) => {
+    const viewType = e.currentTarget.dataset.view;
+
+    $mainNavViewerButtons.forEach(($button) => {
+      if (viewType !== $button.dataset.view) {
+        $button.classList.remove("main-nav_viewer--selected");
+      } else {
+        $button.classList.add("main-nav_viewer--selected");
+      }
+    });
+
+    if (viewType === VIEW_TYPE.GRID) {
+      $gridView.classList.remove("hidden");
+      $listView.classList.add("hidden");
+    } else {
+      $gridView.classList.add("hidden");
+      $listView.classList.remove("hidden");
+    }
+  };
+
+  console.log($mainNavViewerButtons);
   $prevPageButton.addEventListener("click", handlePrevButtonClick);
   $nextPageButton.addEventListener("click", handleNextButtonClick);
+  $mainNavViewerButtons.forEach(($button) => {
+    $button.addEventListener("click", handleViewerButtonClick);
+  });
 })();
