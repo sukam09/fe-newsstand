cdeba9759f084a35d5b1fb25c38a9bd40fc2062a
yen001004 <yenn001004@gmail.com>
2023-07-11 13:26:03 +0900

    feat: 리스트 그리드 뷰 전환

--- src/news-display/setViewMode.js
+++ src/news-display/setViewMode.js
@@ -1,15 +1,23 @@
 const gridIcon = document.querySelector(".news-display-grid-button");
 const listIcon = document.querySelector(".news-display-list-button");
+const gridView = document.querySelector(".news-press-grid-view");
+const listView = document.querySelector(".news-press-list-view");
 
 const handleClickViewIcon = () => {
     gridIcon.addEventListener("click", () => {
         listIcon.classList.remove("news-display-active");
         gridIcon.classList.add("news-display-active");
+
+        gridView.classList.remove("hidden");
+        listView.classList.add("hidden");
     });
 
     listIcon.addEventListener("click", () => {
         gridIcon.classList.remove("news-display-active");
         listIcon.classList.add("news-display-active");
+
+        listView.classList.remove("hidden");
+        gridView.classList.add("hidden");
     });
 };
 
