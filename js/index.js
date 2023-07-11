8bb89d3c322572b673abfc33afb5002e92b4f62f
WongilK <wwwkim99@naver.com>
2023-07-11 13:53:24 +0900

    [FEAT] 프로그래스 바 구현

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ function GridWrapper() {
   const MAX_PAGE = 4;
 
   let main_list_page = MIN_PAGE;
-  const main_list_ul = document.querySelector(".main-list-ul");
+  const main_list_ul = document.querySelector(".grid-view-ul");
   const left_btn = document.getElementById("left-btn");
   const right_btn = document.getElementById("right-btn");
 
@@ -92,70 +92,81 @@ function GridWrapper() {
 }
 
 //롤링 뉴스바
-let leftInterval, rightInterval;
-function addEvent() {
-  const newsList = document.querySelectorAll(".auto-rolling-news ul li");
-  newsList.forEach((news) => {
-    // 왼쪽, 오른쪽 뉴스 롤링 바 구분
-    if (news.parentElement.id === "left-rolling") {
-      news.addEventListener("mouseover", () => {
-        clearInterval(leftInterval);
-      });
-      news.addEventListener("mouseout", () => {
-        leftInterval = setInterval(rollingNewsBarLeft, 5000);
-      });
+function rollingNewsBarWrapper() {
+  let leftInterval, rightInterval;
+  function addEventToRolling() {
+    const newsList = document.querySelectorAll(".auto-rolling-news ul li");
+    newsList.forEach((news) => {
+      // 왼쪽, 오른쪽 뉴스 롤링 바 구분
+      if (news.parentElement.id === "left-rolling") {
+        news.addEventListener("mouseover", () => {
+          clearInterval(leftInterval);
+        });
+        news.addEventListener("mouseout", () => {
+          leftInterval = setInterval(rollingNewsBarLeft, 5000);
+        });
+      } else {
+        news.addEventListener("mouseover", () => {
+          clearInterval(rightInterval);
+        });
+        news.addEventListener("mouseout", () => {
+          rightInterval = setInterval(rollingNewsBarRight, 5000);
+        });
+      }
+    });
+  }
+  function rollingNewsBarLeft() {
+    document.querySelector(".prev").classList.remove("prev");
+
+    const current = document.querySelector(".current");
+    current.classList.remove("current");
+    current.classList.add("prev");
+
+    const next = document.querySelector(".next");
+    //마지막 요소 check
+    if (next.nextElementSibling === null) {
+      document
+        .querySelector(".auto-rolling-news ul li:first-child")
+        .classList.add("next");
     } else {
-      news.addEventListener("mouseover", () => {
-        clearInterval(rightInterval);
-      });
-      news.addEventListener("mouseout", () => {
-        rightInterval = setInterval(rollingNewsBarRight, 5000);
-      });
+      next.nextElementSibling.classList.add("next");
     }
-  });
-}
-function rollingNewsBarLeft() {
-  document.querySelector(".prev").classList.remove("prev");
-
-  const current = document.querySelector(".current");
-  current.classList.remove("current");
-  current.classList.add("prev");
-
-  const next = document.querySelector(".next");
-  //마지막 요소 check
-  if (next.nextElementSibling === null) {
-    document
-      .querySelector(".auto-rolling-news ul li:first-child")
-      .classList.add("next");
-  } else {
-    next.nextElementSibling.classList.add("next");
+    next.classList.remove("next");
+    next.classList.add("current");
   }
-  next.classList.remove("next");
-  next.classList.add("current");
-}
 
-function rollingNewsBarRight() {
-  document.querySelectorAll(".prev")[1].classList.remove("prev");
+  function rollingNewsBarRight() {
+    document.querySelectorAll(".prev")[1].classList.remove("prev");
 
-  const current = document.querySelectorAll(".current")[1];
-  current.classList.remove("current");
-  current.classList.add("prev");
+    const current = document.querySelectorAll(".current")[1];
+    current.classList.remove("current");
+    current.classList.add("prev");
 
-  const next = document.querySelectorAll(".next")[1];
-  //마지막 요소 check
-  if (next.nextElementSibling === null) {
-    document
-      .querySelectorAll(".auto-rolling-news ul li:first-child")[1]
-      .classList.add("next");
-  } else {
-    next.nextElementSibling.classList.add("next");
+    const next = document.querySelectorAll(".next")[1];
+    //마지막 요소 check
+    if (next.nextElementSibling === null) {
+      document
+        .querySelectorAll(".auto-rolling-news ul li:first-child")[1]
+        .classList.add("next");
+    } else {
+      next.nextElementSibling.classList.add("next");
+    }
+    next.classList.remove("next");
+    next.classList.add("current");
   }
-  next.classList.remove("next");
-  next.classList.add("current");
+
+  addEventToRolling();
+  leftInterval = setInterval(rollingNewsBarLeft, 5000);
+  setTimeout(() => {
+    rightInterval = setInterval(rollingNewsBarRight, 5000);
+  }, 1000);
 }
 
 //뷰 타입 변경
 const view_type = document.querySelectorAll(".viewer-btn button");
+const grid_view = document.querySelector(".grid-view");
+const list_view = document.querySelector(".list-view");
+list_view.style.display = "none";
 view_type[0].addEventListener("click", () => {
   view_type[0].innerHTML = `<img
   src="../images/icon/List-view-checked.svg"
@@ -165,6 +176,8 @@ view_type[0].addEventListener("click", () => {
   src="../images/icon/Grid-view-unchecked.svg"
   alt="images"
 />`;
+  grid_view.style.display = "none";
+  list_view.style.display = "block";
 });
 view_type[1].addEventListener("click", () => {
   view_type[0].innerHTML = `<img
@@ -175,16 +188,16 @@ view_type[1].addEventListener("click", () => {
   src="../images/icon/Grid-view-checked.svg"
   alt="images"
 />`;
+  list_view.style.display = "none";
+  grid_view.style.display = "block";
 });
+
+//todo : 카테고리 애니메이션
 
 window.addEventListener("DOMContentLoaded", () => {
   document.documentElement.setAttribute("color-theme", "light");
   ReloadWrapper();
   DateWrapper();
-  // GridWrapper();
-  addEvent();
-  leftInterval = setInterval(rollingNewsBarLeft, 5000);
-  setTimeout(() => {
-    rightInterval = setInterval(rollingNewsBarRight, 5000);
-  }, 1000);
+  rollingNewsBarWrapper();
+  GridWrapper();
 });
