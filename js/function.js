331436c5f0e02c52c5f51ca5dba2988a3dff9040
choiseona <tjsdk617@naver.com>
2023-07-12 02:18:25 +0900

    Feat: JSON fetch로 언론사 사진 띄우기

--- js/function.js
+++ js/function.js
@@ -28,31 +28,33 @@ function handleClickLogo() {
 function initImgs() {
   const $sectionNewsList = document.querySelector('.press-lists');
   const newsList = [];
-  const imgId = [];
   const page = [[], [], [], []];
-  const PRESS_CNT = 96;
   const ONE_PRESS_CNT = 24;
   const FIRST_PAGE_IDX = 0;
 
-  for (let i = 0; i < PRESS_CNT; i++) { //고차함수로 바꾸기
-    imgId.push(i+1);
+  async function fetchPressInfos() {
+    const response = await fetch("./assets/data/pressInfo.json");
+    const jsonData = await response.json();
+    return jsonData.pressInfoArr;
   }
 
-  imgId.forEach(arr => {
-    newsList.push({ "id": arr });
+  fetchPressInfos().then(pressInfoArr => {
+    pressInfoArr.forEach(pressInfo => {
+      newsList.push({"id":pressInfo["id"]});
+    })
+
+    const shuffledArray = [...newsList].sort(() => Math.random() - 0.5);
+    shuffledArray.forEach((pressId, idx) => {
+      const pageIndex = Math.floor(idx / ONE_PRESS_CNT);
+      page[pageIndex].push(pressId);
+    })
+
+    $sectionNewsList.innerHTML = `
+      ${page[FIRST_PAGE_IDX].map(arr => `<li><img class="pointer" src="./assets/logo/light/img${arr["id"]}.svg"</li>`).join('')};
+    `
   })
 
-  const shuffledArray = [...newsList].sort(() => Math.random() - 0.5);
-
-  shuffledArray.forEach((arr, idx) => {
-    const pageIndex = Math.floor(idx / ONE_PRESS_CNT);
-    page[pageIndex].push(arr);
-  })
-
-  $sectionNewsList.innerHTML = `
-    ${page[FIRST_PAGE_IDX].map(arr => `<li><img class="pointer" src="./assets/logo/light/img${arr["id"]}.svg"</li>`).join('')};
-  `
-  turnPage(page);
+   turnPage(page);
 }
 
 /**
