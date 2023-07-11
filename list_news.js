1ef48b5444b3dc8c96cf105c1fbcd9de09c086ca
ParkByungGwon <parkbyunggwon@ParkByungGwonui-MacBookPro.local>
2023-07-11 17:58:52 +0900

    feat: list_view 동적 페이지 구현

--- list_news.js
+++ list_news.js
@@ -1,3 +1,5 @@
+import { fetchNewsData } from "./utils.js";
+
 const category = [
     "종합/경제",
     "방송/통신",
@@ -8,17 +10,22 @@ const category = [
     "지역",
 ];
 
-let currentCategory = "영자지";
-let current = 50;
+let currentCategory = "종합/경제";
+let currentPage = 0;
+let current = 10;
+let count = 23;
 let max = 100;
 
-function rendListNews() {
+function rendListNews(data, category, page) {
     const news_data_container = document.querySelector(".main_news_container");
+
+    // data.category = category search
+    const filteredData = data.filter((item) => item.category === category);
 
     news_data_container.innerHTML = "";
     createMainNav(news_data_container);
-    createMainContent(news_data_container);
-    changeCategory();
+    createMainContent(news_data_container, filteredData, 0);
+    changeCategory(data);
 }
 
 function createMainNav(container) {
@@ -30,11 +37,11 @@ function createMainNav(container) {
         if (item === currentCategory) {
             ul.innerHTML += `<li class="main_nav_title">
             <progress
-                value="50"
+                value="${count}"
                 min="0"
                 max="100"></progress>
             <span>${currentCategory}</span>
-            <span>${current}/${max}</span></li>`;
+            <span>${current} / ${max}</span></li>`;
         } else {
             ul.innerHTML += `<li class="main_nav_item">${item}</li>`;
         }
@@ -43,56 +50,58 @@ function createMainNav(container) {
     container.appendChild(nav);
 }
 
-function changeCategory() {
+function changeCategory(data) {
     const main_nav_item = document.querySelectorAll(".main_nav_item");
     main_nav_item.forEach((item) => {
         item.addEventListener("click", () => {
             currentCategory = item.innerText;
-            console.log(currentCategory);
-            rendListNews();
+            rendListNews(data, currentCategory, 0);
         });
     });
 }
 
-function createMainContent(container) {
+function createMainContent(container, data, page) {
     const main_content = document.createElement("div");
     main_content.classList.add("main_content");
 
+    const ul = document.createElement("ul");
+    ul.classList.add("content_body_contents");
+
+    data[page].contents.forEach((item) => {
+        ul.innerHTML += `
+        <li class="content_body_contents_item"><span>${item}</span></li>
+        `;
+    });
+
+    const li = document.createElement("li");
+    li.classList.add("content_body_contents_item", "contents_press");
+    li.innerHTML = `<span>${data[page].press_name}에서 직접 편집한 뉴스입니다.</span>`;
+    ul.appendChild(li);
+    const list_news = ul.outerHTML;
+
     main_content.innerHTML = `
     <div class="content_header">
-        <img src="./public/images/sbs.png" class="content_press" />
-        <p class="content_edit">2023.07.11 09:09 편집</p>
-        <img src="./public/icons/subscribe_btn.png" class="content_subscribe" />
+        <img src="${data[page].press_url}" class="content_press" />
+        <p class="content_edit">${data[page].last_edit} 편집</p>
+        <img src="./assets/icons/subscribe_btn.png" class="content_subscribe" />
     </div>
     <div class="content_body">
         <div class="content_body_title">
-            <img src=./public/pictures/Thumbnail.png class="content_picture" />
+            <img src="${data[page].main_url}" class="content_picture" />
             <p class="content_title">또 국민연금의 몽니…현대百 지주사 불발</p>
         </div>
-        <ul class="content_body_contents">
-            <li class="content_body_contents_item">
-            <span>"위스키 사려고 이틀 전부터 줄 섰어요"</span></li>
-            <li class="content_body_contents_item">
-            <span>'방시혁 제국'이냐 '카카오 왕국'이냐…K엔터 누가 거머쥘까</span></li>
-            <li class="content_body_contents_item">
-            <span>사용후핵연료 저장시설 포화…이대론 7년 뒤 원전 멈춘다</span></li>
-            <li class="content_body_contents_item">
-            <span>[단독] 원희룡 "해외건설 근로자 소득공제 월 500만원으로 상향할 것"</span></li>
-            <li class="content_body_contents_item">
-            <span>태평양에는 우영우의 고래만 있는게 아니었다 [로비의 그림]</span></li>
-            <li class="content_body_contents_item">
-            <span>LG엔솔, 폴란드 자동차산업협회 가입…“유럽서 목소리 키운다”</span></li>
-            <li class="content_body_contents_item contents_press"><span>서울 경제 언론사에서 직접 편집한 뉴스입니다.</span></li>
-        </ul>
-    </div>
+        ${list_news}
+    </div>`;
 
-    `;
     container.appendChild(main_content);
 }
 
 function initListNews() {
-    rendListNews();
-    changeCategory();
+    const promise_data = fetchNewsData();
+
+    promise_data.then((data) => {
+        rendListNews(data, currentCategory, 0);
+    });
 }
 
 function setProgress() {}
