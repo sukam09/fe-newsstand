829724aa9a51cbc08ce697105d23ebcdc6b327bb
sukam09 <sukam09@naver.com>
2023-07-11 17:47:40 +0900

    refactor: setInterval 함수 setTimeout 재귀로 구현 방식 변경

--- src/Headline.js
+++ src/Headline.js
@@ -15,29 +15,32 @@ export default function Headline({ $target, initialState }) {
   };
 
   const setHeadline = () => {
+    // TODO: setState 지우고 transition과 class 바꿔주는 방식으로 구현
     this.setState({
       ...this.state,
       index: (this.state.index + 1) % HEADLINE_NUMBERS,
     });
   };
 
-  const setTimer = () => {
-    return setInterval(() => {
+  const setTimer = (callback, interval) => {
+    return setTimeout(() => {
       setHeadline();
-    }, HEADLINE_ROLLING_DELAY);
+      setTimer(callback, interval);
+    }, interval);
   };
 
-  // this.timer = setTimer();
-
   // setTimeout 함수에 delay를 0으로 걸어주는 것은 엄밀하게 0초가 아니기 때문에 offset이 0이 아닐 경우에만 setTimeout을 적용
-  this.timer = this.state.offset === 0 ? setTimer() : setTimeout(setTimer, this.state.offset);
+  this.timer =
+    this.state.offset === 0
+      ? setTimer(setHeadline, HEADLINE_ROLLING_DELAY)
+      : setTimeout(() => setTimer(setHeadline, HEADLINE_ROLLING_DELAY), this.state.offset);
 
   const handleMouseEnter = () => {
-    clearInterval(this.timer);
+    clearTimeout(this.timer);
   };
 
   const handleMouseLeave = () => {
-    this.timer = setTimer();
+    this.timer = setTimer(setHeadline, HEADLINE_ROLLING_DELAY);
   };
 
   this.render = () => {
