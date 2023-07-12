8fffab311bb363ad4652fa045f0cf9c9b0d08ab2
1lsang <ilsangpark.98@gmail.com>
2023-07-12 17:10:53 +0900

    Feat: Add createElement functions

--- src/lib/dom.js
+++ src/lib/dom.js
@@ -1,8 +1,37 @@
 /**
+ * html template literal을 parameter로 받아 HTMLElement를 생성합니다.
+ * @param {string} html
+ * @returns {HTMLElement}
+ */
+function createElement(html) {
+    try {
+        const $template = document.createElement("div");
+        $template.innerHTML = html;
+        if ($template.children.length !== 1)
+            throw new Error("The createElement function must get an HTML string that has only one tag.");
+        const $tag = $template.firstChild;
+        return $tag;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+/**
+ * html template literal을 parameter로 받아 HTMLElement 배열을 생성합니다.
+ * @param {string} html
+ * @returns {HTMLElement[]}
+ */
+function createElements(html) {
+    const $template = document.createElement("div");
+    $template.innerHTML = html;
+    const tags = $template.children;
+    return [...tags];
+}
+/**
  * 요소를 DOM에서 제거합니다.
  * @param {HTMLElement} $element
  */
 function removeElement($element) {
     $element.remove();
 }
-export { removeElement };
+export { createElement, createElements, removeElement };
