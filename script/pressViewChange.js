d134a42c8def9d2f18926dc52c68d5aa9585604a
meanz1 <zndnfjqbd1206@gmail.com>
2023-07-12 18:32:37 +0900

    [feat] 뉴스 컨텐츠 카테고리바 생성(html)

--- script/pressViewChange.js
+++ script/pressViewChange.js
@@ -1,26 +1,25 @@
 function pressViewChange() {
     const pressListView = document.getElementById('pressbar-icon-list-view');
-    const pressGridview = document.getElementById('pressbar-icon-grid-view');
+    const pressGridView = document.getElementById('pressbar-icon-grid-view');
 
     pressListView.addEventListener('click', () => {
         pressListView.childNodes[1].setAttribute("fill", "#4362D0");
-        pressGridview.childNodes[1].setAttribute("fill", "#879298");
+        pressGridView.childNodes[1].setAttribute("fill", "#879298");
         showPressView("list");
     });
 
-    pressGridview.addEventListener('click', () => {
+    pressGridView.addEventListener('click', () => {
         pressListView.childNodes[1].setAttribute("fill", "#879298");
-        pressGridview.childNodes[1].setAttribute("fill", "#4362D0");
+        pressGridView.childNodes[1].setAttribute("fill", "#4362D0");
         showPressView("grid");
     });
 }
 
 function showPressView(status) {
-    console.log("hdhdhd");
     const pressContentContainer = document.getElementsByClassName("press-content-view");
     const pressContentListView = document.getElementsByClassName("press-content-list-view");
 
-    if(status=="list") {
+    if(status === "list") {
         pressContentContainer[0].style.display = "none";
         pressContentListView[0].style.display = "block";
     }
