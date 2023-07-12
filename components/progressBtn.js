a7e68d1ff07a9eedc48c4d3a6e9969269715dee0
ori5r2 <96720326+ori5r2@users.noreply.github.com>
2023-07-12 17:56:52 +0900

    feat: 프로그레스바 토글

--- components/progressBtn.js
+++ components/progressBtn.js
@@ -1,29 +1,43 @@
 import * as element from "./common.js";
 
-let isProgressBar = false;
+let progressIdx = 0,
+    oldProgressIdx = 0;
 
-async function changeProgress() {
-    isProgressBar = !isProgressBar;
+async function changeProgressIdx(idx) {
+    oldProgressIdx = progressIdx;
+    progressIdx = idx;
 }
 
-function clickProgressBtn() {
-    const list_view_btn = document.querySelector(".list-view-btn");
-    const tab_item = list_view_btn.querySelector(".tab-item");
-    const btn_tab_item = list_view_btn.querySelector(".btn-tab-item");
+function initProgressBar() {
+    const list_view_btn = document.querySelectorAll(".list-view-btn");
+    const tab_item = list_view_btn[0].querySelector(".tab-item");
+    const btn_tab_item = list_view_btn[0].querySelector(".btn-tab-item");
 
-    list_view_btn.addEventListener("click", () => {
-        changeProgress().then(() => {
-            if (isProgressBar) {
-                tab_item.style.display = "none";
-                btn_tab_item.style.display = "flex";
-            } else {
-                tab_item.style.display = "flex";
-                btn_tab_item.style.display = "none";
-            }
+    tab_item.style.display = "none";
+    btn_tab_item.style.display = "flex";
+}
+
+function clickProgressBar() {
+    const list_view_btn = document.querySelectorAll(".list-view-btn");
+    list_view_btn.forEach((elem, idx) => {
+        elem.addEventListener("click", () => {
+            changeProgressIdx(idx).then(() => {
+                const old_tab_item = list_view_btn[oldProgressIdx].querySelector(".tab-item");
+                const old_btn_tab_item = list_view_btn[oldProgressIdx].querySelector(".btn-tab-item");
+                const new_tab_item = list_view_btn[progressIdx].querySelector(".tab-item");
+                const new_btn_tab_item = list_view_btn[progressIdx].querySelector(".btn-tab-item");
+
+                old_tab_item.style.display = "flex";
+                old_btn_tab_item.style.display = "none";
+
+                new_tab_item.style.display = "none";
+                new_btn_tab_item.style.display = "flex";
+            });
         });
     });
 }
 
 export function rederProgressBtn() {
-    clickProgressBtn();
+    initProgressBar();
+    clickProgressBar();
 }
