b8add470dc0e924e136c9f35e126a7423513010e
오지은 <0909oje@naver.com>
2023-07-12 17:30:14 +0900

    feat: 카테고리 바 구현

--- modules/list.js
+++ modules/list.js
@@ -3,14 +3,41 @@ import { getJSON } from "./data.js";
 let listInfo;
 let lightMediaInfo;
 let darkMediaInfo;
+let categoryInfo;
+
+const setCategoryBar = () => {
+  const $categoryBar = document.querySelector(".news-list_category");
+  categoryInfo.forEach((cate, idx) => {
+    let cateHTML;
+    // 카테고리 이름 넣는 건 디폴트
+    const $li = document.createElement("li");
+    if (idx === 0) {
+      $li.classList.add("category_selected");
+      cateHTML = `
+          <p>${cate.name}</p>
+          <div>
+            <p>${idx + 1}</p>
+            <p> &nbsp; / ${cate.mediaIdList.length}</p>
+          </div>
+          <div class="progress_bar"/>
+      `;
+    } else {
+      $li.classList.add("category_unselected");
+      cateHTML = `
+          <p>${cate.name}</p>
+          <div></div>
+      `;
+    }
+    $li.innerHTML = cateHTML;
+    $categoryBar.append($li);
+  });
+};
 
 const setListView = (mediaId) => {
   const nowMedia = listInfo[mediaId];
   const $logo = document.querySelector(".news-list_media_header img");
   const $date = document.querySelector(".news-list_media_header p");
   const $mainTitle = document.querySelector(".news-list_media_content_main p");
-
-  console.log(lightMediaInfo[mediaId].src);
 
   $logo.src = lightMediaInfo[mediaId].src;
   $date.innerText = `${nowMedia.date} 편집`;
@@ -26,9 +53,17 @@ const setListView = (mediaId) => {
 };
 
 const getListInfo = async () => {
+  // Promise.all()
   lightMediaInfo = await getJSON("/assets/light-media.json");
   darkMediaInfo = await getJSON("/assets/dark-media.json");
   listInfo = await getJSON("/assets/media-content.json");
+  categoryInfo = await getJSON("/assets/category-info.json");
 };
 
-export { getListInfo, setListView };
+(async function initListView() {
+  await getListInfo();
+  setCategoryBar();
+  setListView(categoryInfo[0].mediaIdList[0]);
+})();
+
+export { getListInfo, setCategoryBar, setListView };
