5058ca10108d6e83fc612dcfc6c4b46ec06f329a
WongilK <wwwkim99@naver.com>
2023-07-12 17:56:31 +0900

    [FEAT] 프로그래스바 애니메이션  카테고리 자동 이동

--- js/main/listView.js
+++ js/main/listView.js
@@ -2,34 +2,57 @@ import { category, news_by_category } from "../../assets/news.js";
 
 let current_page = 1;
 
+function passAnimation() {
+  const prevSelected = document.querySelector(".selected-category");
+  prevSelected.classList.remove("selected-category");
+  prevSelected.children[2].style.display = "none";
+
+  if (prevSelected.nextElementSibling === null) {
+    document
+      .querySelector(".category li:first-child")
+      .classList.add("selected-category");
+  } else {
+    prevSelected.nextElementSibling.classList.add("selected-category");
+  }
+  document.querySelector(".selected-category").children[2].style.display =
+    "flex";
+}
+
 function makeCategory() {
-  const $ul = document.querySelector(".category");
+  const _ul = document.querySelector(".category");
+
   category.forEach((item, index) => {
-    const $li = document.createElement("li");
-    $li.innerHTML = `
-      <div></div>
+    const _li = document.createElement("li");
+
+    _li.innerHTML = `
+      <div style="animation-iteration-count:${news_by_category[item].length}"></div>
       <span class="category-item">${item}</span> 
       <span class="category-num">${current_page}/${news_by_category[item].length}</span>
     `;
-    $ul.appendChild($li);
+
+    _ul.appendChild(_li);
+
+    // _li.children[0].addEventListener("animationiteration", (e) => {
+    //   document.querySelector(".category-num").innerText = `${e.elapsedTime}/81`;
+    // });
+
+    _li.children[0].addEventListener("animationend", passAnimation);
+
     if (index === 0) {
-      $li.classList.add("selected-category");
-      $li.children[2].style.display = "flex";
+      _li.classList.add("selected-category");
+      _li.children[2].style.display = "flex";
     }
   });
 }
 
 function handleCategoryClick(e, item) {
   const prevSelected = document.querySelector(".selected-category");
+  // category-num 안 보이게
   prevSelected.children[2].style.display = "none";
   prevSelected.classList.remove("selected-category");
   item.classList.add("selected-category");
   item.querySelector(".category-num").style.display = "flex";
-
-  const news = news_by_category[e.target.innerText];
-
-  // news의 length를 div의 animation-iteration-count,
-  item.children[0].style.animationIterationCount = news.length;
+  // const news = news_by_category[e.target.innerText];
 }
 
 function addEventToCategory() {
