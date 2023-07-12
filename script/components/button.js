80d892dea0ce2420c973494ad0550f7db910b4f6
Jo <tockiib@gmail.com>
2023-07-12 13:39:33 +0900

    feat: SubButton에 onClick 추가

--- script/components/button.js
+++ script/components/button.js
@@ -15,19 +15,20 @@ const Button = (icon, isWhite, text, onClick) => {
   return buttonElement;
 };
 
-const unSubButton = withText => {
+const unSubButton = (withText, onClick) => {
   return withText
-    ? Button('close', 'gray', '해지하기')
-    : Button('close', 'gray');
+    ? Button('close', 'gray', '해지하기', onClick)
+    : Button('close', 'gray', onClick);
 };
 
-const SubButton = (isSub, withText) => {
+const SubButton = (isSub, withText, onClick) => {
   const subElement = document.createElement('div');
   subElement.className = 'media_hover surface_alt';
   const button = isSub
-    ? Button('plus', 'white', '구독하기')
-    : unSubButton(withText);
+    ? Button('plus', 'white', '구독하기', onClick)
+    : unSubButton(withText, onClick);
   subElement.appendChild(button);
+
   return subElement;
 };
 
