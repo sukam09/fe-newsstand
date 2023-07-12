46963c8b6dd0cee97025cf5356595cd76f66e9c4
afk0323 <jwv0323@naver.com>
2023-07-12 11:25:46 +0900

    Refactor: 언론사별 기사 영역 js로 Element 생성

--- src/components/press-grid.js
+++ src/components/press-grid.js
@@ -52,7 +52,9 @@ const setTotalPress = (isLightMode) => {
   fetch('./assets/data/total-press.json')
     .then((response) => response.json())
     .then((data) => {
-      makeGrid(data, isLightMode);
+      let shufflePress = setPressList(data);
+      getPressLogoElement(data, shufflePress, isLightMode);
+      makePressGrid(data, shufflePress, isLightMode);
     })
     .catch((error) => {
       console.error('언론사 정보를 불러오는 중에 오류가 발생했습니다.', error);
@@ -63,13 +65,18 @@ const shuffleList = (list) => {
   list.sort(() => Math.random() - 0.5);
 };
 
-const makeGrid = (pressData, isLightMode) => {
-  const pressLogoWrapper = document.querySelector('.press-logo__wrapper');
-  let gridIndex = Array.from({ length: pressData.length }, (_, idx) => idx + 1);
-  shuffleList(gridIndex);
+const setPressList = (pressData) => {
+  let shufflePress = Array.from({ length: pressData.length }, (_, idx) => idx + 1);
+  shuffleList(shufflePress);
 
-  let gridPage = gridIndex.slice(pageNum * 24, pageNum * 24 + 24);
-  gridPage.forEach((pressNum, idx) => {
+  return shufflePress;
+};
+
+const makePressGrid = (pressData, shufflePress, isLightMode) => {
+  const pressLogoWrapper = document.querySelector('.press-logo__wrapper');
+
+  let shufflePressNum = shufflePress.slice(pageNum * 24, pageNum * 24 + 24);
+  shufflePressNum.forEach((pressNum, idx) => {
     const $li = document.createElement('li');
     $li.classList.add('press-logo__li');
 
@@ -80,10 +87,56 @@ const makeGrid = (pressData, isLightMode) => {
     checkImg.src = imgSrc;
     checkImg.onload = () => {
       const $img = document.createElement('img');
+      $img.classList.add(`img${idx}`);
       $img.src = imgSrc;
       $li.appendChild($img);
       pressLogoWrapper.append($li);
     };
+  });
+};
+
+/**
+ * 그리드 화살표
+ */
+const getPressLogoElement = (pressData, shufflePress, isLightMode) => {
+  const arrowsWrapper = document.querySelector('.arrows__wrapper');
+
+  const $ul = document.createElement('ul');
+  $ul.classList.add('press-logo__wrapper');
+
+  const $imgLeft = document.createElement('img');
+  $imgLeft.classList.add('arrows__img-left');
+  $imgLeft.src = './assets/icons/chevron-left.svg';
+  $imgLeft.addEventListener('click', () => {
+    pageNum -= 1;
+    changePressLogo(pressData, shufflePress, isLightMode);
+  });
+
+  const $imgRight = document.createElement('img');
+  $imgRight.classList.add('arrows__img-right');
+  $imgRight.src = './assets/icons/chevron-right.svg';
+  $imgRight.addEventListener('click', () => {
+    pageNum += 1;
+    changePressLogo(pressData, shufflePress, isLightMode);
+  });
+
+  arrowsWrapper.appendChild($ul);
+  arrowsWrapper.appendChild($imgLeft);
+  arrowsWrapper.appendChild($imgRight);
+};
+
+const changePressLogo = (pressData, shufflePress, isLightMode) => {
+  let shufflePressNum = shufflePress.slice(pageNum * 24, pageNum * 24 + 24);
+  shufflePressNum.forEach((pressNum, idx) => {
+    const $img = document.querySelector(`.img${idx}`);
+
+    const selectPress = pressData.filter((data) => data.id === pressNum);
+    const imgSrc = isLightMode ? selectPress[0].lightSrc : selectPress[0].darkSrc;
+
+    let checkImg = new Image();
+    checkImg.src = imgSrc;
+    checkImg.onload = () => ($img.src = imgSrc);
+    checkImg.onerror = () => $img.remove();
   });
 };
 
--- src/index.js
+++ src/index.js
@@ -4,95 +4,55 @@ import { setPressGrid } from './components/press-grid.js';
 
 const newsWrapper = document.querySelector('.press-logo__wrapper');
 
-let idList = Array.from({ length: 96 }, (_, idx) => idx);
 let isLightMode = true;
-// let pageNum = 0;
+let pageNum = 0;
 
-// /**
-//  * 배열을 섞는 함수
-//  */
-// const shuffleList = (list) => {
-//   list.sort(() => Math.random() - 0.5);
-// };
-
-// /**
-//  * 뉴스스탠드 Grid 제작하기
-//  */
-// const makeGrid = () => {
-//   for (let i = 1; i <= 24; i++) {
-//     const gridItem = document.createElement('li');
-//     const imgSrc = isLightMode
-//       ? `./assets/images/light-press-logo/img${idList[i]}.svg`
-//       : `./assets/images/dark-press-logo/img${idList[i]}.svg`;
-
-//     let checkImg = new Image();
-//     checkImg.src = imgSrc;
-//     checkImg.onload = function () {
-//       const img = document.createElement('img');
-//       img.classList.add(`img${i}`);
-//       img.src = imgSrc;
-//       img.style.height = '20px';
-//       gridItem.classList.add('press-logo__li');
-//       gridItem.appendChild(img);
-//     };
-
-//     newsWrapper.append(gridItem);
-//   }
-// };
-
-// /**
-//  * 이미지 src 변경하기
-//  */
-// const changeImgSrc = () => {
-//   let newImg = idList.slice(pageNum * 24, pageNum * 24 + 24);
-
-//   for (let i = 0; i < 24; i++) {
-//     const $img = document.querySelector(`.img${i}`);
-//     const imgSrc = isLightMode
-//       ? `./assets/images/light-press-logo/img${newImg[i]}.png`
-//       : `./assets/images/dark-press-logo/img${newImg[i]}.png`;
-
-//     let checkImg = new Image();
-//     checkImg.src = imgSrc;
-//     checkImg.onload = function () {
-//       $img.src = imgSrc;
-//     };
-//     checkImg.onerror = function () {
-//       $img.remove();
-//     };
-//   }
-// };
-
-/////////////////////
 /**
-//  * Grid 화살표 hidden 처리
-//  */
-// const setArrowVisible = (mediaList) => {
-//   const leftArrow = document.querySelector('.arrows__img-left');
-//   const rightArrow = document.querySelector('.arrows__img-right');
+ * 이미지 src 변경하기
+ */
+const changeImgSrc = () => {
+  let newImg = idList.slice(pageNum * 24, pageNum * 24 + 24);
 
-//   // 페이지 제한 0~3에 따른 hidden 여부
-//   if (pageNum === 0) {
-//     leftArrow.classList.add('hidden');
-//   } else if (pageNum > 0 && pageNum < 3) {
-//     leftArrow.classList.remove('hidden');
-//     rightArrow.classList.remove('hidden');
-//   } else if (pageNum === 3) {
-//     rightArrow.classList.add('hidden');
-//   }
+  for (let i = 0; i < 24; i++) {
+    const $img = document.querySelector(`.img${i}`);
+    const imgSrc = isLightMode
+      ? `./assets/images/light-press-logo/img${newImg[i]}.png`
+      : `./assets/images/dark-press-logo/img${newImg[i]}.png`;
 
-//   // 언론사 로고 개수 따른 hidden 여부
-// };
+    let checkImg = new Image();
+    checkImg.src = imgSrc;
+    checkImg.onload = function () {
+      $img.src = imgSrc;
+    };
+    checkImg.onerror = function () {
+      $img.remove();
+    };
+  }
+};
+
+/**
+ * Grid 화살표 hidden 처리
+ */
+const setArrowVisible = (mediaList) => {
+  const leftArrow = document.querySelector('.arrows__img-left');
+  const rightArrow = document.querySelector('.arrows__img-right');
+
+  // 페이지 제한 0~3에 따른 hidden 여부
+  if (pageNum === 0) {
+    leftArrow.classList.add('hidden');
+  } else if (pageNum > 0 && pageNum < 3) {
+    leftArrow.classList.remove('hidden');
+    rightArrow.classList.remove('hidden');
+  } else if (pageNum === 3) {
+    rightArrow.classList.add('hidden');
+  }
+
+  // 언론사 로고 개수 따른 hidden 여부
+};
 
 /**
  * Grid 화살표 클릭
  */
-const clickArrow = (className) => {
-  if (className === 'left-arrow') pageNum--;
-  if (className === 'right-arrow') pageNum++;
-  changeImgSrc();
-  setArrowVisible();
-};
 
 function init() {
   setNewsStandHeader();
