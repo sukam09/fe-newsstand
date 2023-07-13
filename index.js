391a9297881b64a60f34d5b36919e7aeb5d19760
wonho1401 <wonho1401@naver.com>
2023-07-13 14:12:38 +0900

    edit: edit slice data function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { render, createGridPages } from "./components/gridView.js";
 import { rollNews } from "./components/roller.js";
+import { RECENT_NEWS_NUM } from "./constants/constant.js";
 import { viewSelectHandler } from "./utils/event.js";
 import { fetchData } from "./utils/fetch.js";
 import { shuffleData } from "./utils/shuffle.js";
@@ -14,11 +15,10 @@ fetchData().then((data) => {
   const agencies = shuffleData(data);
 
   // rolling할 뉴스 slice
-  const slicedData = sliceData(data);
+  const slicedData = sliceData(data, 0, RECENT_NEWS_NUM);
 
   // 뉴스 rolling
   rollNews(slicedData);
-
   // 상태에 따라 grid, list view render
   viewSelectHandler(agencies);
 });
