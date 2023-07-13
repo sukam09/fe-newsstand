f9564c9150774a5151978ca45b1e7440985eba18
김은민 <menuin@gim-eunmin-ui-MacBookPro.local>
2023-07-13 18:11:58 +0900

    [feat] 언론사 로고 추가, 페이지 전환

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
+const gridContainer = document.querySelector(".grid-box");
+const dateContainer = document.querySelector(".header-right");
+const leftArrow = document.querySelector(".arrow-left");
+const rightArrow = document.querySelector(".arrow-right");
 
-const date_container = document.querySelector(".header-right");
+let crntPage = 0;
 
+function listenArrow(){
+    leftArrow.addEventListener("click",()=> {
+        crntPage--;
+        updateArrow();
+        drawPress(crntPage);
+    })
+    rightArrow.addEventListener("click",() => {
+        crntPage++;
+        updateArrow();
+        drawPress(crntPage);
+    })
+    
+}
+function updateArrow(){
+    if (crntPage == 0){
+        leftArrow.classList.add("hidden");
+    } else if (crntPage == 3){
+        rightArrow.classList.add("hidden");
+    } else {
+        leftArrow.classList.remove("hidden");
+        rightArrow.classList.remove("hidden");
+    }
+  
+}
+function drawPress(idx){
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    for (let i=24*idx;i<24*(idx+1);i++){
+        const pressElem = document.createElement("li");
+        const pressLogo = document.createElement("img");
+        pressLogo.src = `./asset/logo/light/img${i+1}.svg`
+        pressElem.appendChild(pressLogo);
+        gridContainer.appendChild(pressElem);
+    }
+}
 function drawDate() {
     let date = new Date();
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
     let weekDay = ["일요일","월요일","화요일","수요일","목요일","금요일","토요일"]
-    
+
     let fullDate = year + ". " + month + ". " + day + " " + weekDay[date.getDay()];
     let dateContent = document.createTextNode(fullDate);
-    date_container.appendChild(dateContent);
-    
+    dateContainer.appendChild(dateContent);
 }
 function init(){
     drawDate();
+    drawPress(crntPage);
+    updateArrow();
+    listenArrow();
+    
 }
+
 window.onload = init;
