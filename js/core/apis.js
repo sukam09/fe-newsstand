be123d23cc4efaec1833b2296d36305e14ef31af
박태규 Park Tae Kyu <ptq124@naver.com>
2023-07-13 14:51:53 +0900

    Style: {}축약

--- js/core/apis.js
+++ js/core/apis.js
@@ -1,12 +1,11 @@
 async function getNewsData() {
-  return await fetch('./newsHeadData.json').then((response) => {
-    return response.json();
-  });
+  return await fetch('./newsHeadData.json').then((response) => response.json());
 }
 async function getRollingData() {
-  return await fetch('./rollingData.json').then((response) => {
-    return response.json();
-  });
+  return await fetch('./rollingData.json').then((response) => response.json());
+}
+async function getNewsLIstData() {
+  return await fetch('./newsListData.json').then((response) => response.json());
 }
 
-export { getNewsData, getRollingData };
+export { getNewsData, getRollingData, getNewsLIstData };
