ece3ca1ee67c8c4415b94a85483384a2f6000463
김다예 <63107805+kimdaye77@users.noreply.github.com>
2023-07-13 10:19:25 +0900

    [REFACTOR] 카테고리 constants.js 파일로 이동

--- js/constants/constants.js
+++ js/constants/constants.js
@@ -6,3 +6,11 @@ export const FIRST_PAGE_NUM = 1;
 export const LAST_PAGE_NUM = 4;
 export const PRESS_CNT = 96;
 export const PRESS_VIEW_COUNT = 24;
+export const CATEGORY = [
+  "종합/경제",
+  "방송/통신",
+  "IT",
+  "영자지",
+  "스포츠/연예",
+  "매거진/전문지",
+];
