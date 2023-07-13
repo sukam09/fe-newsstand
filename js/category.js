0fd4821e31ffd510810a662b2f4a60c77fc04aff
seongho <seongho@seonghoui-MacBookPro.local>
2023-07-13 14:28:28 +0900

    feature: initCategoryItem함수

--- js/category.js
+++ js/category.js
@@ -1,9 +1,11 @@
 import categoryData from "../json/category.json" assert { type: "json" };
 
-let progressValue = 1;
-let progressTime = 20;
+let progress = 0;
+const increment = 10;
+const totalTime = 20000; //초
 let category = [];
-
+let categoryItem;
+let currentPage = 1;
 function setCategoryData() {
   for (const categories of categoryData) {
     for (const key in categories) {
@@ -19,12 +21,37 @@ function setCategoryData() {
 function addInitCategory() {
   let categoryHtml = `<div class="categoryWrap"><ul>`;
   category.forEach((key, index) => {
-    categoryHtml += `<div class="categoryItem"><div class="progress-bar" id=${key.key}></div><span class="category">${key.key}</span></li><span class="categoryCnt">${key.value}</span></div>`;
+    categoryHtml += `<div class="categoryItem" id="category${index}"><div class="progress-bar" id=${key.key}></div><span class="category">${key.key}</span></li><span class="currentCategoryPage">1</span><span class="categoryCnt">/ ${key.value}</span></div>`;
   });
   categoryHtml += `</ul></div>`;
   document.getElementById("category").innerHTML = categoryHtml;
+  initCategoryItem();
 }
 
+function initCategoryItem() {
+  categoryItem = document.querySelectorAll(".categoryItem");
+  categoryItem[0].style.backgroundColor = "#7890E7";
+  categoryItem[0].style.color = "#FFFFFF";
+  categoryItem[0].style.display = "flex";
+  document.querySelectorAll(".currentCategoryPage")[0].style.display = "flex";
+  document.querySelectorAll(".categoryCnt")[0].style.display = "flex";
+}
 
+function addOnClickCategory() {
+  category.forEach((value, index) => {
+    document.addEventListener("click", () => {});
+  });
+}
+
+function doProgress(progressBar) {
+  console.log(progressBar);
+  if (progress >= 100) {
+    clearInterval(progressInterval);
+    return;
+  }
+  progress += increment;
+  progressBar.style.width = `${progress}%`;
+}
+// const progressInterval = setInterval(doProgress,totalTime / (100 / increment), progressBar);
 setCategoryData();
 export { addInitCategory };
