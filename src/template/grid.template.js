560cb648678ee4eb3403ef50fe2feedde4774488
1lsang <ilsangpark.98@gmail.com>
2023-07-13 16:27:14 +0900

    Feat: Add grid template literal functions

---
+++ src/template/grid.template.js
@@ -0,0 +1,27 @@
+import { html } from "../lib/html";
+/**
+ * HTML 형태 Template Literal을 입력받아 Grid View Template Literal을 return 합니다.
+ * @param { HTMLString } page
+ * @returns { HTMLString }
+ */
+export const gridTemplate = (page) => html `
+  <div class="grid-view">
+    <ul class="company-list">
+      ${page}
+    </ul>
+  </div>
+`;
+/**
+ * Company Data를 입력받아 하나의 Grid Item Template Literal을 return 합니다.
+ * @param { Company } data
+ * @returns { HTMLString }
+ */
+export const gridItemTemplate = (data) => html `
+  <li class="company-list__item">
+    <img
+      class="logo"
+      src=${`/public/asset/images/light/${data.id}.png`}
+      alt=${data.name}
+    />
+  </li>
+`;
