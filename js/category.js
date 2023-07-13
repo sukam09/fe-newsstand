774e1bf3f00b5c8661e731b65766b3f76558e23f
seongho <seongho@seonghoui-MacBookPro.local>
2023-07-13 11:45:32 +0900

    update: category전역변수화

--- js/category.js
+++ js/category.js
@@ -1,22 +1,39 @@
 import categoryData from "../json/category.json" assert { type: "json" };
 
-function addInitCategory() {
-  let categoryHtml = `<div class="categoryWrap"><ul>`;
-  for (const category of categoryData) {
-    for (const key in category) {
-      categoryHtml += `<div class="categoryItem" id="${key}"><li><label><input type="radio" name="category">${key}</label></li><span class="categoryCnt">${category[key]}</span></div>`;
+let progressValue = 1;
+let progressTime = 20;
+let category = [];
+
+function setCategoryData() {
+  for (const categories of categoryData) {
+    for (const key in categories) {
+      const item = {
+        key: key,
+        value: categories[key],
+      };
+      category.push(item);
     }
   }
+}
+
+function addInitCategory() {
+  let categoryHtml = `<div class="categoryWrap"><ul>`;
+  category.forEach((key, index) => {
+    categoryHtml += `<div class="categoryItem"><li><span>${key.key}</span></li><span class="categoryCnt">${key.value}</span></div>`;
+  });
   categoryHtml += `</ul></div>`;
   document.getElementById("category").innerHTML = categoryHtml;
 }
 
-// function addSpanEvent(){
-//   const spanTag = document.querySelector(".categoryCnt");
-//   spanTag.addEventListener("click",()=>{
+function increaseValue(id) {
+  setInterval(() => {
+    const progressElement = document.getElementById(key);
+    if (progressValue < progressTime) {
+      progressValue++; // 값 증가
+      progressElement.value = progressValue; // 값 업데이트
+    }
+  }, 1000);
+}
 
-//   })
-// }
-
-// addSpanEvent();
+setCategoryData();
 export { addInitCategory };
