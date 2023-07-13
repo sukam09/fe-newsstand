671e01f5325ae4814dc5836e3b477577fea53760
김은민 <menuin@gim-eunmin-ui-MacBookPro.local>
2023-07-14 00:02:35 +0900

    [feat] 새로고침 시 언론사 리스트 랜덤 배치

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ const header = document.querySelector(".header-left");
 const pressCover = document.querySelector(".press-cover");
 
 let crntPage = 0;
+let pressIdxArray = Array.from(Array(96).keys()); // create array of consecutive numbers [0...95] - to be used in drawPress()
 
+
+function shuffleArray(arr){
+    for (let i = arr.length - 1; i>0;i--){
+        const j = Math.floor(Math.random()*(i+1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
 function listenPressHover(){
     const pressItems = document.querySelectorAll(".pressItem");
     pressItems.forEach((item)=>{
@@ -50,12 +59,13 @@ function drawPress(idx){
     while (gridContainer.firstChild){
         gridContainer.removeChild(gridContainer.firstChild);
     }
+    let shuffledArray = shuffleArray(pressIdxArray);
 
     for (let i=24*idx;i<24*(idx+1);i++){
         const pressElem = document.createElement("li");
         pressElem.classList.add("pressItem");
         const pressLogo = document.createElement("img");
-        pressLogo.src = `./asset/logo/light/img${i+1}.svg`
+        pressLogo.src = `./asset/logo/light/img${shuffledArray[i+1]}.svg`
         pressElem.appendChild(pressLogo);
         gridContainer.appendChild(pressElem);
     }
@@ -77,8 +87,7 @@ function init(){
     drawPress(crntPage);
     updateArrow();
     listenArrow();
-    listenReload();
-    
+    listenReload();   
 }
 
 window.onload = init;
