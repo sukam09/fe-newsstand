42dd48d39acff1452ffa932e1fbb5783e149c5b3
seokhun <tommya98@seokhunui-MacBookPro.local>
2023-07-13 13:52:58 +0900

    feat: 프로그래스바 현재 페이지 출력 구현

--- module/list.js
+++ module/list.js
@@ -14,8 +14,12 @@ function setListNews() {
 
 function setListNavBar() {
   const navBarDefaultLeft = GLOBAL.DOM.field_tab.getBoundingClientRect().left;
+  const cur_page_in_category = getCurPageInCategory();
 
-  GLOBAL.DOM.field_tab.querySelector(".progress").classList.remove("progress");
+  const removeTarget = GLOBAL.DOM.field_tab.querySelector(".progress");
+  removeTarget.classList.remove("progress");
+  removeTarget.classList.remove("selected-bold14");
+  removeTarget.classList.add("available-medium14");
 
   GLOBAL.DOM.field_tab.querySelectorAll("div").forEach((element) => {
     if (element.className != "progress-bar") {
@@ -25,38 +29,66 @@ function setListNavBar() {
 
   switch (GLOBAL.list_cur_category) {
     case CATEGORY.ECONOMY:
-      GLOBAL.DOM.nav_economy.querySelector("div").style.display = "block";
+      GLOBAL.DOM.nav_economy.querySelector("div").style.display = "flex";
       GLOBAL.DOM.nav_economy.classList.add("progress");
+      GLOBAL.DOM.nav_economy.classList.remove("available-medium14");
+      GLOBAL.DOM.nav_economy.classList.add("selected-bold14");
+      GLOBAL.DOM.nav_economy.querySelectorAll("span")[1].innerHTML = cur_page_in_category;
+      GLOBAL.DOM.nav_economy.querySelectorAll("span")[2].innerHTML = GLOBAL.CATEGORY_NUM.ECONOMY;
       GLOBAL.DOM.progress_bar.style.left = `${GLOBAL.DOM.nav_economy.getBoundingClientRect().left - navBarDefaultLeft}px`;
       break;
     case CATEGORY.BROADCAST:
-      GLOBAL.DOM.nav_broadcast.querySelector("div").style.display = "block";
+      GLOBAL.DOM.nav_broadcast.querySelector("div").style.display = "flex";
       GLOBAL.DOM.nav_broadcast.classList.add("progress");
+      GLOBAL.DOM.nav_broadcast.classList.remove("available-medium14");
+      GLOBAL.DOM.nav_broadcast.classList.add("selected-bold14");
+      GLOBAL.DOM.nav_broadcast.querySelectorAll("span")[1].innerHTML = cur_page_in_category;
+      GLOBAL.DOM.nav_broadcast.querySelectorAll("span")[2].innerHTML = GLOBAL.CATEGORY_NUM.BROADCAST;
       GLOBAL.DOM.progress_bar.style.left = `${GLOBAL.DOM.nav_broadcast.getBoundingClientRect().left - navBarDefaultLeft}px`;
       break;
     case CATEGORY.IT:
-      GLOBAL.DOM.nav_it.querySelector("div").style.display = "block";
+      GLOBAL.DOM.nav_it.querySelector("div").style.display = "flex";
       GLOBAL.DOM.nav_it.classList.add("progress");
+      GLOBAL.DOM.nav_it.classList.remove("available-medium14");
+      GLOBAL.DOM.nav_it.classList.add("selected-bold14");
+      GLOBAL.DOM.nav_it.querySelectorAll("span")[1].innerHTML = cur_page_in_category;
+      GLOBAL.DOM.nav_it.querySelectorAll("span")[2].innerHTML = GLOBAL.CATEGORY_NUM.IT;
       GLOBAL.DOM.progress_bar.style.left = `${GLOBAL.DOM.nav_it.getBoundingClientRect().left - navBarDefaultLeft}px`;
       break;
     case CATEGORY.ENGLISH:
-      GLOBAL.DOM.nav_english.querySelector("div").style.display = "block";
+      GLOBAL.DOM.nav_english.querySelector("div").style.display = "flex";
       GLOBAL.DOM.nav_english.classList.add("progress");
+      GLOBAL.DOM.nav_english.classList.remove("available-medium14");
+      GLOBAL.DOM.nav_english.classList.add("selected-bold14");
+      GLOBAL.DOM.nav_english.querySelectorAll("span")[1].innerHTML = cur_page_in_category;
+      GLOBAL.DOM.nav_english.querySelectorAll("span")[2].innerHTML = GLOBAL.CATEGORY_NUM.ENGLISH;
       GLOBAL.DOM.progress_bar.style.left = `${GLOBAL.DOM.nav_english.getBoundingClientRect().left - navBarDefaultLeft}px`;
       break;
     case CATEGORY.SPORTS:
-      GLOBAL.DOM.nav_sports.querySelector("div").style.display = "block";
+      GLOBAL.DOM.nav_sports.querySelector("div").style.display = "flex";
       GLOBAL.DOM.nav_sports.classList.add("progress");
+      GLOBAL.DOM.nav_sports.classList.remove("available-medium14");
+      GLOBAL.DOM.nav_sports.classList.add("selected-bold14");
+      GLOBAL.DOM.nav_sports.querySelectorAll("span")[1].innerHTML = cur_page_in_category;
+      GLOBAL.DOM.nav_sports.querySelectorAll("span")[2].innerHTML = GLOBAL.CATEGORY_NUM.SPORTS;
       GLOBAL.DOM.progress_bar.style.left = `${GLOBAL.DOM.nav_sports.getBoundingClientRect().left - navBarDefaultLeft}px`;
       break;
     case CATEGORY.MAGAZINE:
-      GLOBAL.DOM.nav_magazine.querySelector("div").style.display = "block";
+      GLOBAL.DOM.nav_magazine.querySelector("div").style.display = "flex";
       GLOBAL.DOM.nav_magazine.classList.add("progress");
+      GLOBAL.DOM.nav_magazine.classList.remove("available-medium14");
+      GLOBAL.DOM.nav_magazine.classList.add("selected-bold14");
+      GLOBAL.DOM.nav_magazine.querySelectorAll("span")[1].innerHTML = cur_page_in_category;
+      GLOBAL.DOM.nav_magazine.querySelectorAll("span")[2].innerHTML = GLOBAL.CATEGORY_NUM.MAGAZINE;
       GLOBAL.DOM.progress_bar.style.left = `${GLOBAL.DOM.nav_magazine.getBoundingClientRect().left - navBarDefaultLeft}px`;
       break;
     case CATEGORY.LOCAL:
-      GLOBAL.DOM.nav_local.querySelector("div").style.display = "block";
+      GLOBAL.DOM.nav_local.querySelector("div").style.display = "flex";
       GLOBAL.DOM.nav_local.classList.add("progress");
+      GLOBAL.DOM.nav_local.classList.remove("available-medium14");
+      GLOBAL.DOM.nav_local.classList.add("selected-bold14");
+      GLOBAL.DOM.nav_local.querySelectorAll("span")[1].innerHTML = cur_page_in_category;
+      GLOBAL.DOM.nav_local.querySelectorAll("span")[2].innerHTML = GLOBAL.CATEGORY_NUM.LOCAL;
       GLOBAL.DOM.progress_bar.style.left = `${GLOBAL.DOM.nav_local.getBoundingClientRect().left - navBarDefaultLeft}px`;
       break;
   }
@@ -71,6 +103,34 @@ function moveListPage(pagenum) {
 
 function setCurCategory() {
   GLOBAL.list_cur_category = GLOBAL.list_news_data[GLOBAL.list_cur_page].category;
+}
+
+function getCurPageInCategory() {
+  let page = GLOBAL.list_cur_page;
+  switch (GLOBAL.list_cur_category) {
+    case CATEGORY.ECONOMY:
+      break;
+    case CATEGORY.BROADCAST:
+      page = page - GLOBAL.CATEGORY_NUM.ECONOMY;
+      break;
+    case CATEGORY.IT:
+      page = page - GLOBAL.CATEGORY_NUM.ECONOMY - GLOBAL.CATEGORY_NUM.BROADCAST;
+      break;
+    case CATEGORY.ENGLISH:
+      page = page - GLOBAL.CATEGORY_NUM.ECONOMY - GLOBAL.CATEGORY_NUM.BROADCAST - GLOBAL.CATEGORY_NUM.IT;
+      break;
+    case CATEGORY.SPORTS:
+      page = page - GLOBAL.CATEGORY_NUM.ECONOMY - GLOBAL.CATEGORY_NUM.BROADCAST - GLOBAL.CATEGORY_NUM.IT - GLOBAL.CATEGORY_NUM.ENGLISH;
+      break;
+    case CATEGORY.MAGAZINE:
+      page = page - GLOBAL.CATEGORY_NUM.ECONOMY - GLOBAL.CATEGORY_NUM.BROADCAST - GLOBAL.CATEGORY_NUM.IT - GLOBAL.CATEGORY_NUM.ENGLISH - GLOBAL.CATEGORY_NUM.SPORTS;
+      break;
+    case CATEGORY.LOCAL:
+      page = page - GLOBAL.CATEGORY_NUM.ECONOMY - GLOBAL.CATEGORY_NUM.BROADCAST - GLOBAL.CATEGORY_NUM.IT - GLOBAL.CATEGORY_NUM.ENGLISH - GLOBAL.CATEGORY_NUM.SPORTS - GLOBAL.CATEGORY_NUM.MAGAZINE;
+      break;
+  }
+
+  return page;
 }
 
 export { moveListPage };
