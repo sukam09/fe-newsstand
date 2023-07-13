6c554f0b81cb4534e985f87a2f74b089a1fa33c9
1lsang <ilsangpark.98@gmail.com>
2023-07-13 22:42:49 +0900

    Feat: Add helper functions for data processing

---
+++ src/api/helpers.js
@@ -0,0 +1,40 @@
+import { MAX_PAGE_NUM } from "../config.js";
+/**
+ * company 데이터 배열을 입력받아 pagination을 적용한 데이터 배열을 return 합니다.
+ * @param {[]} data
+ * @param {number} PAGINATION_NUM
+ * @returns {[]}
+ */
+function pagination(data, PAGINATION_NUM) {
+    const pages = [];
+    let pageNumber = Math.min(Math.ceil(data?.length / PAGINATION_NUM), MAX_PAGE_NUM);
+    for (let i = 0; i < pageNumber; i++) {
+        pages.push(data.slice(i * PAGINATION_NUM, (i + 1) * PAGINATION_NUM));
+    }
+    return pages;
+}
+/**
+ * 입력받은 배열 데이터를 shuffle하여 return 합니다.
+ * @param {[]} data
+ * @returns {[]}
+ */
+function shuffle(data) {
+    return data.sort(() => 0.5 - Math.random());
+}
+/**
+ * Company 데이터 배열을 입력받아 카테고리별로 분류한 데이터 배열을 return 합니다.
+ * @param {[]} data
+ * @returns {[]}
+ */
+function categorize(data) {
+    const categorizedData = {};
+    data.forEach((agency) => {
+        const category = agency.category;
+        if (categorizedData[category])
+            categorizedData[category].push(agency);
+        else
+            categorizedData[category] = [agency];
+    });
+    return categorizedData;
+}
+export { pagination, shuffle, categorize };
