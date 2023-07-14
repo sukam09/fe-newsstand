4df97ead38ea5808318fcb855e4f0efd288b4016
asdf99245 <asdf99245@naver.com>
2023-07-14 11:14:07 +0900

    feat: 여러개의 reducer를 합치는 combineReducer 구현

--- core/zzapdux.js
+++ core/zzapdux.js
@@ -1,3 +1,26 @@
+// reducer 합치는 함수
+export const combineReducers = (reducers) => {
+  const reducerKeys = Object.keys(reducers);
+
+  return (state = {}, action) => {
+    const nextState = {};
+
+    let changed = false;
+    reducerKeys.forEach((reducerKey) => {
+      const reducer = reducers[reducerKey];
+
+      const previousState = state[reducerKey];
+      const newState = reducer(previousState, action);
+
+      nextState[reducerKey] = newState;
+
+      changed = changed || previousState !== newState;
+    });
+
+    return changed ? nextState : state;
+  };
+};
+
 export const actionCreator = (type, payload) => {
   return { type, payload };
 };
@@ -5,11 +28,11 @@ export const actionCreator = (type, payload) => {
 export const createStore = (reducer) => {
   let state = undefined;
 
+  const subscribers = [];
+
   const setState = (newState) => {
     state = newState;
   };
-
-  const subscribers = [];
 
   const getState = () => {
     return { ...state };
@@ -21,6 +44,8 @@ export const createStore = (reducer) => {
 
   const dispatch = (action) => {
     const newState = reducer(state, action);
+
+    console.log(newState);
     if (state === newState) return;
     if (JSON.stringify(state) === JSON.stringify(newState)) return;
 
