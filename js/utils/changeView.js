69304ebe0df06ff267aeefd6701a3da8ec37bda0
김다예 <63107805+kimdaye77@users.noreply.github.com>
2023-07-14 11:21:52 +0900

    [FIX] 클릭 된 상태에서 클래스 못 찾는 버그 수정

--- js/utils/changeView.js
+++ js/utils/changeView.js
@@ -7,17 +7,22 @@ export function changeView(target) {
 
   if (target === "grid") {
     const view = document.querySelector(".list-view");
-    grid_btn.src = "../assets/icons/grid-view-clicked.svg";
-    list_btn.src = "../assets/icons/list-view.svg";
-    view.classList.remove("list-view");
-    view.classList.add("grid-view");
-    showGridView(1);
+    if (view && view.classList.contains("list-view")) {
+      grid_btn.src = "../assets/icons/grid-view-clicked.svg";
+      list_btn.src = "../assets/icons/list-view.svg";
+      view.classList.remove("list-view");
+      view.classList.add("grid-view");
+      showGridView(1);
+    }
   } else {
     const view = document.querySelector(".grid-view");
-    grid_btn.src = "../assets/icons/grid-view.svg";
-    list_btn.src = "../assets/icons/list-view-clicked.svg";
-    view.classList.remove("grid-view");
-    view.classList.add("list-view");
-    showListView(1);
+    if (view && view.classList.contains("grid-view")) {
+      const view = document.querySelector(".grid-view");
+      grid_btn.src = "../assets/icons/grid-view.svg";
+      list_btn.src = "../assets/icons/list-view-clicked.svg";
+      view.classList.remove("grid-view");
+      view.classList.add("list-view");
+      showListView(1);
+    }
   }
 }
