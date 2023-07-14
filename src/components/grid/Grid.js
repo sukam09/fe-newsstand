ba7164cfbfd17dfae8c95e05846f48570988e2ef
kim-limit <kimjaehhan@gmail.com>
2023-07-14 16:45:41 +0900

    feat: flip 애니메이션 적용

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,8 +1,9 @@
-import { gridPageState, isDarkMode } from "../../store/store.js";
 import { getState } from "../../observer/observer.js";
+import { gridPageState, isDarkMode } from "../../store/store.js";
+import { _querySelector } from "../../utils/my-query-selector.js";
 import { NEWS_COUNT, PRESS_ICON } from "../../constants/constants.js";
 import { ObjectToArrayRandom, shuffleArrayRandom } from "../../utils/utils.js";
-import { _querySelector } from "../../utils/my-query-selector.js";
+import { createSubscribeButton } from "../subscribe-button/SubscribeButton.js";
 
 const $gridView = _querySelector(".grid-view");
 
@@ -25,10 +26,15 @@ const fillPressIcons = () => {
 };
 
 const createGridContent = (name, light) => {
-  return `<li class="grid-cell">
-    <button>
-      <img class="grid-cell_news-img" src=${light} alt=${name}/>
-    </button>
+  return `<li class="grid-cell card">
+    <div class="content">
+      <div class="front">
+        <button>
+          <img class="grid-cell_news-img" src=${light} alt=${name} />
+        </button>
+      </div>
+      <div class="back">${createSubscribeButton()}</div>
+    </div>
   </li>`;
 };
 
