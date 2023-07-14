23126ae52a8d4634bd222a20aee06c37311e8f01
김다예 <63107805+kimdaye77@users.noreply.github.com>
2023-07-14 11:04:05 +0900

    [REFACTOR] 언론사 뉴스 그리는 함수 분리

--- js/utils/makeListView.js
+++ js/utils/makeListView.js
@@ -1,6 +1,7 @@
 import { CATEGORY } from "../constants/constants.js";
 import { drawCategory } from "./drawCategory.js";
 import { drawPressInfo } from "./drawPressInfo.js";
+import { drawPressNews } from "./drawPressNews.js";
 //임시로
 let order_list = [
   { press: "서울경제", imgIndex: 96 },
@@ -46,7 +47,7 @@ async function drawList(order, category) {
     newDiv.classList.add("press-news");
     main_list.appendChild(newDiv);
     drawPressInfo(img);
-    drawPressNews(category_news, order);
+    drawPressNews(category_news, order_list[order - 1].press);
   } catch (error) {
     console.log(error);
   }
@@ -58,36 +59,6 @@ function handleClick(e) {
     const category = target.querySelector(".ctg").textContent.trim();
     drawList(1, category);
   }
-}
-
-function drawPressNews(category_news, order) {
-  let main_news,
-    sub_news = "";
-  category_news.forEach((news) => {
-    if (news.press === order_list[order - 1].press) {
-      if (news.thumbnail === "") {
-        sub_news += ` <li>
-        ${news.title}
-      </li>`;
-      } else {
-        main_news = `<img src="${news.thumbnail}" alt="thumbnail"/>
-        <p class="thumbnail-title">${news.title}</p>`;
-      }
-    }
-  });
-  sub_news += `<li id="caption">
-  ${order_list[order - 1].press} 언론사에서 직접 편집한 뉴스입니다.
-  </li>`;
-  const news_content = main_list.querySelector(".news-content");
-
-  news_content.innerHTML = `<div class="main-news">
-      ${main_news}
-    </div>
-    <div class = "sub-news">
-    <ul class = "sub-news-ul">
-     ${sub_news}
-  </ul>
-  </div>`;
 }
 
 export function showListView(order) {
