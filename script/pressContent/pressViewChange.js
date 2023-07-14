3d49673e528d44cb5192371a25bfbc11cedd4dc6
meanz1 <zndnfjqbd1206@gmail.com>
2023-07-14 11:18:12 +0900

    [Style] setInterval 초 변경

--- script/pressContent/pressViewChange.js
+++ script/pressContent/pressViewChange.js
@@ -1,5 +1,6 @@
 import { getElemId, getElemClass } from "../../utils/js/getElements.js";
 
+// 언론사 뷰 전환시 아이콘 색상 변경(UI)
 function pressViewChange() {
   const pressListView = getElemId(document, 'pressbar-icon-list-view');
   const pressGridView = getElemId(document, 'pressbar-icon-grid-view');
@@ -17,6 +18,7 @@ function pressViewChange() {
   });
 }
 
+// 언론사 뷰 전환시 해당하는 뷰(그리드 or 리스트) 나타내기
 function showPressView(status) {
   const pressContentContainer = getElemClass(document, "press-content-view");
   const pressContentListView = getElemClass(document, "press-content-list-view");
