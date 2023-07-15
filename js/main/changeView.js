22879c398501215a0d3c90395c6c3d3f79972b3b
WongilK <wwwkim99@naver.com>
2023-07-15 23:41:55 +0900

    [REFACTOR] changeView 함수 분리

--- js/main/changeView.js
+++ js/main/changeView.js
@@ -1,16 +1,43 @@
 //뷰 타입 변경
 import { main_list_page, MIN_PAGE, MAX_PAGE } from "./gridView/gridView.js";
-function initViewChange() {
-  const view_type = document.querySelectorAll(".viewer-btn button");
-  //view_type[0] : listview, view_type[1] : gridview
+
+function getById(id) {
+  return document.getElementById(id);
+}
+
+function changeBtnAndView(view) {
   const grid_view = document.querySelector(".grid-view");
   const list_view = document.querySelector(".list-view");
 
-  const grid_left_btn = document.getElementById("grid-left-btn");
-  const grid_right_btn = document.getElementById("grid-right-btn");
+  const grid_left_btn = getById("grid-left-btn");
+  const grid_right_btn = getById("grid-right-btn");
+  const list_left_btn = getById("list-left-btn");
+  const list_right_btn = getById("list-right-btn");
 
-  const list_left_btn = document.getElementById("list-left-btn");
-  const list_right_btn = document.getElementById("list-right-btn");
+  if (view === "list") {
+    grid_view.style.display = "none";
+    list_view.style.display = "block";
+
+    //button 교체
+    grid_left_btn.style.visibility = "hidden";
+    grid_right_btn.style.visibility = "hidden";
+    list_left_btn.style.visibility = "visible";
+    list_right_btn.style.visibility = "visible";
+  } else {
+    list_view.style.display = "none";
+    grid_view.style.display = "flex";
+
+    if (main_list_page !== MIN_PAGE) grid_left_btn.style.visibility = "visible";
+    if (main_list_page !== MAX_PAGE)
+      grid_right_btn.style.visibility = "visible";
+    list_left_btn.style.visibility = "hidden";
+    list_right_btn.style.visibility = "hidden";
+  }
+}
+
+function initViewChange() {
+  const view_type = document.querySelectorAll(".viewer-btn button");
+  //view_type[0] : listview, view_type[1] : gridview
 
   view_type[0].addEventListener("click", () => {
     view_type[0].innerHTML = `<img
@@ -21,14 +48,7 @@ function initViewChange() {
     src="../images/icon/Grid-view-unchecked.svg"
     alt="images"
   />`;
-    grid_view.style.display = "none";
-    list_view.style.display = "block";
-
-    //button 교체
-    grid_left_btn.style.visibility = "hidden";
-    grid_right_btn.style.visibility = "hidden";
-    list_left_btn.style.visibility = "visible";
-    list_right_btn.style.visibility = "visible";
+    changeBtnAndView("list");
   });
   view_type[1].addEventListener("click", () => {
     view_type[0].innerHTML = `<img
@@ -39,15 +59,7 @@ function initViewChange() {
     src="../images/icon/Grid-view-checked.svg"
     alt="images"
   />`;
-    list_view.style.display = "none";
-    grid_view.style.display = "flex";
-
-    if (main_list_page !== MIN_PAGE) grid_left_btn.style.visibility = "visible";
-    if (main_list_page !== MAX_PAGE)
-      grid_right_btn.style.visibility = "visible";
-    list_left_btn.style.visibility = "hidden";
-    list_right_btn.style.visibility = "hidden";
+    changeBtnAndView("grid");
   });
 }
-/**/
 export { initViewChange };
