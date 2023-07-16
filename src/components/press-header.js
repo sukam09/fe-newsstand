7c13e8618416409aa22a0fff541bc22ce80bd407
afk0323 <jwv0323@naver.com>
2023-07-16 13:37:47 +0900

    feat: 언론사의 상수 파일 분리

--- src/components/press-header.js
+++ src/components/press-header.js
@@ -1,6 +1,10 @@
+import { HEADER_CLASS, PATH, TITLE, STYLE } from '../constants/press-header.js';
 import { setTotalPressGrid } from './press-grid.js';
 import { setTotalPressList } from './press-list.js';
 
+/**
+ * 언론사의 INIT
+ */
 const initPressHeader = () => {
   setNav(getNavLeft());
   setNav(getNavRight());
@@ -11,33 +15,37 @@ const initPressHeader = () => {
 };
 
 const setNav = (pressElement) => {
-  const pressHeader = document.querySelector('.press__header');
+  const pressHeader = document.querySelector(`.${HEADER_CLASS.PRESS_HEADER}`);
   pressHeader.insertAdjacentHTML('beforeend', pressElement);
 };
 
 const getNavLeft = () => {
   return `
-    <nav class='press__nav-left'>
-      <h2 class='press__h2-entire'>전체 언론사</h2>
-      <h2 class='press__h2-subscribe'>내가 구독한 언론사</h2>
+    <nav class=${HEADER_CLASS.NAV_LEFT}>
+      <h2 class=${HEADER_CLASS.H2_ENTIRE}>${TITLE.ENTIRE}</h2>
+      <h2 class=${HEADER_CLASS.H2_SUBSCRIBE}>${TITLE.SUBSCRIBE}</h2>
     </nav>
   `;
 };
 
 const getNavRight = () => {
   return `
-    <nav class='press__nav-right'>
-      <img class='press__img-list' src='./assets/icons/list-hide.svg' />
-      <img class='press__img-grid' src='./assets/icons/grid-view.svg' />
+    <nav class=${HEADER_CLASS.NAV_RIGHT}>
+      <img class=${HEADER_CLASS.IMG_LIST} src=${PATH.HIDE_LIST_ICON} />
+      <img class=${HEADER_CLASS.IMG_GRID} src=${PATH.GRID_ICON}  />
     </nav>
   `;
 };
 
+/**
+ * 언론사의 헤더 설정
+ */
+
 const setNavImg = () => {
-  const imgList = document.querySelector('.press__img-list');
-  const imgGrid = document.querySelector('.press__img-grid');
-  const gridWrapper = document.querySelector('.arrows__wrapper-grid');
-  const listWrapper = document.querySelector('.arrows__wrapper-list');
+  const imgList = document.querySelector(`.${HEADER_CLASS.IMG_LIST}`);
+  const imgGrid = document.querySelector(`.${HEADER_CLASS.IMG_GRID}`);
+  const gridWrapper = document.querySelector(`.${HEADER_CLASS.WRAPPER_GRID}`);
+  const listWrapper = document.querySelector(`.${HEADER_CLASS.WRAPPER_LIST}`);
 
   setNavList(imgList, imgGrid, gridWrapper, listWrapper);
   setNavGrid(imgList, imgGrid, gridWrapper, listWrapper);
@@ -45,19 +53,19 @@ const setNavImg = () => {
 
 const setNavList = (imgList, imgGrid, gridWrapper, listWrapper) => {
   imgList.addEventListener('click', () => {
-    imgList.src = './assets/icons/list-view.svg';
-    imgGrid.src = './assets/icons/grid-hide.svg';
-    gridWrapper.classList.add('none');
-    listWrapper.classList.remove('none');
+    imgList.src = PATH.LIST_ICON;
+    imgGrid.src = PATH.HIDE_GRID_ICON;
+    gridWrapper.classList.add(STYLE.NONE);
+    listWrapper.classList.remove(STYLE.NONE);
   });
 };
 
 const setNavGrid = (imgList, imgGrid, gridWrapper, listWrapper) => {
   imgGrid.addEventListener('click', () => {
-    imgGrid.src = './assets/icons/grid-view.svg';
-    imgList.src = './assets/icons/list-hide.svg';
-    listWrapper.classList.add('none');
-    gridWrapper.classList.remove('none');
+    imgGrid.src = PATH.GRID_ICON;
+    imgList.src = PATH.HIDE_LIST_ICON;
+    listWrapper.classList.add(STYLE.NONE);
+    gridWrapper.classList.remove(STYLE.NONE);
   });
 };
 
---
+++ src/constants/press-header.js
@@ -0,0 +1,29 @@
+const HEADER_CLASS = {
+  PRESS_HEADER: 'press__header',
+  NAV_LEFT: 'press__nav-left',
+  NAV_RIGHT: 'press__nav-right',
+  H2_ENTIRE: 'press__h2-entire',
+  H2_SUBSCRIBE: 'press__h2-subscribe',
+  IMG_LIST: 'press__img-list',
+  IMG_GRID: 'press__img-grid',
+  WRAPPER_GRID: 'arrows__wrapper-grid',
+  WRAPPER_LIST: 'arrows__wrapper-list',
+};
+
+const PATH = {
+  LIST_ICON: './assets/icons/list-view.svg',
+  GRID_ICON: './assets/icons/grid-view.svg',
+  HIDE_LIST_ICON: './assets/icons/list-hide.svg',
+  HIDE_GRID_ICON: './assets/icons/grid-hide.svg',
+};
+
+const TITLE = {
+  ENTIRE: '전체 언론사',
+  SUBSCRIBE: '내가 구독한 언론사',
+};
+
+const STYLE = {
+  NONE: 'none',
+};
+
+export { HEADER_CLASS, PATH, TITLE, STYLE };
