bcf68d04b4220b96530b3577bc9444aa30d302be
ori5r2 <ori5ri512@gmail.com>
2023-07-17 01:24:28 +0900

    refactor: grid_view_info closure적용

--- src/components/grid/gridToggle.js
+++ src/components/grid/gridToggle.js
@@ -3,28 +3,31 @@ import { press_list } from "../../../data/pressList.js";
 import { class_name } from "../../utils/domClassName.js";
 
 // grid view page 정보
-export const grid_view_info = {
-    current_page: 0,
-    shuffle_press_list: press_list.slice().sort(() => Math.random() - 0.5),
+export const grid_view_info = (function () {
+    let current_page = 0;
+    const shuffle_press_list = press_list.slice().sort(() => Math.random() - 0.5);
 
-    // 클래스 or return 할 수 있도록 바꾸기!
-    getCurrentPage: function () {
-        return this.current_page;
-    },
-    setPage: function (isRight) {
-        isRight ? (this.current_page += 1) : (this.current_page -= 1);
-    },
-    getShuffleList: function () {
-        return this.shuffle_press_list;
-    },
-};
+    function getCurrentPage() {
+        return current_page;
+    }
+
+    function getShuffleList() {
+        return shuffle_press_list;
+    }
+
+    function setPage(isRight) {
+        isRight ? (current_page += 1) : (current_page -= 1);
+    }
+
+    return { getCurrentPage, getShuffleList, setPage };
+})();
 
 // 페이지 넘길 때 첫번째 마지막 페이지 화살표 숨김
 export function toggleArrow() {
     const left_btn = document.querySelector(`.${class_name.GRID_LEFT_BTN}`);
     const right_btn = document.querySelector(`.${class_name.GRID_RIGHT_BTN}`);
 
-    switch (grid_view_info.current_page) {
+    switch (grid_view_info.getCurrentPage()) {
         case 0:
             left_btn.style.visibility = "hidden";
             right_btn.style.visibility = "visible";
