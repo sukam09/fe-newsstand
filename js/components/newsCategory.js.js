df77e8e45bdfbebbc6e068c1966832e75c423641
박태규 Park Tae Kyu <ptq124@naver.com>
2023-07-17 11:23:07 +0900

    Feat: 프로그래시브바 버튼 기반 이동

--- js/components/newsCategory.js.js
+++ js/components/newsCategory.js.js
@@ -1,12 +1,11 @@
 import { html } from '../core/createElement.js';
-function createCategoryHtml(categorys, CURRENT_INDEX, KEY) {
+function createCategoryHtml(categorys, NEWCATEGORY, CURRENT_INDEX, KEY) {
   const categoryCount = categorys.filter((name) => name === KEY).length;
-  const newCategorys = categorysParser(categorys);
   let htmls = '';
-  newCategorys.map((category) => {
+  NEWCATEGORY.map((category) => {
     if (category === KEY) {
       htmls += html`
-        <div class="category-item select-category">${category} <span></span> ${CURRENT_INDEX}/${categoryCount}</div>
+        <div class="category-item select-category">${category} <span></span> ${CURRENT_INDEX + 1}/${categoryCount}</div>
       `;
     } else {
       htmls += html` <div class="category-item">${category}</div> `;
@@ -16,7 +15,4 @@ function createCategoryHtml(categorys, CURRENT_INDEX, KEY) {
   document.querySelector('.newsstand__category').insertAdjacentHTML('beforeend', htmls);
 }
 
-function categorysParser(datas) {
-  return [...new Set(datas)];
-}
 export { createCategoryHtml };
