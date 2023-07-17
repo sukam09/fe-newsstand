e34fee2271229a8d2ebdf3a4f4322886820e3dae
seokhun <tommya98@seokhunui-MacBookPro.local>
2023-07-17 17:16:23 +0900

    feat: 구독/해지 여부 확인 함수 구현 (#3)

--- module/subscribe.js
+++ module/subscribe.js
@@ -1,5 +1,11 @@
+import { GLOBAL } from "./variable.js";
+
 function checkSubscribe(src) {
-  return false;
+  for (let news of GLOBAL.NEWS_DATA) {
+    if (news.path.slice(-6) === src.slice(-6)) {
+      return news.is_subscribe === "true" ? true : false;
+    }
+  }
 }
 
 function toggleSubscribe(event) {}
