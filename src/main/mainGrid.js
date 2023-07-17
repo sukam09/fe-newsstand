3d5edd5fb4d18a42e85f19b08c4af2344917efdf
hb9901 <hb9901@gmail.com>
2023-07-17 18:35:21 +0900

    update: 구독버튼기능 (오류있음)

--- src/main/mainGrid.js
+++ src/main/mainGrid.js
@@ -2,15 +2,10 @@ import { shuffled_data } from "../../data/shuffled_data.js";
 import articleState from "../state/articleState.js";
 
 const COUNT_PER_PAGE = 24;
-const logoAll = shuffled_data;
 let press;
 
-function logoMouseOver(e){
-    press = e.target.children[0].className;
+function addSubscribeBtn(e){
     const subscribedData = articleState.getSubscribe();
-
-    e.target.innerHTML = "";
-    e.target.style.backgroundColor = "#F5F7F9";
     const subscribeBtn = document.createElement("button");
 
     if(subscribedData.indexOf(shuffled_data[press]) >= 0){
@@ -19,9 +14,10 @@ function logoMouseOver(e){
     else{
         subscribeBtn.innerText = "+ 구독하기";
     }
+
     subscribeBtn.classList.add("gridSubscribeBtn");
+
     subscribeBtn.addEventListener("click", (e) => {
-        console.log(subscribedData);
         if(subscribedData.indexOf(shuffled_data[press]) >= 0){
             e.target.innerText = "+ 구독하기";
             articleState.removeSubscribe(shuffled_data[press]);
@@ -31,31 +27,35 @@ function logoMouseOver(e){
             articleState.addSubscribe(shuffled_data[press]);
         }
     });
-    if(press % 24 >= 18 && press % 24 <= 23){
-        e.target.style.borderBottom = "1px solid black";
-    }
-    if(press % 6 === 5){
-        e.target.style.borderRight = "1px solid black";
-    }
     e.target.appendChild(subscribeBtn);
+}
+
+function logoMouseOver(e){
+    //마우스 오버한 언론사 선택
+    press = e.target.children[0].className;
+
+    //초기화
+    e.target.innerHTML = "";
+    e.target.style.backgroundColor = "#F5F7F9";
+
+    addSubscribeBtn(e);
 }
 
 function logoMouseOut(e){
     e.target.innerHTML = "";
     e.target.style.backgroundColor = "white";
+
     const newsLogo = document.createElement("img");
     newsLogo.src = `${shuffled_data[press].logo}`;
     newsLogo.classList.add(press);
-    if(press % 24 >= 18 && press % 24 <= 23){
-        e.target.style.borderBottom = "1px solid black";
-    }
-    if(press % 6 === 5){
-        e.target.style.borderRight = "1px solid black";
-    }
+
     e.target.appendChild(newsLogo);
 }
 
-function refreshGridAll(currentPageNumber){
+function refreshGrid(isAll, currentPageNumber){
+    let pressData;
+    isAll === "all" ? pressData = shuffled_data : pressData = articleState.getSubscribe();
+
     const mainCenter = document.getElementById("main-center");
     const mainGrid = document.createElement("div");
     mainGrid.id = "main-grid"
@@ -63,56 +63,28 @@ function refreshGridAll(currentPageNumber){
     mainCenter.style.border = '0px';
     mainCenter.appendChild(mainGrid);
     mainGrid.innerHTML='';
-    mainGrid.style.borderBottom = "1px solid black";
-    mainGrid.style.borderRight = "1px solid black";
 
     for(let PAGE_INDEX = (currentPageNumber-1) * COUNT_PER_PAGE; PAGE_INDEX < COUNT_PER_PAGE * (currentPageNumber-1) + 24 ; PAGE_INDEX++){
         const outerDiv = document.createElement("div");
-        const newsLogo = document.createElement("img");
-        newsLogo.src = `${shuffled_data[PAGE_INDEX].logo}`;
-        newsLogo.classList.add(PAGE_INDEX);
-        outerDiv.append(newsLogo);
+        outerDiv.classList.add(`outerDiv${PAGE_INDEX}`);
         mainGrid.append(outerDiv);
-        
-        //마우스 오버시 구독하기/해지하기 
-        outerDiv.addEventListener("mouseenter", (e) => logoMouseOver(e));
-        outerDiv.addEventListener("mouseleave", (e) => logoMouseOut(e));
     }
-}
 
+    pressData.forEach((value, index) => {
+        if(index >= (currentPageNumber -1) * COUNT_PER_PAGE && index < (currentPageNumber) * COUNT_PER_PAGE){
+            const outerDiv = document.querySelector(`.outerDiv${index}`);
+            const newsLogo = document.createElement("img");
+            newsLogo.src = `${value.logo}`;
+            newsLogo.classList.add(index);
+            outerDiv.append(newsLogo);
 
-function refreshGridSubscribe(currentPageNumber){
-    const subscribedData = articleState.getSubscribe();
-    const mainCenter = document.getElementById("main-center");
-    const mainGrid = document.createElement("div");
-    mainGrid.id = "main-grid"
-    mainCenter.innerHTML = '';
-    mainCenter.style.border = '0px';
-    mainCenter.appendChild(mainGrid);
-    mainGrid.innerHTML='';
-    mainGrid.style.borderBottom = "1px solid black";
-    mainGrid.style.borderRight = "1px solid black";
-
-    for(let PAGE_INDEX = (currentPageNumber-1) * COUNT_PER_PAGE; PAGE_INDEX < COUNT_PER_PAGE * (currentPageNumber-1) + 24 ; PAGE_INDEX++){
-        const outerDiv = document.createElement("div");
-        const newsLogo = document.createElement("img");
-        newsLogo.src = `${subscribedData[PAGE_INDEX].logo}`;
-        newsLogo.classList.add(PAGE_INDEX);
-        outerDiv.append(newsLogo);
-        mainGrid.append(outerDiv);
-        
-        //마우스 오버시 구독하기/해지하기 
-        outerDiv.addEventListener("mouseenter", (e) => logoMouseOver(e));
-        outerDiv.addEventListener("mouseleave", (e) => logoMouseOut(e));
-    };
+            //마우스 오버시 구독하기/해지하기 
+            outerDiv.addEventListener("mouseenter", (e) => logoMouseOver(e));
+            outerDiv.addEventListener("mouseleave", (e) => logoMouseOut(e));
+        }   
+    })
 }
 
 export default function MainGrid(isAll, currentPageNumber){
-    console.log(isAll);
-    if(isAll === "all"){
-        refreshGridAll(currentPageNumber);
-    }
-    else if(isAll === "subscribe"){
-        refreshGridSubscribe(currentPageNumber);
-    }
+    refreshGrid(isAll, currentPageNumber);
 }
