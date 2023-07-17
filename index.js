7ed64ff3538b6cb5cbaed76fc0053dbf4ace9651
KimJungHun <j7papa@naver.com>
2023-07-17 15:59:58 +0900

    [Feat] 언론사, 구독하기 버튼 전환 기능 구현

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import App from "./src/App.js";
+// import json from "./listView.js";
 
 const root = document.querySelector(".root");
 new App(root, "light");
--- src/App/Main/MainContent/PressGridView.js
+++ src/App/Main/MainContent/PressGridView.js
@@ -8,10 +8,56 @@ const GRID_PRESS_NUBER = 24;
 const indexArr = Array.from({ length: TOTAL_PRESS_NUMBER }, (_, i) => i);
 indexArr.sort(() => Math.random() - 0.5);
 
+const findTargetParentNode = (element, targetTagName) => {
+  if (!element) {
+    return null;
+  }
+
+  if (element.tagName === targetTagName) {
+    return element;
+  }
+
+  return findTargetParentNode(element.parentNode, targetTagName);
+};
+
+const findTargetChildNode = (element, targetTagName) => {
+  if (!element) return null;
+
+  const children = Array.from(element.children);
+
+  for (const child of children) {
+    if (child.tagName.toLowerCase() === targetTagName.toLowerCase()) {
+      return child;
+    }
+
+    const matchedElement = findTargetChildNode(child, targetTagName);
+    if (matchedElement) return matchedElement;
+  }
+
+  return null;
+};
+
+const viewSubscriptionButton = (e) => {
+  let target = e.target;
+  let img, btn;
+
+  target = findTargetParentNode(target, "LI");
+  img = findTargetChildNode(target, "img");
+  btn = findTargetChildNode(target, "button");
+
+  if (img) img.style.display = "none";
+  if (btn) btn.style.display = "inline";
+};
+
 const createNewspaperItem = function (index, mode) {
   return `
     <li class="newspaper__item">
     <img src="./assets/newspaper/${mode}/${index + 1}.png" alt=${"name"} />
+    <button class="subscribe-btn" data-key=${
+      index + 1
+    }><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none">
+    <path d="M19 12.998H13V18.998H11V12.998H5V10.998H11V4.99799H13V10.998H19V12.998Z" fill="#879298"/>
+    </svg>구독하기</button>
     </li>
     `;
 };
@@ -32,7 +78,20 @@ export default function PressGridView($target, props) {
     const $ul = document.createElement("ul");
 
     $ul.setAttribute("class", "newspaper__list");
+
+    $ul.addEventListener("mouseover", viewSubscriptionButton);
+
     $ul.innerHTML = createPressList(props.currentPage, props.mode);
+
+    $ul.childNodes.forEach((elem) => {
+      elem.addEventListener("mouseout", (e) => {
+        const img = findTargetChildNode(e.target, "img");
+        const btn = findTargetChildNode(e.target, "button");
+
+        if (img) img.style.display = "inline";
+        if (btn) btn.style.display = "none";
+      });
+    });
 
     $target.innerHTML = "";
     $target.appendChild($ul);
