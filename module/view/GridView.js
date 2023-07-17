4c8847868272b8e1138c28f2ece5944aaa1271df
hwangJN <wlsk401@gmail.com>
2023-07-17 13:34:53 +0900

    feat: 그리드뷰 hover 시 구독하기 버튼 생성

--- module/view/GridView.js
+++ module/view/GridView.js
@@ -4,17 +4,48 @@ import { current_grid_page } from "../components/Arrow.js";
 let news_icon;
 const ICONS_PER_PAGE = 24;
 
+function createSubscribeDiv(li) {
+  const subscribeModeDiv = `
+  <div class="subscribe-wrap">
+
+    <button class="subscribe-btn">
+        <img class="plus-btn" src="../../asset/button/plus.png">
+        <span class="scribe-text available-medium12">구독하기</span>
+    </button>
+  </div>`;
+
+  li.innerHTML += subscribeModeDiv;
+}
+
+function pressMouseOver(li) {
+  const subscribeMode = li.querySelector("div");
+  if (!subscribeMode) {
+    createSubscribeDiv(li);
+    const pressLogo = li.querySelector(".press-logo");
+    pressLogo.classList.add("hidden");
+  }
+}
+function pressMouseOut(li) {
+  const subscribeModeDiv = li.querySelector("div");
+  if (subscribeModeDiv) {
+    li.removeChild(subscribeModeDiv);
+    const pressLogo = li.querySelector(".press-logo");
+    pressLogo.classList.remove("hidden");
+  }
+}
+
 export function updateGrid() {
   try {
     if (news_icon) {
       let icon_idx = current_grid_page * ICONS_PER_PAGE;
       const grid_row = document.querySelectorAll(".grid ul");
-
       grid_row.forEach((ul) => {
         const grid_li = ul.querySelectorAll("li");
         grid_li.forEach((li) => {
           const press_logo = li.querySelector(".press-logo");
           press_logo.src = news_icon[icon_idx++].path;
+          li.addEventListener("mouseenter", () => pressMouseOver(li));
+          li.addEventListener("mouseleave", () => pressMouseOut(li));
         });
       });
     } else {
@@ -45,8 +76,8 @@ export async function printGrid() {
         grid_row.appendChild(grid_li);
       }
       grid.appendChild(grid_row);
-      updateGrid();
     }
+    updateGrid();
     const left_btn = document.querySelector(".left-btn");
     left_btn.style.display = "none";
   } catch (e) {
