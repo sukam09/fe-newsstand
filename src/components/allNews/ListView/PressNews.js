78f6e6c33e7cd71f4f0f8a60cea875bd50d6bf4a
lee-yun-pyo <yun2193@hanmail.net>
2023-07-18 23:41:01 +0900

    chore: 로고 이미지 파일 이름 변경

--- src/components/allNews/ListView/PressNews.js
+++ src/components/allNews/ListView/PressNews.js
@@ -1,30 +1,61 @@
-import mainNews from "../../../data/mainNews.js";
+import CategoriesParent from "./CategoriesParent.js";
 import PressInfo from "./PressInfo.js";
 import PressMain from "./PressMain.js";
 
 export default class PressNews {
-  constructor() {
+  constructor(pressData) {
+    // super(pressData, 0);
     this.$wrapper = document.createElement("main");
     this.$wrapper.className = "press-news";
+    this.page = 0;
 
-    this.mainNews = mainNews[0].data[0];
+    this.pressData = pressData;
+    this.mainNews = this.pressData[this.page];
+    //this.currentCategory = this.getCategoryName.call(new CategoriesParent());
 
-    this.render();
-
-    return this.$wrapper;
+    // this.filterData();
+    this.nextPage();
+    //return this.$wrapper;
   }
 
   render() {
+    this.$wrapper.replaceChildren();
+
     this.$wrapper.appendChild(
-      new PressInfo(this.mainNews.logoSrc, this.mainNews.editTime)
+      new PressInfo(this.mainNews.logo, this.mainNews.editTime)
     );
     this.$wrapper.appendChild(
       new PressMain(
-        this.mainNews.imgSrc,
+        this.mainNews.mainArticle.thumbnail,
         this.mainNews.name,
-        this.mainNews.mainTitle,
-        this.mainNews.subTitleList
+        this.mainNews.mainArticle.title,
+        this.mainNews.subArticles
       )
     );
   }
+
+  nextPage(page) {
+    this.page = page;
+    this.render();
+  }
+
+  // goNextNews() {
+  //   // 현재 페이지 가져오기
+  //   const currentPage = new CategoriesParent().categories[this.currentCategory]
+  //     .currentPage;
+  //   this.mainNews = this.pressData[currentPage - 1];
+  //   this.render();
+  // }
+
+  // getCategoryName() {
+  //   return Object.keys(this.categories)[this.currentCategory];
+  // }
+
+  // filterData() {
+  //   this.pressData = this.pressData.filter(
+  //     (v) => v["category"] === this.currentCategory
+  //   );
+  // }
 }
+// 각 카테고리 이름과 현재 페이지를 가져와서 바뀔때마다  mainNews 바꿔줌 -> render() 해주면
+// render 인자로 mainNews 를 넘김
