3f8ff8140ddda712c1b9f7687e2d0fbddc0f60cd
sukam09 <sukam09@naver.com>
2023-07-18 11:16:01 +0900

    refactor: response 변수 풀네임으로 변경

--- src/api.js
+++ src/api.js
@@ -1,19 +1,19 @@
 export async function fetchNewsPress() {
-  const res = await fetch('../data/list-view.json');
-  const json = await res.json();
+  const response = await fetch('../data/list-view.json');
+  const json = await response.json();
   return json;
 }
 
 export async function fetchListView(index, present) {
-  const res = await fetch('../data/list-view.json');
-  const json = await res.json();
+  const response = await fetch('../data/list-view.json');
+  const json = await response.json();
   const { materials, name, regDate } = json[index][present];
   return { materials, name, regDate };
 }
 
 export async function getPressLogo(name) {
-  const res = await fetch('../data/press-info.json');
-  const json = await res.json();
+  const response = await fetch('../data/press-info.json');
+  const json = await response.json();
   const { logo } = json.find(pressInfo => pressInfo.name === name);
   return logo;
 }
