a2377d419958611c289247c859d6dd4b457b3bbd
wonho1401 <wonho1401@naver.com>
2023-07-18 11:05:03 +0900

    feat: re-render when click subscription button

--- components/Grid/gridElement.js
+++ components/Grid/gridElement.js
@@ -1,64 +1,55 @@
-// Grid 내에 list를 추가
-
 import { dispatcher } from "../../store/dispatcher.js";
 import { store } from "../../store/store.js";
 
 // 구독 or 해지 버튼 누르고 버튼 다시 렌더시키는 것 필요
 
-// const render = () => {
-//   const $agency_btn_hover = document.querySelector(".agency-btn-hover");
-// };
-
 export const makeGrid = (data) => {
   const li = document.createElement("li");
 
   const img = document.createElement("img");
+  img.src = data.logo;
+
   li.appendChild(img);
 
-  const btn = document.createElement("button");
-
-  const sr_only = document.createElement("span");
-  sr_only.className = "screen-reader-only";
-  sr_only.innerText = data.subscribe ? "해지하기" : "구독하기";
-
-  if (store.subscriptions.find((item) => item.name === data.name).subscribe) {
-    const $minus_btn = document.createElement("img");
-    $minus_btn.className = "minus";
-    $minus_btn.src = "./asset/icon/closed.svg";
-    $minus_btn.alt = "minus";
-
-    const $subscribe = document.createElement("div");
-    $subscribe.className = "subscribe-text";
-    $subscribe.innerText = "해지하기";
-
-    btn.appendChild(sr_only);
-    btn.appendChild($minus_btn);
-    btn.appendChild($subscribe);
-  } else {
-    const $plus_btn = document.createElement("img");
-    $plus_btn.className = "plus";
-    $plus_btn.src = "./asset/icon/plus.svg";
-    $plus_btn.alt = "plus";
-
-    const $subscribe = document.createElement("div");
-    $subscribe.className = "subscribe-text";
-    $subscribe.innerText = "구독하기";
-
-    btn.appendChild(sr_only);
-    btn.appendChild($plus_btn);
-    btn.appendChild($subscribe);
-  }
-
-  btn.className = "agency-btn-hover";
-  btn.id = data.name;
-
   li.addEventListener("mouseenter", () => {
-    li.appendChild(btn);
+    li.appendChild(createButton(data)); // Show the button when hovering
   });
 
   li.addEventListener("mouseleave", () => {
-    li.removeChild(btn);
+    const btn = document.querySelector(`#${data.name}`);
+    if (btn) li.removeChild(btn); // Remove the button when not hovering
   });
+
+  document.querySelector(".agency-grid").appendChild(li);
+};
+
+// 호버 시 나타날 구독 or 해지 버튼 생성
+
+const createButton = (data) => {
+  const btn = document.createElement("button");
+
+  const isSubscribed = store.subscriptions.find(
+    (item) => item.name === data.name
+  ).subscribe;
+
+  const sr_only = document.createElement("span");
+  sr_only.className = "screen-reader-only";
+  sr_only.innerText = isSubscribed ? "해지하기" : "구독하기";
+
+  const icon = document.createElement("img");
+  icon.alt = isSubscribed ? "minus" : "plus";
+  icon.src = isSubscribed ? "./asset/icon/closed.svg" : "./asset/icon/plus.svg";
+
+  const $subscribe = document.createElement("div");
+  $subscribe.className = "subscribe-text";
+  $subscribe.innerText = isSubscribed ? "해지하기" : "구독하기";
+
+  btn.appendChild(sr_only);
+  btn.appendChild(icon);
+  btn.appendChild($subscribe);
+
+  btn.className = "agency-btn-hover";
+  btn.id = `${data.name}`;
 
   btn.addEventListener("click", (e) => {
     dispatcher({
@@ -67,10 +58,26 @@ export const makeGrid = (data) => {
       value: !store.subscriptions.find((item) => item.name === e.target.id)
         .subscribe,
     });
-    console.log(store);
+    updateButton(data.name);
   });
+  return btn;
+};
 
-  img.src = data.logo;
+// 구독 or 해지 버튼 클릭 시, dispatcher를 통해 구독 상태를 변화 시키고 구독 상태에 따라 버튼을 변경하는 함수
 
-  document.querySelector(".agency-grid").appendChild(li);
+const updateButton = (name) => {
+  const btn = document.getElementById(`${name}`);
+  if (btn) {
+    const data = store.subscriptions.find((item) => item.name === name);
+    const icon = btn.querySelector("img");
+    const $subscribe = btn.querySelector(".subscribe-text");
+    const sr_only = btn.querySelector(".screen-reader-only");
+
+    icon.alt = data.subscribe ? "minus" : "plus";
+    icon.src = data.subscribe
+      ? "./asset/icon/closed.svg"
+      : "./asset/icon/plus.svg";
+    $subscribe.innerText = data.subscribe ? "해지하기" : "구독하기";
+    sr_only.innerText = data.subscribe ? "해지하기" : "구독하기";
+  }
 };
