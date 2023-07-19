bbacd9e30cef0df6e657c3c296b06ac99f202aa5
sukam09 <sukam09@naver.com>
2023-07-19 11:44:43 +0900

    refactor: JSON 한꺼번에 가져오도록 API 함수 변경

--- src/api.js
+++ src/api.js
@@ -4,17 +4,8 @@ export async function fetchPressInfo() {
   return json;
 }
 
-export async function fetchListView(index, present) {
+export async function fetchListView() {
   const response = await fetch('../data/list-view.json');
   const json = await response.json();
-  const entire = json[index].length;
-  const { materials, pid, regDate } = json[index][present];
-  return { entire, materials, pid, regDate };
-}
-
-export async function fetchPressItem(pid) {
-  const response = await fetch('../data/press-info.json');
-  const json = await response.json();
-  const { name, logo } = json.find(item => item.id === pid);
-  return { name, logo };
+  return json;
 }
