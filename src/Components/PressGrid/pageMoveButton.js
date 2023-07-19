1c7afaddaf40fcab1af679838aae4f641dd1f346
choiseona <tjsdk617@naver.com>
2023-07-19 11:39:23 +0900

    Feat #4 #5: 그리드에서 구독하기

--- src/Components/PressGrid/pageMoveButton.js
+++ src/Components/PressGrid/pageMoveButton.js
@@ -1,5 +1,6 @@
 import { FIRST_PAGE_IDX, LEFT_UNDISPLAY, RIGHT_UNDISPLAY } from "../../constant.js";
-import {drawPressImg} from "./pressLogos.js"
+import { drawPressImg } from "./pressLogos.js"
+import { subscribePress } from "../../subscribeButton.js";
 
 const $pagePrevButton = document.querySelector('.left-button');
 const $pageNextButton = document.querySelector('.right-button');
@@ -8,7 +9,8 @@ let pageCnt = FIRST_PAGE_IDX;
 /**
  페이지 넘기는 버튼의 클릭 이벤트 핸들링
  */
- function turnPressPage(page) {
+function turnPressPage(page) {
+
   $pagePrevButton.addEventListener('click', (event) => handleClickTurner(event, page));
   $pageNextButton.addEventListener('click', (event) => handleClickTurner(event, page));
 }
@@ -16,17 +18,18 @@ let pageCnt = FIRST_PAGE_IDX;
 /**
  페이지 카운트 하기
  */
- function handleClickTurner(event, page) {
+function handleClickTurner(event, page) {
   const $sectionNewsList = document.querySelector('.press-lists');
   event.target.className.animVal === 'left-button' ? pageCnt-- : pageCnt++;
   drawPressImg(page, pageCnt)
   showPageTurner();
+  subscribePress();
 }
 
 /**
  페이지 넘기는 버튼 유무 설정
  */
- function showPageTurner() {
+function showPageTurner() {
   $pagePrevButton.style.display = pageCnt === LEFT_UNDISPLAY ? "none" : "block";
   $pageNextButton.style.display = pageCnt === RIGHT_UNDISPLAY ? "none" : "block"
 }
--- src/Components/PressGrid/pressLogos.js
+++ src/Components/PressGrid/pressLogos.js
@@ -1,15 +1,17 @@
 import { FIRST_PAGE_IDX, ONE_PRESS_CNT } from "../../constant.js";
 import { fetchPressInfos } from "../../dataFetch.js";
+import { subscribePress } from "../../subscribeButton.js";
 import turnPressPage from "./pageMoveButton.js";
+
 
 /**
  언론사 id 순서 랜덤화해서 각 페이지에 id할당
  */
 async function randomizeImgs() {
   const pressInfoArr = await fetchPressInfos();
-  const pressList = [...Array(0)].map(()=>[]);
-  const pressLogoPage = [...Array(4)].map(()=>[]);
-  
+  const pressList = [...Array(0)].map(() => []);
+  const pressLogoPage = [...Array(4)].map(() => []);
+
   pressInfoArr.forEach(pressInfo => {
     pressList.push({ "id": pressInfo["id"] })
   })
@@ -19,7 +21,7 @@ async function randomizeImgs() {
     const pageIndex = Math.floor(idx / ONE_PRESS_CNT);
     pressLogoPage[pageIndex].push(pressId);
   })
-  
+
   return pressLogoPage;
 }
 
@@ -29,8 +31,10 @@ async function randomizeImgs() {
 function drawPressImg(page, gridPageIndex) {
   const $sectionNewsList = document.querySelector('.press-lists');
   $sectionNewsList.innerHTML = `
-  ${page[gridPageIndex].map(arr => `<li><img class="pointer" src="./assets/logo/light/img${arr["id"]}.svg"</li>`).join('')};
-`
+  ${page[gridPageIndex].map(arr =>
+    `<li class="current-logos-container">
+    <img class="pointer current-logos" data-id=${arr["id"]} src="./assets/logo/light/img${arr["id"]}.svg"></li>`).join('')}
+    `
 }
 
 /**
@@ -40,6 +44,7 @@ async function initPressImg() {
   const page = await randomizeImgs();
   drawPressImg(page, FIRST_PAGE_IDX);
   turnPressPage(page);
+  subscribePress()
 }
 
 
