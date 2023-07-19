c857d2be846d49d0234775950bcc093b3edc3fe9
김은민 <menuin@gim-eunmin-ui-MacBookPro.local>
2023-07-19 21:56:47 +0900

    [fix] 롤링 수정

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 import { drawArrow, handleArrowClick } from "./script/arrow.js";
 import { drawDate } from "./script/date.js";
 import { drawPress } from "./script/grid-view.js";
-import rollingList from "./asset/data/rollingList.js";
-import { rollHeadline } from "./script/rolling-view.js";
 import { handleViewChange } from "./script/view-change.js";
 import {handleFilterChange} from "./script/filter-change.js"
 import { handleReload } from "./script/reload.js";
+import { rollHeadlineInit } from "./script/rolling-view.js";
 
-const rollingContent = document.querySelectorAll(".rolling-content");
 
 function init () {
     handleReload();
     drawDate();
     drawPress();
     drawArrow();
-    rollingContent.forEach((item, index) => {
-        const isFirstRoll = true;
-        rollHeadline(item, rollingList.length, index, isFirstRoll);
-    })
+    rollInit();
     handleArrowClick();
     handleViewChange();
     handleFilterChange();
--- script/rolling-view.js
+++ script/rolling-view.js
@@ -1,6 +1,8 @@
 import rollingList from "../asset/data/rollingList.js";
 
-function listenHeadlineHover(target){
+const rollingContent = document.querySelectorAll(".rolling-content");
+
+function handleHeadlineHover(target){
     target.addEventListener("mouseover", () => {
         target.classList.add("paused");
     })
@@ -8,38 +10,44 @@ function listenHeadlineHover(target){
         target.classList.remove("paused")
     })
 }
+
 function drawHeadline(target){
     target.innerHTML = "";
     rollingList.forEach((item) => {
         target.innerHTML += `<span>${item.title}</span>`
     })
     target.innerHTML += `<span>${rollingList[0].title}</span>` // repeat drawHeadline instantly when rolling-window shows the last headline
+
 }
-function rollHeadline(target, headlineIdx, sectionIdx, isFirstRoll) {
+
+function rollHeadline(target, headlineIdx) {
     if (headlineIdx >= rollingList.length){
         drawHeadline(target);
         headlineIdx = 0;
     }
-    const crntHeadline = target.children[headlineIdx];
-
-    // add 1 sec gap between left and right rolling sections
-    if (isFirstRoll && sectionIdx == 1){ // right rolling section (initial roll)
-        setTimeout(() => {
-            crntHeadline.classList.add("roll");
-        }, 1000);
-    } else if (sectionIdx == 0 || (!isFirstRoll && sectionIdx == 1)){ // left rolling section & right rolling section from second round roll
-        crntHeadline.classList.add("roll");
-    }
-    listenHeadlineHover(crntHeadline);
-    crntHeadline.addEventListener("animationend", () => {
-            headlineIdx++;
-            target.children[headlineIdx].classList.add("roll");
-            isFirstRoll = false
-            rollHeadline(target,headlineIdx, sectionIdx, isFirstRoll);
-    })
     
+    const crntHeadline = target.children[headlineIdx];
+    crntHeadline.classList.add("roll");
+    handleHeadlineHover(crntHeadline);
+
+    crntHeadline.addEventListener("animationend",() => {
+        headlineIdx++;
+        rollHeadline(target, headlineIdx);
+    })
+}
+
+function rollInit(){
+    rollingContent.forEach((item, index) => {
+        if (index == 0){
+            rollHeadline(item, rollingList.length);
+        } else if (index == 1){
+            item.innerHTML += `<span>${rollingList[0].title}</span>`
+            setTimeout(() => {
+                rollHeadline(item, rollingList.length);
+            }, 1000);
+        }
+    })
 }
 
 
-
-export {rollHeadline};
+export {rollInit};
