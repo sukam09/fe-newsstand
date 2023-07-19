f86fb1ba93c823461415593513f16a69fcfb7ab1
Jo <tockiib@gmail.com>
2023-07-19 11:15:38 +0900

    feat: 구독 리스트 드래그 시 페이지 전환 방지

--- script/components/ListNavItem.js
+++ script/components/ListNavItem.js
@@ -5,12 +5,19 @@ const ListNavItem = ({ title, selected, indicator, onClick, afterDelay }) => {
   const listNavItem = document.createElement('li');
   const listNavItemContent = document.createElement('div');
   const listNavItemName = document.createElement('div');
+  let mouse = { x: null, y: null };
 
   listNavItem.classList.add('list_view_select');
   listNavItemContent.classList.add('list_view_select_content', 'pointer');
   listNavItemName.classList.add('name');
   listNavItemName.innerText = title;
-  listNavItemContent.addEventListener('click', onClick);
+  listNavItemContent.addEventListener('mousedown', e => {
+    mouse = { x: e.clientX, y: e.clientY };
+  });
+  listNavItemContent.addEventListener('mouseup', e => {
+    if (mouse.x !== e.clientX || mouse.y !== e.clientY) return;
+    onClick();
+  });
   listNavItemContent.appendChild(listNavItemName);
   if (selected) {
     listNavItemName.classList.add('selected_bold14');
