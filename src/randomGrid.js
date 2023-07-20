476d7187c17c11b1a52f9837c27861a1524d930a
jhyep <pjh1115_@naver.com>
2023-07-20 23:47:44 +0900

    feat: alert 창 구현 + 이벤트 리스너 연결

--- src/randomGrid.js
+++ src/randomGrid.js
@@ -1,10 +1,11 @@
 import { getPressObj } from "./api/api.js";
-import { setDisplay } from "./util/utils.js";
+import { subscribeState } from "./store/subscribeState.js";
 
 const PRESS_NUM_IN_GRID = 24;
 const TOTAL_PRESS_NUM = 96;
 let grid_page_count = 0;
 let is_light_mode = true;
+const unsub_btn = "../assets/others/unsubButton.svg";
 
 /***** 언론사 사진 셔플 *****/
 let presses;
@@ -20,16 +21,16 @@ function appendPressInGrid(press) {
   //언론사 이미지 추가
   const $image = document.createElement("img");
   $image.src = is_light_mode ? `${press.lightSrc}` : `${press.darkSrc}`;
-  $image.classList.add("original");
+  $image.classList.add("original", `${press.id}`);
 
   //구독하기 이미지 추가
   const $sub = document.createElement("img");
   $sub.src = `./assets/others/Button.svg`;
-  $sub.classList.add("sub");
+  $sub.classList.add("sub", `${press.id}`);
 
   //ul에 li 추가
   const $list = document.createElement("li");
-  $list.classList.add("press-item");
+  $list.classList.add("press-item", "full");
   $list.append($image, $sub);
   document.getElementById("press-list").appendChild($list);
 }
@@ -48,10 +49,10 @@ async function setGrid() {
 const grid_next = document.getElementById("grid-next");
 grid_next.addEventListener("click", () => {
   if (grid_page_count + 1 === TOTAL_PRESS_NUM / PRESS_NUM_IN_GRID - 1) {
-    setDisplay("#grid-next", "none");
+    grid_next.style.display = "none";
   }
   if (grid_page_count + 1 < TOTAL_PRESS_NUM / PRESS_NUM_IN_GRID) {
-    setDisplay("#grid-prev", "block");
+    document.getElementById("grid-prev").style.display = "block";
     document.getElementById("press-list").innerHTML = "";
     grid_page_count += 1;
     const slice_shuffled_presses = shuffled_presses.slice(
@@ -68,10 +69,10 @@ grid_next.addEventListener("click", () => {
 const grid_prev = document.getElementById("grid-prev");
 grid_prev.addEventListener("click", () => {
   if (grid_page_count - 1 === 0) {
-    setDisplay("#grid-prev", "none");
+    grid_prev.style.display = "none";
   }
   if (grid_page_count - 1 >= 0) {
-    setDisplay("#grid-next", "block");
+    document.getElementById("grid-next").style.display = "block";
     document.getElementById("press-list").innerHTML = "";
     grid_page_count -= 1;
     const slice_shuffled_presses = shuffled_presses.slice(
@@ -81,6 +82,31 @@ grid_prev.addEventListener("click", () => {
     slice_shuffled_presses.forEach((press) => {
       appendPressInGrid(press);
     });
+  }
+});
+
+async function getPressItemById(id) {
+  presses = await getPressObj();
+  const subPress = await Promise.all(
+    presses.filter((item) => item.id === parseInt(id))
+  );
+  return subPress;
+}
+
+const sub_btns = document.querySelector("#press-list");
+sub_btns.addEventListener("click", async (e) => {
+  const target = e.target;
+  const press_id = target.classList[1];
+  if (target.classList.contains("sub")) {
+    const subPress = await getPressItemById(press_id);
+    subscribeState.setSubscribeState(
+      press_id,
+      subPress[0].name,
+      subPress[0].lightSrc
+    );
+    target.src = unsub_btn;
+    target.classList.remove("sub");
+    target.classList.add("un-sub");
   }
 });
 
