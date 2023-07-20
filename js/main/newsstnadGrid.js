8f118273c3ce821ee1578d9711e7399f058a6d00
박태규 Park Tae Kyu <ptq124@naver.com>
2023-07-20 23:59:01 +0900

    Feat: 글로벌 스토어 개발

--- js/main/newsstnadGrid.js
+++ js/main/newsstnadGrid.js
@@ -1,80 +1,28 @@
 import { shuffle } from '../utils/utils.js';
 import { getNewsData } from '../core/apis.js';
 import { paintNewsstand } from '../components/newStandGrid.js';
-// import { attachEventListener, detachEventListener } from '../core/eventListener.js';
-// import { subScribeStore } from '../subScribeStore.js';
 import { globalStore } from '../store/globalVarStore.js';
-// let SELECTED_PAGE = 0;
-const $rightBtn = document.querySelector('.newsstand--right-btn');
-const $leftBtn = document.querySelector('.newsstand--left-btn');
+import { subScribeStore } from '../store/subScribeStore.js';
 let datas = [];
 
 async function initNewsStandGrid() {
   const newsData = await getNewsData();
-  datas = newsDataPaser(shuffle(newsData));
-  paintNewsstand(datas, globalStore.state.전체언론_그리드_인덱스);
-
-  // isSubscribe();
-  // SubScribeNews();
+  if (globalStore.state.OPTION === '전체_언론사') 전체_언론사(newsData);
+  else if (globalStore.state.OPTION === '내가_구독한_언론사') 내가_구독한_언론사(newsData);
 }
 
-// function SubScribeNews() {
-//   const back = document.querySelectorAll('.back');
-//   back.forEach((item) => item.addEventListener('click', (e) => isSubscribeIncludeHandler(e)));
+function 전체_언론사(newsData) {
+  datas = newsDataPaser(newsData);
+  paintNewsstand(datas, globalStore.state.전체언론_그리드_인덱스);
+}
 
-//   const isSubscribeIncludeHandler = (e) => {
-//     const tartgetParent = e.target.parentElement.parentElement;
-//     const data = tartgetParent.querySelector('.front').getAttribute('alt');
-
-//     if (!subScribeStore.getGetter('getsubscribeData').includes(data)) subScribeStore.commit('setState', data);
-//     else {
-//       subScribeStore.commit('updateState', data);
-//       tartgetParent.querySelector('.back').textContent = '+ 구독하기';
-//     }
-//   };
-// }
-
-// function isSubscribe() {
-//   const subscrtionBox = document.querySelectorAll('.newsstand—subscrtion-box');
-
-//   subscrtionBox.forEach((item) => {
-//     const inner = item.querySelector('.inner');
-//     item.addEventListener('mouseover', (e) => isIncludeSubScribeHandler(e));
-//     inner.addEventListener('mouseover', (e) => e.stopPropagation());
-//   });
-
-//   const isIncludeSubScribeHandler = (e) => {
-//     const currentTargetChildren = e.currentTarget.children[0];
-//     const data = currentTargetChildren.querySelector('.front').getAttribute('alt');
-//     if (subScribeStore.getGetter('getsubscribeData').includes(data))
-//       currentTargetChildren.querySelector('.back').textContent = '해제하기';
-//   };
-// }
-
-// function toggleGridEventListner(type) {
-//   if (type === 'attach') attachGridEvent();
-//   else if (type === 'detach') detachGridEvent();
-// }
-
-// function handleRightBtn() {
-//   ++SELECTED_PAGE;
-//   rePaintNewsStand();
-// }
-
-// function handleLeftBtn() {
-//   --SELECTED_PAGE;
-//   rePaintNewsStand();
-// }
-
-// function attachGridEvent() {
-//   attachEventListener('click', $rightBtn, handleRightBtn);
-//   attachEventListener('click', $leftBtn, handleLeftBtn);
-// }
-
-// function detachGridEvent() {
-//   detachEventListener('click', $rightBtn, handleRightBtn);
-//   detachEventListener('click', $leftBtn, handleLeftBtn);
-// }
+function 내가_구독한_언론사(newsData) {
+  const subscribeData = subScribeStore.state.subscribeData;
+  datas = newsData.filter((data) => subscribeData.includes(data.name));
+  datas = newsDataPaser(datas);
+  while (datas.length < 96) datas.push(['', '']);
+  paintNewsstand(datas, globalStore.state.구독언론_그리드_인덱스);
+}
 
 function newsDataPaser(datas) {
   return datas.map((data) => [data.name, data.src]);
--- js/store/globalVarStore.js
+++ js/store/globalVarStore.js
@@ -3,16 +3,57 @@ import { Store } from '../core/Store.js';
 const globalStore = new Store({
   state: {
     전체언론_그리드_인덱스: 0,
-    전체언론_리스트_인덱스: 0,
+    전체언론_리스트: {
+      뉴스_인덱스: 0,
+      카테고리_인덱스: 0,
+      전체카테고리: 0,
+    },
     구독언론_그리드_인덱스: 0,
     구독언론_리스트_인덱스: 0,
+    KEY: '전체언론_그리드_인덱스',
+    OPTION: '전체_언론사',
   },
   mutations: {
     nextIndex(state, payload) {
-      state[payload]++;
+      if (payload === '전체언론_리스트') {
+        state.전체언론_리스트.뉴스_인덱스++;
+      } else {
+        state[payload]++;
+      }
     },
     prevIndex(state, payload) {
-      state[payload]--;
+      if (payload === '전체언론_리스트') {
+        state.전체언론_리스트.뉴스_인덱스--;
+      } else {
+        state[payload]--;
+      }
+    },
+    nextCategoryIndex(state) {
+      state.전체언론_리스트.카테고리_인덱스++;
+      state.전체언론_리스트.뉴스_인덱스 = 0;
+    },
+    prevCategoryIndex(state) {
+      state.전체언론_리스트.카테고리_인덱스--;
+      state.전체언론_리스트.뉴스_인덱스 = 0;
+    },
+    updateKey(state, payload) {
+      state.KEY = payload;
+    },
+    updateOption(state, payload) {
+      state.OPTION = payload;
+    },
+    updateCateGoryCount(state, payload) {
+      state.전체언론_리스트.전체카테고리 = payload;
+    },
+    updateCategoryIndex(state, payload) {
+      state.전체언론_리스트.카테고리_인덱스 = payload;
+    },
+    resetNewsList(state) {
+      state.전체언론_리스트.카테고리_인덱스 = 0;
+      state.전체언론_리스트.뉴스_인덱스 = 0;
+    },
+    resetNewsIndex(state) {
+      state.전체언론_리스트.뉴스_인덱스 = 0;
     },
   },
 
