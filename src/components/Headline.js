12e1d4254973a8bab3bc02ac7318d05a72fcd10b
sukam09 <sukam09@naver.com>
2023-07-21 12:00:49 +0900

    refactor: early return을 if문으로 변경

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -1,44 +1,42 @@
 export default function Headline({ $target, initialState }) {
-  const $div = document.createElement("div");
-  $div.classList.add("recent-news-item");
+  const $div = document.createElement('div');
+  $div.classList.add('recent-news-item');
 
   $target.appendChild($div);
 
   this.state = initialState;
 
   const rollHeadline = () => {
-    const $prev = $div.querySelector(".prev");
-    const $current = $div.querySelector(".current");
-    const $next = $div.querySelector(".next");
+    const $prev = $div.querySelector('.prev');
+    const $current = $div.querySelector('.current');
+    const $next = $div.querySelector('.next');
 
-    $current.removeEventListener("mouseenter", handleMouseEnter);
-    $current.removeEventListener("mouseleave", handleMouseLeave);
+    $current.removeEventListener('mouseenter', handleMouseEnter);
+    $current.removeEventListener('mouseleave', handleMouseLeave);
 
-    $prev.classList.remove("prev");
+    $prev.classList.remove('prev');
 
-    $current.classList.remove("current");
-    $current.classList.add("prev");
+    $current.classList.remove('current');
+    $current.classList.add('prev');
 
-    $next.classList.remove("next");
-    $next.classList.add("current");
+    $next.classList.remove('next');
+    $next.classList.add('current');
 
     if ($next.nextElementSibling === null) {
-      const $first = $div.querySelector("li:first-child");
-      $first.classList.add("next");
+      const $first = $div.querySelector('li:first-child');
+      $first.classList.add('next');
     } else {
-      $next.nextElementSibling.classList.add("next");
+      $next.nextElementSibling.classList.add('next');
     }
 
-    $next.addEventListener("mouseenter", handleMouseEnter);
-    $next.addEventListener("mouseleave", handleMouseLeave);
+    $next.addEventListener('mouseenter', handleMouseEnter);
+    $next.addEventListener('mouseleave', handleMouseLeave);
   };
 
   const setHeadline = () => {
-    if (!this.isAutoRolling) {
-      return;
+    if (this.isAutoRolling) {
+      rollHeadline();
     }
-
-    rollHeadline();
   };
 
   this.isAutoRolling = true;
