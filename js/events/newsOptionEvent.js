21fb116f59e835dc42e63e01c7ec20fa6f1e4a43
박태규 Park Tae Kyu <ptq124@naver.com>
2023-07-21 18:28:18 +0900

    Feat: 내가 구독한 언론사 기능 추가

--- js/events/newsOptionEvent.js
+++ js/events/newsOptionEvent.js
@@ -6,11 +6,22 @@ function initOptionEvent() {
 }
 const optionHandler = (e) => {
   const option = e.target.textContent;
+
   if (option === '전체 언론사') {
     globalStore.commit('updateOption', '전체_언론사');
+    toggleClicked();
   } else if (option === '내가 구독한 언론사') {
     globalStore.commit('updateOption', '내가_구독한_언론사');
+    toggleClicked();
   }
   reRenderComponent('GRID_ALL');
 };
+
+const toggleClicked = () => {
+  const textClicked = document.querySelector('.newsstand—text-clicked');
+  const textunClicked = document.querySelector('.newsstand—text-unclicked');
+  textClicked.classList.replace('newsstand—text-clicked', 'newsstand—text-unclicked');
+  textunClicked.classList.replace('newsstand—text-unclicked', 'newsstand—text-clicked');
+};
+
 export { initOptionEvent };
