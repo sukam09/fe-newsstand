df6da9f9efd1e002ab0c048f5b2c859e689e2319
Jo <tockiib@gmail.com>
2023-07-21 18:25:55 +0900

    refactor: 쓰이지 않는 코드 제거

--- script/components/Button.js
+++ script/components/Button.js
@@ -21,37 +21,4 @@ const Button = ({ icon, isWhite, text, once, onClick }) => {
   );
   return buttonElement;
 };
-
-const newSubButton = (id, navStore, withText = true) => {
-  const store = new Store({
-    id,
-    subscribed,
-  });
-  const subscribed = navStore.getState().subscribed.includes(id);
-  const subButtonData = {
-    icon: 'plus',
-    isWhite: true,
-    text: BUTTON.SUBSCRIBE,
-    once: true,
-  };
-  const unsubButtonData = {
-    icon: 'close',
-    isWhite: false,
-    text: withText ? BUTTON.UNSUBSCRIBE : null,
-  };
-  const buttonData = subscribed ? unsubButtonData : subButtonData;
-
-  const draw = () => {
-    return Button({
-      ...buttonData,
-      onClick: () => {
-        store.setState({ subscribed: !subscribed });
-      },
-    });
-  };
-
-  store.subscribe(draw);
-  return draw();
-};
-
 export default Button;
