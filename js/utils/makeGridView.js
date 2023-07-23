ded45962e8d851b620c157bb2971511d0bb5fae5
김다예 <63107805+kimdaye77@users.noreply.github.com>
2023-07-23 22:04:29 +0900

    [FEAT] 그리드뷰 구독 상태에 따라 버튼 구현

--- js/utils/makeGridView.js
+++ js/utils/makeGridView.js
@@ -1,7 +1,6 @@
 import {
   PRESS_VIEW_COUNT,
   PRESS_LOGO_IMG_PATH,
-  ICON_IMG_PATH,
 } from "../constants/constants.js";
 import {
   getIndex,
@@ -10,14 +9,16 @@ import {
   getTabMode,
 } from "../core/getter.js";
 import { checkPage } from "./checkPage.js";
-import { handleSubscribe } from "./subscribePress.js";
+import { handleSubscribe, showSubscribeButton } from "./subscribePress.js";
 import { getData } from "../core/api.js";
 const grid_view = `
     <ul class="main-list-ul"></ul>
     `;
 
-function handleEvent(event, img, button) {
+function handleEvent(event, img) {
   const li = img.parentNode;
+  const button = img.nextElementSibling;
+  // const button = document.querySelector("button");
   switch (event) {
     case "over":
       img.style.display = "none";
@@ -60,6 +61,7 @@ export function showGridView() {
   - all 모드일 때
   - 2번에서 전체 언론사 갯수를 상수로 일단 잡아봄 
    */
+
   let list;
   getTabMode() === "all"
     ? (list = getIndex())
@@ -73,22 +75,21 @@ export function showGridView() {
     i < PRESS_VIEW_COUNT * getPage();
     i++
   ) {
+    const isSubscribed = getSubscribedPress().some(
+      (press) => press.index === list[i]
+    );
     const li = document.createElement("li");
-    const img = document.createElement("img");
+    let img = document.createElement("img");
     img.setAttribute("class", "logo-img");
-    const button = document.createElement("button");
-    button.setAttribute("class", "subscribe");
-    button.innerHTML = `
-    <img src="${ICON_IMG_PATH}plus.svg"/>
-    <span>구독하기</span>
-  `;
     main_list_ul.appendChild(li);
     if (list.length > i) {
       img.setAttribute("src", `${PRESS_LOGO_IMG_PATH}${list[i]}.svg`);
-      li.append(img, button);
-      li.addEventListener("mouseover", () => handleEvent("over", img, button));
-      li.addEventListener("mouseout", () => handleEvent("out", img, button));
-      button.addEventListener("click", () => handleEvent("click", img, button));
+      li.append(img);
+      li.innerHTML += showSubscribeButton(isSubscribed);
+      img = li.querySelector("img");
+      li.addEventListener("mouseover", () => handleEvent("over", img));
+      li.addEventListener("mouseout", () => handleEvent("out", img));
+      // button.addEventListener("click", () => handleEvent("click", img));
     } else {
       li.style.cursor = "default";
     }
--- js/utils/subscribePress.js
+++ js/utils/subscribePress.js
@@ -7,12 +7,12 @@ export function showSubscribeButton(isSubscribed) {
   return isSubscribed
     ? `
         <button class="sub cancel">
-          <img src="${ICON_IMG_PATH}/closed.svg" />
+          <img src="${ICON_IMG_PATH}closed.svg" />
         </button>
       `
     : `
         <button class="sub subscribe">
-          <img src="${ICON_IMG_PATH}/plus.svg" />
+          <img src="${ICON_IMG_PATH}plus.svg" />
           <span>구독하기</span>
         </button>
         `;
@@ -43,34 +43,37 @@ function checkAnswer(e, _press) {
     console.log("no");
   }
 }
-export function handleSubscribe(press) {
-  //   let btn_target = e.target.closest("button");
+export function handleSubscribe(_press) {
   const button = document.querySelector(".sub");
   const press_news = document.querySelector(".press-news");
+  const main_list = document.querySelector(".main-list");
   const newDiv = document.createElement("div");
 
-  let isSubscribed = getSubscribedPress().includes(press);
+  const isSubscribed = getSubscribedPress().some(
+    (press) => press.name === _press.name
+  );
   //구독한 상태에서 누를 경우
   if (isSubscribed) {
+    console.log("dd");
     newDiv.classList.add("popup", "alert");
     newDiv.innerHTML = `
-        <div class="message"><span class="press">${press.name}</span>을(를)\n구독해지하시겠습니까?</div>
+        <div class="message"><span class="press">${_press.name}</span>을(를)\n구독해지하시겠습니까?</div>
         <div class="buttons">
           <button class="btn-yes">예, 해지합니다</button>
           <button class="btn-no">아니오</button>
         </div>`;
     press_news.appendChild(newDiv);
     const btn = document.querySelector(".buttons");
-    btn.addEventListener("click", (e) => checkAnswer(e, press));
+    btn.addEventListener("click", (e) => checkAnswer(e, _press));
   }
   //구독하지 않았을 때 => 구독됨
   else {
-    const updatedSubscribedPress = [...getSubscribedPress(), press];
+    const updatedSubscribedPress = [...getSubscribedPress(), _press];
     store.setState({ subscribedPress: updatedSubscribedPress });
     //스낵바
-    // newDiv.classList.add("popup", "snackbar");
-    // newDiv.textContent = "내가 구독한 언론사에 추가되었습니다.";
-    // press_news.appendChild(newDiv);
+    newDiv.classList.add("popup", "snackbar");
+    newDiv.textContent = "내가 구독한 언론사에 추가되었습니다.";
+    main_list.appendChild(newDiv);
     //구독한 상태로 바뀜
     //버튼 변경
     button.innerHTML = showSubscribeButton(!isSubscribed);
