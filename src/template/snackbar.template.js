114877368ab05cd9943bb8c9f74756b779aa41d9
1lsang <ilsangpark.98@gmail.com>
2023-07-24 16:25:25 +0900

    Refactor: Remove unnecessary file & Move directory for some files

--- src/template/snackbar.template.js
+++
@@ -1,9 +0,0 @@
-import { html } from "../lib/html";
-/**
- * HTML 형태 Template Literal을 입력받아 Snackbar Template Literal을 return 합니다.
- * @param { HTMLString } page
- * @returns { HTMLString }
- */
-export const snackbarTemplate = (company) => html `
-  <div class="snack-bar popup">내가 구독한 언론사에 추가되었습니다.</div>
-`;
