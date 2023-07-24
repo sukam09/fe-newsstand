259ddb1301c97c6d0288408ed3341b06ad07abd2
meanz1 <zndnfjqbd1206@gmail.com>
2023-07-24 18:34:18 +0900

    [bug] 옵저버 패턴 적용중...

--- script/header/pageReload.js
+++ script/header/pageReload.js
@@ -1,11 +1,8 @@
-import { getQuerySelector } from "../../utils/js/getElements.js"
+import { getQuerySelector } from "../../utils/js/getElements.js";
 
 function pageReload() {
-  const newsLogo = getQuerySelector('#header-news-icon');
-  newsLogo.addEventListener('click', ()=> location.reload());
+  const newsLogo = getQuerySelector("#header-news-icon");
+  newsLogo.addEventListener("click", () => location.reload());
 }
 
-export {pageReload};
-
-
-
+export { pageReload };
--- script/pressContent/pressGridList.js
+++ script/pressContent/pressGridList.js
@@ -1,20 +1,18 @@
-import { getElemClass, getQuerySelector} from "../../utils/js/getElements.js";
+import { getQuerySelectorAll } from "../../utils/js/getElements.js";
 import { fetchData } from "../../utils/js/getJson.js";
+import { getState, register } from "../observer/observer.js";
+import { gridPageIdx } from "../store/store.js";
 
 const page = [[], [], [], []];
 
-const sectionPrevButton = getQuerySelector('#press-content-grid-prev');
-const sectionNextButton = getQuerySelector('#press-content-grid-next');
-const pressContentView = getElemClass('press-content-view');
-
-let pageNumber = 0;
+const pressContentView = getQuerySelectorAll(".press-content-view");
 
 // Json 객체로부터 받아오는 뉴스 데이터의 id값 랜덤 셔플 후 첫번째 페이지 구현
 async function shuffleImgs() {
   const imgPath = await fetchData("../assets/data/newspaperSrc.json");
   const imgId = imgPath.newsList.map((elem) => {
     return elem.id;
-  })
+  });
 
   const shuffledArray = [...imgId].sort(() => Math.random() - 0.5);
   shuffledArray.forEach((arr, idx) => {
@@ -25,30 +23,19 @@ async function shuffleImgs() {
   let imgSrcContent = "";
   page[0].forEach((elem) => {
     imgSrcContent += `<li><img src="../assets/images/pressLogo/light/img${elem}.svg"</li>`;
-  })
+  });
   pressContentView[0].innerHTML = imgSrcContent;
+  register(gridPageIdx, showPressImg);
 }
 
 // 각각의 페이지에 올바른 뉴스데이터 나타내기
-function showPressImg({pageValue}) {
-  const pressContentView = getElemClass('press-content-view');
-  pageNumber = (pageValue >= 0) ? ++pageNumber : --pageNumber;
-
-  sectionPrevButton.style.display = pageNumber !== 0 ? "block" : "none";
-  sectionNextButton.style.display = pageNumber >= 3 ? "none" : "block";
+function showPressImg() {
+  const nowGridIdx = getState(gridPageIdx);
   let imgSrcContent = "";
-  page[pageNumber].forEach((elem) => {
+  page[nowGridIdx].forEach((elem) => {
     imgSrcContent += `<li><img src="../assets/images/pressLogo/light/img${elem}.svg"</li>`;
-  })
+  });
   pressContentView[0].innerHTML = imgSrcContent;
 }
 
-// 이전 페이지 이동 및 다음 페이지 이동 구현
-function changePressGrid() {
-  sectionPrevButton.addEventListener('click', () => showPressImg({pageValue:-1}));
-  sectionNextButton.addEventListener('click', () => showPressImg({pageValue:1}));
-}
-
-
-
-export { shuffleImgs, changePressGrid };
+export { shuffleImgs };
