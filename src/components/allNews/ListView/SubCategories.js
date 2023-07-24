062af47523eab2c31c8b44a49b5a41feac78e918
lee-yun-pyo <yun2193@hanmail.net>
2023-07-24 22:50:30 +0900

    feat: 구독 리스트뷰 프로그래스바 구현

---
+++ src/components/allNews/ListView/SubCategories.js
@@ -0,0 +1,78 @@
+import { PROGRESS_SPEED } from "../../../constants/index.js";
+import pressName from "../../../constants/pressName.js";
+import { store } from "../../../core/store.js";
+import Icon from "../../common/Icon.js";
+
+export default class SubCategories {
+  constructor() {
+    this.$wrapper = document.createElement("div");
+    this.$wrapper.className = "categories-wrapper";
+
+    this.progressInterval;
+    this.currentCategory = 0;
+    this.subCategories = store.press;
+
+    this.handleProgress();
+  }
+
+  render() {
+    this.$wrapper.replaceChildren();
+
+    this.subCategories.forEach((id) => {
+      this.$wrapper.appendChild(this.createCategoryList(id));
+    });
+  }
+
+  /**
+   * 카테고리 리스트 생성
+   */
+  createCategoryList(pressId) {
+    const $categoryList = document.createElement("li");
+    const targetIndex = this.subCategories.findIndex((v) => v === pressId);
+    const isCurrentCategory = this.currentCategory === targetIndex;
+    const subPressName = pressName[pressId - 1][pressId];
+    $categoryList.classList.add(
+      "categories-list",
+      `${isCurrentCategory ? "category-current" : "notCurrent"}`
+    );
+
+    $categoryList.innerHTML += `<span class="category-name">${subPressName}</span>`;
+    $categoryList.appendChild(new Icon({ name: "chevron-right" }));
+    $categoryList.addEventListener("click", () =>
+      this.handleCategoryClick(pressId)
+    );
+
+    return $categoryList;
+  }
+
+  /**
+   * 프로그레스바 구현
+   */
+  handleProgress() {
+    this.render();
+    this.progressInterval = setInterval(() => {
+      this.currentCategory += 1;
+      if (this.currentCategory === this.subCategories.length) {
+        this.currentCategory = 0;
+      }
+      this.render();
+    }, PROGRESS_SPEED);
+  }
+
+  /**
+   * 다음 페이지로 이동
+   */
+  goNextNews() {
+    this.goNextPage();
+  }
+
+  /**
+   * 카테고리 클릭 시 카테고리 이동
+   */
+  handleCategoryClick(pressId) {
+    clearInterval(this.progressInterval);
+    const targetIndex = this.subCategories.findIndex((v) => v === pressId);
+    this.currentCategory = targetIndex;
+    this.handleProgress();
+  }
+}
--- src/components/allNews/ListView/SubListView.js
+++ src/components/allNews/ListView/SubListView.js
@@ -1,8 +1,21 @@
+import ArrowButton from "../ArrowButton.js";
+import SubCategories from "./SubCategories.js";
+
 export default class SubListView {
   constructor() {
     this.$wrapper = document.createElement("div");
+    this.$wrapper.className = "list-wrapper";
 
-    this.$wrapper.innerText = "구독한 언론사 리스트뷰";
+    this.render();
+
     return this.$wrapper;
+  }
+
+  render() {
+    const $container = document.createElement("div");
+    $container.classList.add("list-container");
+    $container.appendChild(new SubCategories().$wrapper);
+
+    this.$wrapper.appendChild($container);
   }
 }
