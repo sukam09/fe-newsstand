77964094acff6b0aafc21e75f0323663367ae528
westofsky <westofsky159@gmail.com>
2023-07-25 11:38:12 +0900

    style: 다크모드 스타일링 (#10)

--- script/main/mode.js
+++ script/main/mode.js
@@ -1,11 +1,13 @@
 const modeInit = () => {
   const checkbox = document.querySelector('#toggle');
-
+  document.documentElement.setAttribute('color-theme', 'light');
   checkbox.addEventListener('change', function() {
     if(this.checked) {
         console.log('체크박스 설정');
+        document.documentElement.setAttribute('color-theme', 'dark');
     } else {
         console.log('체크박스 해제');
+        document.documentElement.setAttribute('color-theme', 'light');
     }
   });
 }
