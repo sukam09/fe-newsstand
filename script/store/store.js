b36abefe441944f0f983e145571d0dc4ea5b3215
meanz1 <zndnfjqbd1206@gmail.com>
2023-07-25 18:38:54 +0900

    [Refactor]현재 기능개발까지(그리드뷰, 리스트뷰 보기)  옵저버 패턴 완성

--- script/store/store.js
+++ script/store/store.js
@@ -8,7 +8,7 @@ const isGrid = initState({
 // 배열 idx로 카테고리 알아오기
 const nowCategoryIdx = initState({
   key: "categoryIdx",
-  defaultState: 0,
+  defaultState: { category: 0, list: 1 },
 });
 
 const nowCategoryName = initState({
@@ -16,20 +16,9 @@ const nowCategoryName = initState({
   defaultState: "",
 });
 
-// const currentPageIdx = initState({
-//   key: "currentPageIdx",
-//   defaultState: 1,
-// });
-
-// 리스트 현재 페이지 받아오기
-const listPageIdx = initState({
-  key: "listPageIdx",
-  defaultState: 1,
-});
-
 const gridPageIdx = initState({
   key: "gridPageIdx",
   defaultState: 0,
 });
 
-export { isGrid, nowCategoryIdx, nowCategoryName, listPageIdx, gridPageIdx };
+export { isGrid, nowCategoryIdx, nowCategoryName, gridPageIdx };
