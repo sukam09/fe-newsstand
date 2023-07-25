c9f81995f49e8c71a8dc11996f692f6fd9e2f151
오지은 <0909oje@naver.com>
2023-07-25 10:58:20 +0900

    feat: mediaInfo, categoryInfo 전역 관리

--- store/media.js
+++ store/media.js
@@ -1,8 +1,32 @@
+import { MEDIA } from "../constant.js";
 import { initState } from "../observer/observer.js";
 
 const gridPageNum = initState({
   key: "gridPageNum",
   defaultState: 0,
+});
+
+const mediaIdList = initState({
+  key: "mediaIdList",
+  defaultState: Array.from({ length: MEDIA.TOTAL_NUM }, (_, idx) => idx),
+});
+
+const mediaInfo = initState({
+  key: "mediaInfo",
+  defaultState: {},
+});
+
+const categoryInfo = initState({
+  key: "categoryInfo",
+  defaultState: {
+    "종합/경제": [],
+    "방송/통신": [],
+    IT: [],
+    영자지: [],
+    "스포츠/연예": [],
+    "매거진/전문지": [],
+    지역: [],
+  },
 });
 
 const listCateIdx = initState({
@@ -20,4 +44,12 @@ const listSubsMediaIdx = initState({
   defaultState: 0,
 });
 
-export { gridPageNum, listCateIdx, listCateMediaIdx, listSubsMediaIdx };
+export {
+  gridPageNum,
+  mediaIdList,
+  mediaInfo,
+  categoryInfo,
+  listCateIdx,
+  listCateMediaIdx,
+  listSubsMediaIdx,
+};
