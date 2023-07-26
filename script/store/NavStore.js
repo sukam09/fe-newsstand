ca2817ffbe8ee4d8c54a00fcb5283147c94b3346
Jo <tockiib@gmail.com>
2023-07-26 11:53:53 +0900

    fix: 초기 구독 배열 수정

--- script/store/NavStore.js
+++ script/store/NavStore.js
@@ -8,10 +8,7 @@ class NavStore extends Store {
   constructor() {
     super({
       data: MEDIA_APP_DATA,
-      subscribed: [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25, 26,
-      ],
+      subscribed: [],
       media: 'all',
       view: 'grid',
     });
