8a288bcb9a663f7ad1fe5908ccfa382112ef2d0c
wonho1401 <wonho1401@naver.com>
2023-07-26 17:20:55 +0900

    feat: add button for dark mode

--- constants/constant.js
+++ constants/constant.js
@@ -4,6 +4,9 @@ export const RECENT_NEWS_NUM = 10;
 export const INITIAL_PAGE = 0;
 export const INITIAL_CATEGORY = 0;
 export const PROGRESS_DURATION_MS = 10000;
+export const LOGO_PATH = `../../asset/logo`;
+export const DARK_MODE = `dark-mode`;
+export const LIGHT_MODE = `light-mode`;
 export const FIELDTAB_LIST = [
   "종합/경제",
   "방송/통신",
