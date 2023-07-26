b7407dc673189aa29e5b9ec3b7e9dc2647fdb810
sukam09 <sukam09@naver.com>
2023-07-26 20:32:12 +0900

    refactor: default 변수를 App 컴포넌트 밖으로 분리

--- src/app.js
+++ src/app.js
@@ -9,6 +9,20 @@ import PressGridView from './components/PressGridview.js';
 import PressListView from './components/PressListView.js';
 import { fetchPressInfo } from './api.js';
 
+const defaultHeadlines = [
+  `정부, '처리수' 표현에 "문제없어", '핵폐수'엔 "불안감 내용 부적절"`,
+  `IAEA 최종보고서 반발…전국 곳곳서 후쿠시마 오염수 방류 규탄`,
+  `일본 원전 오염수 방류도 안했는데…찬바람 부는 횟집·양식어가`,
+  `'오염수 저지' 野의원단 방일 출국…"국제공조 통해 방류 저지"`,
+  `與 "민주 'IAEA 총장면담·日방문', 부끄럽고 한심…국격 추락"`,
+  `尹대통령, 리투아니아로 출국…나토회의서 북핵 공조 모색`,
+  `검찰, 국회 2차 압수수색…송영길 보좌진 동선 추적`,
+  `검찰, 박영수 측근 불러 보강수사…구속영장 재청구 '무게'`,
+  `조국 아들 "연세대 대학원에 석사 학위 반납"`,
+  `민주, 양평고속道 의혹에 "게이트·국정농단"…국조·특검도 거론`,
+];
+const defaultCategories = ['종합/경제', '방송/통신', 'IT', '영자지', '스포츠/연예', '매거진/전문지', '지역'];
+
 const pressInfo = await fetchPressInfo();
 const pidMap = await getPidMap();
 
@@ -31,18 +45,7 @@ export default function App({ $app }) {
   new AutoRollingNews({
     $target: $app,
     initialState: {
-      data: [
-        `정부, '처리수' 표현에 "문제없어", '핵폐수'엔 "불안감 내용 부적절"`,
-        `IAEA 최종보고서 반발…전국 곳곳서 후쿠시마 오염수 방류 규탄`,
-        `일본 원전 오염수 방류도 안했는데…찬바람 부는 횟집·양식어가`,
-        `'오염수 저지' 野의원단 방일 출국…"국제공조 통해 방류 저지"`,
-        `與 "민주 'IAEA 총장면담·日방문', 부끄럽고 한심…국격 추락"`,
-        `尹대통령, 리투아니아로 출국…나토회의서 북핵 공조 모색`,
-        `검찰, 국회 2차 압수수색…송영길 보좌진 동선 추적`,
-        `검찰, 박영수 측근 불러 보강수사…구속영장 재청구 '무게'`,
-        `조국 아들 "연세대 대학원에 석사 학위 반납"`,
-        `민주, 양평고속道 의혹에 "게이트·국정농단"…국조·특검도 거론`,
-      ],
+      data: defaultHeadlines,
     },
   });
 
@@ -76,8 +79,6 @@ export default function App({ $app }) {
 
   const $div = document.createElement('div');
   $app.appendChild($div);
-
-  const defaultCategories = ['종합/경제', '방송/통신', 'IT', '영자지', '스포츠/연예', '매거진/전문지', '지역'];
 
   const onChangePress = press => {
     this.setState({ ...this.state, press });
