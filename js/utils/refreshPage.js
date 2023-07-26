fce6f21c88b68412368afda094787372f2e428f9
WongilK <wwwkim99@naver.com>
2023-07-26 15:32:47 +0900

    "[DOCS] 중복 파일 삭제"

--- js/utils/refreshPage.js
+++
@@ -1,8 +0,0 @@
-function refreshPage(className) {
-  function handleLogoClick() {
-    window.location.reload();
-  }
-  const target = document.querySelector(className);
-  target.addEventListener("click", handleLogoClick);
-}
-export { refreshPage };
