9d1b6751f36c4dfde6dfdf7b02a760d6cce14b48
devMingu <rooster100@naver.com>
2023-07-27 16:25:12 +0900

    refactor: 커스텀 쿼리 리팩토링

--- js/utils/customSelector/customQuery.js
+++ js/utils/customSelector/customQuery.js
@@ -29,9 +29,9 @@ export class CustomQuery {
       [...node.classList].includes(search) && this.result.push(node);
     }
 
-    node.childNodes.forEach((element) =>
-      this.getAllElementWithClassName(search, element)
-    );
+    for (const elem of node.children) {
+      this.getAllElementWithClassName(search, elem);
+    }
 
     return this.result;
   }
@@ -53,63 +53,8 @@ export class CustomQuery {
     }
     return this.result;
   }
-
-  customQuerySelectById(search, node = document) {
-    node.id === search && this.result.push(node);
-
-    node.childNodes.forEach((element) =>
-      this.customQuerySelectById(search, element)
-    );
-
-    return this.result;
-  }
 }
 
-// export const customQuery = new CustomQuery();
+const custom = new CustomQuery();
 
-// const resultTagName = [];
-// const resultClassName = [];
-// const resultId = [];
-
-// /**
-//  * 찾고싶은 HTML Tag들을 전부 찾아서 반환합니다.
-//  * @param {Element} search
-//  * @param {DOM} parent
-//  * @returns Array
-//  */
-// export function customQuerySelectAllByTagName(search, parent = document) {
-//   parent.nodeName === search.toUpperCase() && resultTagName.push(parent);
-
-//   parent.childNodes.forEach((element) =>
-//     customQuerySelectAllByTagName(search, element)
-//   );
-//   return resultTagName;
-// }
-
-// /**
-//  * 클래스 이름을통해 HTML Tag를 찾아서 반환합니다.
-//  * @param {className} search
-//  * @param {DOM} parent
-//  * @returns Array
-//  */
-// export function getElementWithClassName(search, parent = document) {
-//   if (parent.classList) {
-//     [...parent.classList].includes(search) && resultClassName.push(parent);
-//   }
-
-//   parent.childNodes.forEach((element) =>
-//     getElementWithClassName(search, element)
-//   );
-
-//   return resultClassName;
-// }
-
-// export function customQuerySelectById(search, parent = document) {
-//   parent.id === search && resultId.push(parent);
-
-//   parent.childNodes.forEach((element) =>
-//     customQuerySelectById(search, element)
-//   );
-
-//   return resultId;
-// }
+console.log(custom.customQuerySelectAllByTagName("ul"));
