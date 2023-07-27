819159e270c890a6706af078c391882d1ba2de70
wonho1401 <wonho1401@naver.com>
2023-07-27 16:24:04 +0900

    style: change function name

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import { RECENT_NEWS_NUM } from "./constants/constant.js";
 import { setEvents } from "./utils/setEvents.js";
 import { DateComponent } from "./components/Date/Date.js";
 import { initGrid } from "./components/Grid/InitGrid.js";
-import { fetchData, shuffleData, sliceData } from "./utils/utils.js";
+import { fetchAllPress, shufflePress, sliceData } from "./utils/utils.js";
 import { initializeStore } from "./store/store.js";
 import { setAllAgencies } from "./utils/setAgencies.js";
 
 // time 설정
 DateComponent();
 
-fetchData().then((data) => {
+fetchAllPress().then((press_list) => {
   // response로 받은 data shuffle
-  const agencies = shuffleData(data);
+  const agencies = shufflePress(press_list);
   // rolling할 뉴스 slice
   const sliced_data = sliceData(agencies, 0, RECENT_NEWS_NUM);
 
@@ -20,7 +20,7 @@ fetchData().then((data) => {
   rollNews(sliced_data);
   setAllAgencies(agencies);
   // 상태에 따라 grid, list view render
-  initializeStore(data);
+  initializeStore(press_list);
   initGrid(agencies);
   setEvents();
 });
