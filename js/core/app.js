6388858ae3c182b1c735a3ddcdbbfb1183b774c3
devMingu <rooster100@naver.com>
2023-07-27 13:55:50 +0900

    feat: 커스텀 쿼리 구현해보기...아직 미완

--- js/core/app.js
+++ js/core/app.js
@@ -1,5 +1,4 @@
 import { renderHeader } from "./header.js";
 import { renderMain } from "./main.js";
-
 renderHeader();
 renderMain();
---
+++ js/utils/customSelector/customQuery.js
@@ -0,0 +1,111 @@
+export class CustomQuery {
+  constructor() {
+    this.result = [];
+  }
+
+  /**
+   * 찾고싶은 HTML Tag들을 전부 찾아서 반환합니다.
+   * @param {Element} search
+   * @param {DOM} parent
+   * @returns Array
+   */
+  customQuerySelectAllByTagName(search, parent = document) {
+    parent.nodeName === search.toUpperCase() && this.result.push(parent);
+
+    parent.childNodes.forEach((element) =>
+      this.customQuerySelectAllByTagName(search, element)
+    );
+    return resultTagName;
+  }
+
+  /**
+   * 클래스 이름을통해 HTML Tag를 찾아서 반환합니다.
+   * @param {className} search
+   * @param {DOM} parent
+   * @returns Array
+   */
+  getAllElementWithClassName(search, parent = document) {
+    if (parent.classList) {
+      [...parent.classList].includes(search) && this.result.push(parent);
+    }
+
+    parent.childNodes.forEach((element) =>
+      this.getAllElementWithClassName(search, element)
+    );
+
+    return this.result;
+  }
+
+  getElementWithClassName(search, parent = document) {
+    for (const elem of parent.children) {
+      if ([...elem.classList].includes(search)) {
+        if (!this.result.length) {
+          this.result.push(elem);
+        }
+        return this.result;
+      }
+      this.getElementWithClassName(search, elem);
+    }
+
+    return this.result;
+  }
+
+  customQuerySelectById(search, parent = document) {
+    parent.id === search && this.result.push(parent);
+
+    parent.childNodes.forEach((element) =>
+      this.customQuerySelectById(search, element)
+    );
+
+    return this.result;
+  }
+}
+
+// export const customQuery = new CustomQuery();
+
+// const resultTagName = [];
+// const resultClassName = [];
+// const resultId = [];
+
+// /**
+//  * 찾고싶은 HTML Tag들을 전부 찾아서 반환합니다.
+//  * @param {Element} search
+//  * @param {DOM} parent
+//  * @returns Array
+//  */
+// export function customQuerySelectAllByTagName(search, parent = document) {
+//   parent.nodeName === search.toUpperCase() && resultTagName.push(parent);
+
+//   parent.childNodes.forEach((element) =>
+//     customQuerySelectAllByTagName(search, element)
+//   );
+//   return resultTagName;
+// }
+
+// /**
+//  * 클래스 이름을통해 HTML Tag를 찾아서 반환합니다.
+//  * @param {className} search
+//  * @param {DOM} parent
+//  * @returns Array
+//  */
+// export function getElementWithClassName(search, parent = document) {
+//   if (parent.classList) {
+//     [...parent.classList].includes(search) && resultClassName.push(parent);
+//   }
+
+//   parent.childNodes.forEach((element) =>
+//     getElementWithClassName(search, element)
+//   );
+
+//   return resultClassName;
+// }
+
+// export function customQuerySelectById(search, parent = document) {
+//   parent.id === search && resultId.push(parent);
+
+//   parent.childNodes.forEach((element) =>
+//     customQuerySelectById(search, element)
+//   );
+
+//   return resultId;
+// }
