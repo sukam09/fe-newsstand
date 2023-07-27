71403de5aa702ab797042416208588d78377ae75
박태규 Park Tae Kyu <ptq124@naver.com>
2023-07-27 12:48:14 +0900

    Feat: querySelect API 구현

---
+++ js/utils/myQuerySelector.js
@@ -0,0 +1,46 @@
+function myQuerySelector(selector) {
+  //최초 dom 탐색
+  const dom = document.querySelector('#root');
+
+  //ID, Class 구분
+  if (selector.startsWith('.')) return classSelector(dom, selector);
+  if (selector.startsWith('#')) return idSelector(dom, selector);
+
+  return null;
+}
+
+function classSelector(dom, selector) {
+  const className = selector.slice(1);
+  let currentElement = '';
+
+  for (const node of dom.children) {
+    currentElement = dfs(node, className);
+    if (currentElement) break;
+  }
+
+  return currentElement;
+}
+
+function idSelector(dom, selector) {
+  const id = selector.slice(1);
+  let currentElement = '';
+
+  for (const node of dom.children) {
+    currentElement = dfs(node, id);
+    if (currentElement) break;
+  }
+
+  return currentElement;
+}
+
+const dfs = (node, elementIdentifier) => {
+  if (node.className === elementIdentifier) return node;
+  if (node.id === elementIdentifier) return node;
+
+  for (let child of node.children) {
+    const findNode = dfs(child, elementIdentifier);
+    if (findNode) return findNode;
+  }
+};
+
+export { myQuerySelector };
