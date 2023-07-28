1d684761f69d314f5f60bc378110f6d8574f5619
DaeHee99 <mokday99@gmail.com>
2023-07-28 10:30:33 +0900

    [Refactor] svg 파일 분리 & 정리

--- src/Components/NewsGrid/NewsGrid.js
+++ src/Components/NewsGrid/NewsGrid.js
@@ -21,27 +21,12 @@ export default class NewsGrid extends Component {
   template() {
     return `
       <ul class="newspaper__list"></ul>
+
       <div class="left-button_content">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="26"
-          height="42"
-          viewBox="0 0 26 42"
-          fill="none"
-        >
-          <path d="M25 1L1 21L25 41" stroke="#6E8091" />
-        </svg>
+        <img src="./assets/icons/LeftPage.svg" alt="LeftPage" />
       </div>
       <div class="right-button_content">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="26"
-          height="42"
-          viewBox="0 0 26 42"
-          fill="none"
-        >
-          <path d="M1 41L25 21L1 1" stroke="#6E8091" />
-        </svg>
+        <img src="./assets/icons/RightPage.svg" alt="RightPage" />
       </div>
     `;
   }
