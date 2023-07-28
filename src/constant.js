38b1c218890d9b847f3cf703de67a2b3c5f7cdd7
1lsang <ilsangpark.98@gmail.com>
2023-07-28 11:22:18 +0900

    Feat: 상수 파일 추가

---
+++ src/constant.js
@@ -0,0 +1,13 @@
+export const GRID = "grid";
+export const LIST = "list";
+export const PAGINATION_UNIT = 24;
+export const MAX_GRID_PAGE_NUM = 4;
+export const CATEGORY_OPTIONS = [
+    "종합/경제",
+    "방송/통신",
+    "IT",
+    "영자지",
+    "스포츠/연예",
+    "매거진/전문지",
+    "지역",
+];
