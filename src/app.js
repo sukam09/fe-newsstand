eca534bdb5b6c4d9334a7856468df60e5cdb8cd4
1lsang <ilsangpark.98@gmail.com>
2023-07-28 11:22:57 +0900

    Feat: 마무리

--- src/app.js
+++ src/app.js
@@ -1,144 +1,33 @@
-import { renderView } from "./lib/render.js";
-import { observable, observe } from "./store/core.js";
-import { createElement, removeElement } from "./lib/dom.js";
-import { addCustomEventListener } from "./lib/event.js";
-import { updateDBCompanies } from "./api/index.js";
-import { html } from "./lib/html.js";
-import { MAX_GRID_PAGE_NUM, MAX_LIST_PAGE_NUM, setConfig } from "./config.js";
-import { setHeader } from "./components/Header.js";
-(async function init() {
+import { observe, store, updateStoreId } from "./store/state.js";
+import newsStore from "./store/news.js";
+import { html } from "./lib/jsx.js";
+import Header from "./components/header/Header.js";
+import { CATEGORY_OPTIONS } from "./constant.js";
+import RecentNewsSection from "./components/recent-news/RecentNewsSection.js";
+import { render } from "./lib/render.js";
+const app = document.getElementById("app");
+(async function () {
     // Initialize
-    await updateDBCompanies();
-    await setConfig();
-    // Set Component
-    setHeader();
-    // Store 생성
-    const store = observable({
-        type: "grid",
-        recentNews: [],
-        filter: "all",
-        gridPage: 0,
-        listPage: 0,
-        get idx() {
-            switch (store.type) {
-                case "grid":
-                    return store.gridPage;
-                case "list":
-                    return store.listPage;
-                default:
-                    return 0;
-            }
-        },
-        set idx(value) {
-            switch (store.type) {
-                case "grid":
-                    store.gridPage = value;
-                case "list":
-                    if (value >= MAX_LIST_PAGE_NUM)
-                        value = 0;
-                    if (value < 0)
-                        value = MAX_LIST_PAGE_NUM - 1;
-                    store.listPage = value;
-                default:
-                    break;
-            }
-        },
+    await newsStore.init();
+    store.id = newsStore.getCategoryData({
+        category: CATEGORY_OPTIONS[0],
     });
-    // Add Rolling Evnet
-    document.querySelectorAll(".news-article__list")[1].style.animationDelay =
-        "1s";
-    document.querySelectorAll(".news-article__list").forEach(($ul, idx) => {
-        $ul.addEventListener("animationiteration", (e) => {
-            removeElement($ul.children[0]);
-            $ul.appendChild(createElement(html `<li class="news-title"><a href="/#">뉴스 내용</a></li>`));
-        });
-        $ul.addEventListener("mouseenter", (e) => {
-            $ul.style.animationPlayState = "paused";
-        });
-        $ul.addEventListener("mouseleave", (e) => {
-            $ul.style.animationPlayState = "running";
-        });
-    });
-    // Add event listener of view type buttons
-    document.querySelectorAll(".view-type__option").forEach(($element) => {
-        $element.addEventListener("click", (e) => {
-            //@ts-ignore
-            store.type = e.currentTarget.dataset.option;
-        });
-    });
-    // Add event listener of move page buttons
-    document.querySelectorAll(".move-page__btn").forEach(($element) => {
-        const eventListner = (event) => {
-            // @ts-ignore
-            const type = event.currentTarget.dataset.dir;
-            switch (type) {
-                case "prev":
-                    store.idx--;
-                    break;
-                case "next":
-                    store.idx++;
-                    break;
-                default:
-                    break;
-            }
-        };
-        addCustomEventListener($element, "click", eventListner);
-    });
+    app.append(html `
+      <div class="app__container">
+        ${Header()} ${RecentNewsSection()}
+        <section class="main__container">
+          <h2 class="main__title screen-reader-only">언론사 보기</h2>
+        </section>
+      </div>
+    `);
     // log
     observe(() => {
-        console.log(store.type, store.idx);
+        console.log(store.filter, store.type, store.idx, store.category);
     });
-    // view type에 따라 view 영역을 렌더링합니다.
+    observe(updateStoreId);
     observe(() => {
-        renderView(store.type, store.idx);
-    });
-    // 활성화된 view type 버튼의 색상을 변경합니다. (active 클래스 추가)
-    observe(() => {
-        document.querySelectorAll(".view-type__option").forEach(($element) => {
-            if ($element.dataset.option === store.type) {
-                $element.classList.add("active");
-            }
-            else {
-                $element.classList.remove("active");
-            }
-        });
-    });
-    // 페이지 이동 버튼의 활성화 여부를 결정합니다. (disabled 속성 추가)
-    observe(() => {
-        document.querySelectorAll(".move-page__btn").forEach(($element) => {
-            const type = $element.dataset.dir;
-            switch (type) {
-                case "prev":
-                    if (store.type === "grid" && store.idx === 0)
-                        $element.setAttribute("disabled", "");
-                    else
-                        $element.removeAttribute("disabled");
-                    break;
-                case "next":
-                    if (store.type === "grid" && store.idx >= MAX_GRID_PAGE_NUM - 1)
-                        $element.setAttribute("disabled", "");
-                    else
-                        $element.removeAttribute("disabled");
-                    break;
-                default:
-                    break;
-            }
-        });
-    });
-    // 페이지 자동으로 넘기기
-    observe(() => {
-        // console.log(qs(".list__nav .active"));
-        // addCustomEventListener(qs(".category--progress"), "animationend", () => {
-        //   store.idx++;
-        // });
-        let interval;
-        if (store.type === "list") {
-            interval = setInterval(() => {
-                store.idx++;
-            }, 20000);
-        }
-        else {
-            clearInterval(interval);
-        }
+        store.category;
+        store.filter;
+        render();
     });
 })();
--- src/lib/render.js
+++ src/lib/render.js
@@ -1,88 +1,45 @@
-import { gridItemTemplate, gridTemplate } from "../template/grid.template.js";
-import { ListTemplate } from "../template/list.template.js";
-import { qs } from "./dom.js";
-import { html } from "./html.js";
-import { getCategories, getGridPageData, getListPageData, } from "../api/index.js";
-import { PAGINATION_UNIT } from "../config.js";
-import { convertIdx } from "./utils.js";
-const $view = qs(".view");
-const categories = [];
-async function renderGridView(idx) {
-    // grid data fetching
-    const data = await getGridPageData(idx);
-    // empty data padding
-    while (data.length < PAGINATION_UNIT) {
-        // @ts-ignore
-        data.push({});
-    }
-    // render
-    $view.innerHTML = gridTemplate(data.reduce((acc, cur) => {
-        return acc + gridItemTemplate(cur);
-    }, ""));
-}
-async function renderListView(idx) {
-    // list data fetching
-    if (categories.length === 0) {
-        categories.push(...(await getCategories()));
-    }
-    const { convertedIdx, category } = convertIdx(idx, categories);
-    console.log(convertedIdx, category);
-    const data = await getListPageData(category, convertedIdx);
-    // render
-    $view.innerHTML = ListTemplate(categories, category, convertedIdx, data);
-    // $view.innerHTML = ListTemplate(data);
-}
-function renderView(type, idx) {
-    try {
-        switch (type) {
-            case "grid":
-                renderGridView(idx);
-                break;
-            case "list":
-                renderListView(idx);
-                break;
-            default:
-                throw new Error("Invalid View Type");
-        }
-    }
-    catch (error) {
-        console.error(error);
-    }
-}
-function renderListViewPage(data) {
-    const $listViewPage = document.querySelector(".list-view");
-    // $listViewPage.innerHTML = "";
-    $listViewPage.innerHTML = html `
-    <div class="list-view">
-      <div class="list-page__nav">
-        <a href="${data.url}">
-          <img
-            src="/public/asset/images/light/${data.id}.png"
-            class="logo"
-            alt="${data.name}"
-          />
-        </a>
-        <span class="list-page__datetime">${String(data.editTime)} 편집</span>
-        <button class="subscribe__btn">구독하기</button>
-      </div>
-      <div class="list-page__articles">
-        <a href="${data.mainArticle.url}">
-          <article class="main-article">
-            <img src="${data.mainArticle.thumbnail}" />
-            <span> ${data.mainArticle.title} </span>
-          </article>
-        </a>
-        <div class="sub-articles">
-          <ul class="sub-articles__list">
-            ${data.subArticles
-        .slice(0, 6)
-        .reduce((accumulator, article) => accumulator +
-        `<li><a href="${article.url}">${article.title}</a></li>`, "")}
-          </ul>
-          <div class="copyright">${data.name}에서 직접 편집한 뉴스입니다.</div>
-        </div>
+import newsStore from "../store/news.js";
+import GridContainer from "../components/content/grid/GridContainer.js";
+import ListContainer from "../components/content/list/ListContainer.js";
+import ContentNaviagtion from "../components/content/navigation/ContentNavigation.js";
+import NoContent from "../components/content/no-content/NoContent.js";
+import NextButton from "../components/content/page-button/NextButton.js";
+import PrevButton from "../components/content/page-button/PrevButton.js";
+import { GRID, LIST } from "../constant.js";
+import { store } from "../store/state.js";
+import { html } from "./jsx.js";
+
+export const makePage = () => {
+  const { data, maxPage } =
+    store.type === GRID
+      ? newsStore.getGridPageData({
+          idx: store.idx,
+          subscribe: store.filter === "all" ? false : true,
+        })
+      : newsStore.getDataById({
+          id: store.id[store.idx],
+        });
+  const hasData = data.length > 0;
+  return html`
+    <div>
+      ${ContentNaviagtion()}
+      <div class="page__container">
+        ${hasData && [PrevButton(), NextButton({ maxPage })]}
+        <main class="view">
+          ${hasData
+            ? (store.type === GRID && GridContainer({ data })) ||
+              (store.type === LIST && ListContainer({ data }))
+            : NoContent()}
+        </main>
       </div>
     </div>
   `;
-}
-export { renderView };
+};
+
+export const render = () => {
+  store.category;
+  const mainContainer = document.querySelector(".main__container");
+  mainContainer.innerHTML = "";
+  mainContainer.append(makePage());
+  document.querySelector(".list__nav .active")?.scrollIntoView();
+};
